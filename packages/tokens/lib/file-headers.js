cd2bbfd984089cb3edf45a98e877b2804eeaeaf4
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-09-07 14:22:10 +0200

    🐽

--- packages/tokens/lib/file-headers.js
+++ packages/tokens/lib/file-headers.js
@@ -2,10 +2,10 @@ export const legal = {
   name: 'sl/legal',
   fileHeader(defaultMessage) {
     return [
-      ...defaultMessage,
-      '',
       'Copyright 2022 Sanoma Learning',
-      'SPDX-License-Identifier: Apache-2.0'
+      'SPDX-License-Identifier: Apache-2.0',
+      '',
+      ...defaultMessage
     ];
   }
 };

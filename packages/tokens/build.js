572b5066a91791fbc7c5a1dabf028f023048c2b7
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-09-13 13:18:41 +0200

    💥

--- packages/tokens/build.js
+++ packages/tokens/build.js
@@ -4,29 +4,54 @@ import * as FileHeaders from './lib/file-headers.js';
 import * as Formats from './lib/formats.js';
 import * as Transforms from './lib/transforms.js';
 import * as TransformGroups from './lib/transform-groups.js';
-import themes from './src/$themes.json' assert { type: 'json' };
+import figmaThemes from './src/figma/$themes.json' assert { type: 'json' };
 
-const id = process.argv.at(2),
-  [name, variant] = id.split('/'),
-  theme = themes.find(theme => theme.name === id),
-  output = `dist/${name}/${variant}.json`;
+const cwd = new URL('.', import.meta.url).pathname;
 
-const tokenSets = Object
-  .entries(theme.selectedTokenSets)
-  .filter(([_, enabled]) => enabled === 'enabled')
-  .map(([name]) => name);
+// The name, e.g. "sanoma-learning"
+const name = process.argv.at(2);
 
-await (async () => {
+// The theme variants, extracted from "$themes.json"
+const themes = figmaThemes
+  .filter(theme => theme.name.startsWith(name + '/'))
+  .map(({ name: themeName, selectedTokenSets }) => {
+    return {
+      variant: themeName.substring(name.length + 1),
+      tokenSets: Object
+        .entries(selectedTokenSets)
+        .filter(([_, enabled]) => enabled === 'enabled')
+        .map(([name]) => name)
+    };
+  });
+
+// Remove "core" and "base" from the specific variants
+themes.forEach(theme => {
+  theme.tokenSets = theme.tokenSets.filter(set => {
+    return set !== 'core' && !set.endsWith('/base');
+  });
+});
+
+// Add a "base" variant for optimization
+themes.push({
+  variant: 'base',
+  tokenSets: ['core', `${name}/base`]
+});
+
+// Run the token transformer for all variants
+await Promise.all(themes.map(({ variant, tokenSets }) => {
   return new Promise((resolve, reject) => {
-    exec(`yarn run token-transformer src ${output} ${tokenSets.join(',')} --resolveReferences=false`, error => {
+    const output = `src/themes/${name}/${variant}.json`;
+
+    exec(`yarn run token-transformer src/figma ${output} ${tokenSets.join(',')} --resolveReferences=false`, { cwd }, error => {
       if (error) {
+        console.log(error);
         reject(error);
       }
     
       resolve();
     });
   });
-})();
+}));
 
 StyleDictionary
   .registerFileHeader(FileHeaders.legal)
@@ -37,7 +62,7 @@ StyleDictionary
   .registerTransform(Transforms.sizePx)
   .registerTransformGroup(TransformGroups.css)
   .extend({
-    source: [output],
+    source: ['base.json'],
     platforms: {
       css: {
         transformGroup: 'css',
@@ -45,7 +70,7 @@ StyleDictionary
         buildPath: './',
         files: [
           {
-            destination: `dist/${name}/${variant}.css`,
+            destination: 'base.css',
             format: 'css/variables',
             options: {
               fileHeader: 'sl/legal',
@@ -58,7 +83,7 @@ StyleDictionary
         transformGroup: 'css',
         files: [
           {
-            destination: `dist/${name}/mixins.scss`,
+            destination: 'mixins.scss',
             format: 'scss/mixins',
             options: {
               fileHeader: 'sl/legal',

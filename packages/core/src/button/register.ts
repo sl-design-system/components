5da186af059c01aa866c4c2240309b42ca4125d1
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-09-16 09:03:14 +0200

    🏜

--- packages/core/src/button/register.ts
+++ packages/core/src/button/register.ts
@@ -2,8 +2,8 @@ import { Button } from './button';
 
 customElements.define('sl-button', Button);
 
-// declare global {
-//   interface HTMLElementTagNameMap {
-//     'sl-button': Button;
-//   }
-// }
+declare global {
+  interface HTMLElementTagNameMap {
+    'sl-button': Button;
+  }
+}

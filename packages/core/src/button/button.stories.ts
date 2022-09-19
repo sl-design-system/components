d0fbf0246e990b9db7db72b3d2d015d4771ca076
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-09-19 14:08:06 +0200

    🔬

--- packages/core/src/button/button.stories.ts
+++ packages/core/src/button/button.stories.ts
@@ -1,10 +1,39 @@
-import './register.js';
+import { Story } from '@storybook/web-components';
 import { html } from 'lit';
+import './register.js';
 
 export default {
-  title: 'Button'
-}
+  title: 'Button',
+  argTypes: {
+    fill: {
+      control: {
+        type: 'inline-radio',
+        options: ['ghost', 'outline', 'solid', 'subtle']
+      }
+    },
+    size: {
+      control: {
+        type: 'inline-radio',
+        options: ['sm', 'md', 'lg']
+      }
+    },
+    variant: {
+      control: {
+        type: 'radio',
+        options: ['primary', 'secondary']
+      }
+    }
+  }
+};
 
-export const API = ({}) => {
-  return html`<sl-button>Button</sl-button>`;
+export const API: Story = {
+  args: {
+    fill: 'solid',
+    size: 'lg',
+    text: 'Button',
+    variant: 'primary'
+  },
+  render: ({ fill, size, text, variant }) => html`
+    <sl-button .fill=${fill} .size=${size} .variant=${variant}>${text}</sl-button>
+  `
 };
--- packages/core/src/button/button.ts
+++ packages/core/src/button/button.ts
@@ -3,14 +3,24 @@ import { LitElement, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import styles from './button.scss.js';
 
-export type ButtonVariant = 'ghost' | 'outline' | 'solid' | 'subtle';
+export type ButtonSize = 'sm' | 'md' | 'lg';
+
+export type ButtonFill = 'ghost' | 'outline' | 'solid' | 'subtle';
+
+export type ButtonVariant = 'primary' | 'secondary';
 
 export class Button extends LitElement {
   /** @private */
   static styles: CSSResultGroup = styles;
 
-  /** The button variant. */
-  @property({ reflect: true }) variant: ButtonVariant = 'solid';
+  /** The button fill. */
+  @property({ reflect: true }) fill: ButtonFill = 'solid';
+
+  /** Size. */
+  @property({ reflect: true }) size: ButtonSize = 'lg';
+
+  /** The variant. */
+  @property({ reflect: true }) variant: ButtonVariant = 'primary';
 
   render(): TemplateResult {
     return html`<slot></slot>`;

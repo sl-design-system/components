6e0bc8290feafa4a14d04cf3a71b05320ad7ac6a
anna-lach <a.lach@nowaera.pl>
2022-10-26 16:16:22 +0200

    docs: 11ty configuration changes, eslint changes, html minifier

--- docs/website/.eleventy.cjs
+++ docs/website/.eleventy.cjs
@@ -1,38 +1,32 @@
 const litPlugin = require('@lit-labs/eleventy-plugin-lit');
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const markdownItAnchor = require("markdown-it-anchor");
-const markdownIt = require("markdown-it");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const slugify = require("slugify");
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const markdownItAnchor = require('markdown-it-anchor');
+const markdownIt = require('markdown-it');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const slugify = require('slugify');
+const htmlMinifier = require('html-minifier');
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(litPlugin, {
-        mode: 'worker',
-        componentModules: [
-            './src/js/demo-greeter.js',
-            './src/js/my-element.js'
-        ],
-    });
+  eleventyConfig.addPlugin(litPlugin, {
+    mode: 'worker',
+    componentModules: [
+      './src/js/demo-greeter.js',
+      './src/js/my-element.js',
+    ],
+  });
 
-    module.exports = function(eleventyConfig) {
-        eleventyConfig.addPlugin(syntaxHighlight);
-    };
+  module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight);
+  };
 
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-    // let markdownIt = require("markdown-it");
-    // let options = {
-    //     html: true,
-    // };
-
-  // const md = markdownIt();
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   function removeExtraText(s) {
-    let newStr = String(s).replace(/New\ in\ v\d+\.\d+\.\d+/, "");
-    newStr = newStr.replace(/Coming\ soon\ in\ v\d+\.\d+\.\d+/, "");
-    newStr = newStr.replace(/⚠️/g, "");
-    newStr = newStr.replace(/[?!]/g, "");
-    newStr = newStr.replace(/<[^>]*>/g, "");
+    let newStr = String(s).replace(/New\ in\ v\d+\.\d+\.\d+/, '');
+    newStr = newStr.replace(/Coming\ soon\ in\ v\d+\.\d+\.\d+/, '');
+    newStr = newStr.replace(/⚠️/g, '');
+    newStr = newStr.replace(/[?!]/g, '');
+    newStr = newStr.replace(/<[^>]*>/g, '');
     return newStr;
   }
 
@@ -40,71 +34,82 @@ module.exports = function(eleventyConfig) {
     return slugify(removeExtraText(s), { lower: true, remove: /[:’'`,]/g });
   }
 
-   const anchor = require('markdown-it-anchor');
+  const anchor = require('markdown-it-anchor');
 
   let mdIt = markdownIt({
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
   })
-    .disable('code') // disable indent -> code block
+    .disable('code')
     .use(markdownItAnchor, {
       // permalink: true,
       permalink: anchor.permalink.headerLink(),
       slugify: markdownItSlugify,
       permalinkBefore: true,
-      permalinkClass: "direct-link",
-      permalinkSymbol: "#",
-      level: [1,2,3,4]
+      permalinkClass: 'direct-link',
+      permalinkSymbol: '#',
+      level: [1, 2, 3, 4],
     });
 
-/*    .use(markdownItAnchor, {
-    // level: 1,
-    level: [1,2,3,4],
-    // slugify: string => string,
-    permalink: true,
-    // renderPermalink: (slug, opts, state, permalink) => {},
-    permalinkClass: 'header-anchor',
-    permalinkSymbol: '#',
-    permalinkBefore: false
-  });*/
+  /*    .use(markdownItAnchor, {
+      // level: 1,
+      level: [1,2,3,4],
+      // slugify: string => string,
+      permalink: true,
+      // renderPermalink: (slug, opts, state, permalink) => {},
+      permalinkClass: 'header-anchor',
+      permalinkSymbol: '#',
+      permalinkBefore: false
+    });*/
 
 
   eleventyConfig.setBrowserSyncConfig({
     notify: true,
-    files: './public/css/**/*.css'
+    files: './public/css/**/*.css',
   });
 
-    eleventyConfig.setLibrary("md", mdIt/*markdownIt(options)*/);
+  eleventyConfig.setLibrary('md', mdIt/*markdownIt(options)*/);
 
-    eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/assets');
 
-  const fs = require("fs");
-  const NOT_FOUND_PATH = "public/404.html";
+  const fs = require('fs');
+  const NOT_FOUND_PATH = 'public/404.html';
 
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, bs) {
 
-        bs.addMiddleware("*", (req, res) => {
+        bs.addMiddleware('*', (req, res) => {
           if (!fs.existsSync(NOT_FOUND_PATH)) {
             throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
           }
 
           const content_404 = fs.readFileSync(NOT_FOUND_PATH);
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' });
           res.write(content_404);
           res.end();
         });
-      }
-    }
+      },
+    },
   });
 
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        },
-        passthroughFileCopy: true,
+  eleventyConfig.addTransform('htmlMinifier', content => {
+    if (process.env.NODE_ENV === 'production') {
+      return htmlMinifier.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
     }
+    return content;
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+    },
+    passthroughFileCopy: true,
+  };
 };
--- docs/website/.eslintrc.cjs
+++ docs/website/.eslintrc.cjs
@@ -1,12 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  'root': true,
+  'parser': 'espree',
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    "extends": [
-      "airbnb-base",
-      "eslint:recommended"
-    ],
-    "rules": {
-    }
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  plugins: ['unused-imports'],
+  'extends': [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  'rules': {
+    'no-duplicate-imports': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'import/named': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+  }
 }

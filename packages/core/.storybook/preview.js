ae636c545ca72ccf9dbc9d51ceb0cc47a9dc8188
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-10-30 20:42:56 +0100

    🐂

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -7,5 +7,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/
     }
+  },
+  options: {
+    storySort: (a, b) => a.title === b.title ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true })
   }
 };

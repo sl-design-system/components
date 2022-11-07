36db8cb9265f28f3f33f02b0e941a2dbcab251ca
anna-lach <a.lach@nowaera.pl>
2022-11-07 13:24:52 +0100

    docs: search POC with elasticlunr - refactor

--- docs/website/.eleventy.cjs
+++ docs/website/.eleventy.cjs
@@ -27,8 +27,6 @@ module.exports = function(eleventyConfig) {
     return [...collection.getFilteredByGlob("./src/categories/**/*.md")];
   });
 
-  eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
-
   function removeExtraText(s) {
     let newStr = String(s).replace(/New\ in\ v\d+\.\d+\.\d+/, '');
     newStr = newStr.replace(/Coming\ soon\ in\ v\d+\.\d+\.\d+/, '');
@@ -107,31 +105,3 @@ module.exports = function(eleventyConfig) {
     passthroughFileCopy: true,
   };
 };
-
-function extractExcerpt(article) {
-  if (!article.hasOwnProperty('templateContent')) {
-    console.warn('Failed to extract excerpt: Document has no property "templateContent".');
-    return null;
-  }
-
-  let excerpt = null;
-  const content = article.templateContent;
-
-  // The start and end separators to try and match to extract the excerpt
-  const separatorsList = [
-    { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
-    { start: '<p>', end: '</p>' }
-  ];
-
-  separatorsList.some(separators => {
-    const startPosition = content.indexOf(separators.start);
-    const endPosition = content.indexOf(separators.end);
-
-    if (startPosition !== -1 && endPosition !== -1) {
-      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-      return true; // Exit out of array loop on first match
-    }
-  });
-  console.log('excerpt', excerpt);
-  return content;
-}
--- docs/website/src/js/filters/searchFilter.cjs
+++ docs/website/src/js/filters/searchFilter.cjs
@@ -1,19 +1,16 @@
 const elasticlunr = require("elasticlunr");
 
 module.exports = function(collection) {
-  var index = elasticlunr(function() {
-    this.addField("title");
-    // this.addField("excerpt");
-    this.setRef("id");
-    this.addField("content");
+  const index = elasticlunr(function() {
+    this.setRef('id');
+    this.addField('title');
+    this.addField('content');
   });
 
   collection.forEach(page => {
-    console.log('page.template', /*page.templateContent,*/ page.templateContent.replace(/<[^>]+>/g, ''));
     index.addDoc({
       id: page.url,
       title: page.template.frontMatter.data.title,
-      // excerpt: page.template.frontMatter.data.excerpt,
       content: page.templateContent.replace(/<[^>]+>/g, '')
     });
   });

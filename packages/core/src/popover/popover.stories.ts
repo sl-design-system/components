2f1384f15432870a6ab6b21d49944cc3ec0ba1ed
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-11-13 11:00:00 +0100

    🐂

--- packages/core/src/popover/popover.stories.ts
+++ packages/core/src/popover/popover.stories.ts
@@ -2,6 +2,8 @@ import type { StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import '../button/register.js';
+import '../button-bar/register.js';
+import '../input/register.js';
 import './register.js';
 
 export default {
@@ -24,5 +26,32 @@ export const API: StoryObj = {
   render: ({ placement }) => html`
     <sl-button id="button">Toggle popover</sl-button>
     <sl-popover anchor="button" placement=${ifDefined(placement)}>I'm a popover</sl-popover>
+  `
+};
+
+export const Autofocus: StoryObj = {
+  render: () => html`
+    <style>
+      sl-popover form {
+        align-items: center;
+        display: grid;
+        gap: 0.5rem;
+        grid-template-columns: auto 1fr;
+        grid-template-rows: 1fr 1fr;
+      }
+      sl-button-bar {
+        grid-column: 1 / -1;
+      }
+    </style>
+    <sl-button id="button">Toggle popover</sl-button>
+    <sl-popover anchor="button">
+      <form>
+        <label>Label</label>
+        <sl-input autofocus placeholder="Input"></sl-input>
+        <sl-button-bar align="end">
+          <sl-button size="sm" type="submit">Save</sl-button>
+        </sl-button-bar>
+      </form>
+    </sl-popover>
   `
 };

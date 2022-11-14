06edd7ba9b0f24be77ad70784bd790b5f4139f89
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-11-14 16:49:59 +0100

    🛩

--- docs/storybook/.storybook/main.cjs
+++ docs/storybook/.storybook/main.cjs
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ['../dist/components/**/*.stories.js'],
+  stories: [
+    '../stories/**/*.stories.mdx', 
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
   addons: [
     '../../../tools/storybook-addon-themes',
     '@storybook/addon-a11y',
@@ -8,8 +11,7 @@ module.exports = {
     {
       name: '@storybook/addon-essentials',
       options: {
-        backgrounds: false,
-        docs: false
+        backgrounds: false
       }
     },
     '@storybook/addon-storysource'
@@ -20,6 +22,5 @@ module.exports = {
   },
   core: {
     disableTelemetry: true
-  },
-  staticDirs: [{ from: '../../tokens/src/themes', to: '/themes' }]
+  }
 };

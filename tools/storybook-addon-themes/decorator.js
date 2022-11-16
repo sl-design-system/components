78fd95d7345a6adf2a1be0adacb3b0b89fb1f87b
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-11-16 13:31:28 +0100

    🎇

--- tools/storybook-addon-themes/decorator.js
+++ tools/storybook-addon-themes/decorator.js
@@ -2,7 +2,7 @@ import { useEffect } from '@storybook/addons';
 import { updateTheme } from './theme.js';
 
 export const withTheme = (StoryFn, context) => {
-  const { themes, selectedTheme } = context.globals;
+  let { themes, selectedTheme } = context.globals;
 
   useEffect(() => {
     selectedTheme ??= themes[0].id;

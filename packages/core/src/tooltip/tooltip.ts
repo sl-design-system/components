d5fda0502beaf38f7061d9038d64863c255eb676
Jeroen Zwartepoorte <jeroen.zwartepoorte@sanoma.com>
2022-11-16 09:43:47 +0100

    Add popover components (#84)

---
+++ packages/core/src/tooltip/tooltip.ts
@@ -0,0 +1,80 @@
+import type { Placement } from '../popover/utils/position-anchored-element.js';
+import type { CSSResultGroup, TemplateResult } from 'lit';
+import { LitElement, html } from 'lit';
+import { property } from 'lit/decorators.js';
+import { AnchoredPopoverMixin } from '../popover/mixins/anchored-popover.js';
+import { popoverMixinStyles } from '../popover/mixins/popover.js';
+import { EventsController } from '../utils/controllers/events.js';
+import styles from './tooltip.scss.js';
+
+let nextUniqueId = 0;
+
+/**
+ * Tooltip component.
+ */
+export class Tooltip extends AnchoredPopoverMixin(LitElement) {
+  /** @private */
+  static override styles: CSSResultGroup = [popoverMixinStyles, styles];
+
+  static lazy(target: Element, callback: (target: Tooltip) => void): void {
+    const createTooltip = (): void => {
+      const tooltip = document.createElement('sl-tooltip');
+      tooltip.id = `sl-tooltip-${nextUniqueId++}`;
+
+      callback(tooltip);
+
+      target.parentNode?.insertBefore(tooltip, target.nextSibling);
+      target.setAttribute('aria-describedby', tooltip.id);
+
+      tooltip.anchorElement = target as HTMLElement;
+      tooltip.showPopover();
+
+      // We only need to create the tooltip once, so ignore all future events.
+      ['focusin', 'pointerover'].forEach(eventName => target.removeEventListener(eventName, createTooltip));
+    };
+
+    ['focusin', 'pointerover'].forEach(eventName => target.addEventListener(eventName, createTooltip));
+  }
+
+  #events = new EventsController(this);
+
+  #matchesAnchor = (element: Element): boolean => {
+    return !!this.id && element.nodeType === Node.ELEMENT_NODE && this.id === element.getAttribute('aria-describedby');
+  };
+
+  #onHide = ({ target }: Event): void => {
+    if (this.#matchesAnchor(target as Element)) {
+      this.anchorElement = undefined;
+      this.hidePopover();
+    }
+  };
+
+  #onShow = ({ target }: Event): void => {
+    if (this.#matchesAnchor(target as HTMLElement)) {
+      this.anchorElement = target as HTMLElement;
+      this.showPopover();
+    }
+  };
+
+  /** Tooltip placement. */
+  @property() placement: Placement = 'bottom';
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    this.setAttribute('popover', 'manual');
+    this.setAttribute('role', 'tooltip');
+
+    const root = this.getRootNode();
+
+    this.#events.listen(root, 'click', this.#onHide, { capture: true });
+    this.#events.listen(root, 'focusin', this.#onShow);
+    this.#events.listen(root, 'focusout', this.#onHide);
+    this.#events.listen(root, 'pointerover', this.#onShow);
+    this.#events.listen(root, 'pointerout', this.#onHide);
+  }
+
+  override render(): TemplateResult {
+    return html`<slot></slot>`;
+  }
+}

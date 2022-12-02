ecdd10eb4b8e9ae71cbeaab1b76146fdce117545
anna-lach <a.lach@nowaera.pl>
2022-12-02 16:28:24 +0100

    (website/navigation): navigation changes without styles

--- docs/website/src/site/_includes/navigation.js
+++ docs/website/src/site/_includes/navigation.js
@@ -1,7 +1,39 @@
+const sidenav = document.querySelector('.ds-sidebar');
+
+function reportWindowSize() {
+  const widthOutput = window.innerWidth;
+  // console.log(widthOutput);
+
+  if (widthOutput > 580 && sidenav.classList.contains('ds-sidebar--opened')) {
+    hideMenu();
+  }
+}
+
+// window.addEventListener('hashchange', function () {
+//   console.log('onhashchange event occurred!');
+// });
+
+window.onresize = reportWindowSize;
+
+// window.addEventListener('resize', reportWindowSize);
+
+sidenav.addEventListener('keyup', e => {
+  console.log('e', e, sidenav);
+  if (e.code === 'Escape') {
+    if (sidenav.classList.contains('ds-sidebar--opened')) {
+      hideMenu();
+    }
+  }
+});
+
 function showMenu() {
   const topNavigation = document.querySelector('.ds-top-navigation');
   // const hamburger = document.querySelector('.ds-top-navigation__hamburger');
   const menu = document.querySelector('.ds-sidebar');
+  const closeButton = document.querySelector('.ds-sidebar-nav__close-button');
+  const menuButoon = document.querySelector('.ds-top-navigation__hamburger');
+
+  console.log('window.history', window.history, document.location);
 
   // hamburger.addEventListener('click', function (event) {
   // event.stopPropagation();
@@ -18,6 +50,7 @@ function showMenu() {
     // menu.style.transform = 'translateX(0)';
     // menu.style.left = '0';
     topNavigation.style.opacity = '0';
+    closeButton.focus();
   } else {
     // menu.style.display = 'none';
     // menu.style.transition = 'transform 300ms';
@@ -26,6 +59,7 @@ function showMenu() {
     // menu.style.transform = 'translateX(-300px)';
     // menu.style.left = '-300px';
     topNavigation.style.opacity = '1';
+    menuButoon.focus();
   }
   // });
 }
@@ -34,6 +68,7 @@ function hideMenu() {
   const topNavigation = document.querySelector('.ds-top-navigation');
   // const hamburger = document.querySelector('.ds-top-navigation__hamburger');
   const menu = document.querySelector('.ds-sidebar');
+  const menuButoon = document.querySelector('.ds-top-navigation__hamburger');
 
   // hamburger.addEventListener('click', function (event) {
   // event.stopPropagation();
@@ -54,6 +89,7 @@ function hideMenu() {
   // menu.style.transform = 'translateX(-300px)';
   // menu.style.left = '-300px';
   topNavigation.style.opacity = '1';
+  menuButoon.focus();
   // }
   // });
 }

718ef2a94973a85118cf39a3bc2ff827af869ccd
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2022-12-04 13:55:06 +0100

    🌷

--- packages/core/src/utils/controllers/selection.ts
+++ packages/core/src/utils/controllers/selection.ts
@@ -46,8 +46,6 @@ export class SelectionController<T> {
   }
 
   selectAll(): void {
-    console.log('selectAll()');
-
     this.#selectAll = true;
     this.#selection.clear();
     this.#host.requestUpdate();

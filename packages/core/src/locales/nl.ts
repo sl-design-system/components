8bfb8743188d6a1416523880c5aa726792f4cdf7
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-01-15 12:53:58 +0100

    🌈

---
+++ packages/core/src/locales/nl.ts
@@ -0,0 +1,13 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
+
+import { str } from '@lit/localize';
+
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export const templates = {
+  s1e4177e8de76dfc9: `Dit veld volgt niet het gespecificeerde patroon.`,
+  s3ee922cc6e82d53a: `Dit veld moet verplicht ingevuld worden.`,
+  s45d6e41483a83f82: str`Dit veld moet tenminste ${0} karakter${1} lang zijn.`
+};

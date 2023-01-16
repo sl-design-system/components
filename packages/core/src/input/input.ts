e513ba4e2c3999c2b5d22c3e2498eb210c5a31dd
Jeroen Zwartepoorte <jeroen.zwartepoorte@sanoma.com>
2023-01-16 15:13:09 +0100

    Fix focus issues in form controls (#159)

--- packages/core/src/input/input.ts
+++ packages/core/src/input/input.ts
@@ -149,9 +149,11 @@ export class Input extends FormControlMixin(HintMixin(LitElement)) {
   }
 
   #onClick(event: Event): void {
-    event.preventDefault();
+    if (event.target === this.input) {
+      event.preventDefault();
 
-    this.input.focus();
+      this.input.focus();
+    }
   }
 
   #onInput({ target }: Event & { target: HTMLInputElement }): void {

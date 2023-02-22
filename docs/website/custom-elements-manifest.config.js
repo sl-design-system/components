4d0b3e092b8fa2e05d02c1e90144b8190ec5cfe0
anna-lach <a.lach@nowaera.pl>
2023-02-22 13:43:38 +0100

    configuration changes, clear unnecessary elements

--- docs/website/custom-elements-manifest.config.js
+++ docs/website/custom-elements-manifest.config.js
@@ -4,37 +4,17 @@ export default {
     './../../packages/core/src/**/*.ts',
     './../../packages/editor/src/**/*.ts',
     './../../packages/grid/src/**/*.ts'
-  ], // TODO: what about editor and grid components?
-  /** Globs to exclude */
-  // exclude: ['src/foo.js'],
+  ],
   /** Directory to output CEM to */
-  outdir: 'src/site/_data/custom-elements', //'dist/site/test', // TODO: different for prod and dev
+  outdir: 'src/site/_data/custom-elements',
   /** Run in dev mode, provides extra logging */
   dev: true,
   /** Run in watch mode, runs on file changes */
-  watch: true,
+  watch: false,
   /** Include third party custom elements manifests */
   dependencies: true,
   /** Output CEM path to `package.json`, defaults to true */
   packagejson: false,
   /** Enable special handling for litelement */
-  litelement: true,
-  /** Enable special handling for catalyst */
-  catalyst: false,
-  /** Enable special handling for fast */
-  fast: false,
-  /** Enable special handling for stencil */
-  stencil: false,
-  /** Provide custom plugins */
-  plugins: [
-    //myAwesomePlugin()
-  ],
-
-  /** Overrides default module creation: */
-/*  overrideModuleCreation: ({ts, globs}) => {
-    const program = ts.createProgram(globs, defaultCompilerOptions);
-    const typeChecker = program.getTypeChecker();
-
-    return program.getSourceFiles().filter(sf => globs.find(glob => sf.fileName.includes(glob)));
-  },*/
+  litelement: true
 }

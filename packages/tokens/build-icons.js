da165f4665e37e9f769290a409bd4dff51a012ed
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-03-07 13:25:04 +0100

    👒

--- packages/tokens/build-icons.js
+++ packages/tokens/build-icons.js
@@ -4,6 +4,7 @@ const cwd = new URL('.', import.meta.url).pathname,
   name = process.argv.at(2),
   page = process.argv.at(3);
 
+// 1. Get icon tokens from `base.json`
 const {
   default: { icon }
 } = await import(`${cwd}/src/themes/${name}/base.json`, { assert: { type: 'json' } });
@@ -16,6 +17,7 @@ const icons = Object.entries(icon).reduce((acc, cur) => {
   return acc;
 }, {});
 
+// 2. If tokens contain custom icons, get icons from Figma
 if (Object.keys(icons).length) {
   await new Promise((resolve, reject) => {
     exec(`yarn run figma-export use-config .figmaexportrc.cjs ${page}`, { cwd }, error => {
@@ -29,6 +31,14 @@ if (Object.keys(icons).length) {
   });
 }
 
+// 3. Convert downloaded icons to appropriate format?
+// We only need the `<path>` data for `<sl-icon>`
+
+// 4. Write the output to `icons.json`???? Or just `icons.ts` which exports 
+// all the icons?????
+
+// 5. Expose the icons via the theme `IconResolver` in `index.ts`
+// Either use the downloaded icons, or use FontAwesome NPM packages
 
 Object.entries(icons).forEach(([name, value]) => {
   console.log({ name, value });
--- packages/tokens/src/themes/sanoma-learning/index.ts
+++ packages/tokens/src/themes/sanoma-learning/index.ts
@@ -1,9 +1,13 @@
 import { Icon } from '@sanomalearning/slds-core/icon';
 import { faStar } from '@fortawesome/pro-regular-svg-icons';
 
+// 1. Register a custom IconResolver for the theme icons.
 Icon.registerResolver(name => {
   console.log('resolver', { name });
 
+  // 2. Get the supported icons from `icons.[json|ts]`?
+
+  // 3. Return the matching `<path>`
   if (name === 'star') {
     const {
         icon: [width, height, , , path]
@@ -19,6 +23,11 @@ Icon.registerResolver(name => {
   return 'Hello world';
 });
 
+// 4. Have `<sl-icon>` render a nice `<svg>` with the path(s)
+
 export const setup = (): void => {
-  console.log('SETUP!');
+  // 1. Add typekit url to `<head>`
+  // 2. Add Magister icons
+  // 3. Load the base stylesheet/css variables
+  // 4. Start using the SLDS in your application
 };

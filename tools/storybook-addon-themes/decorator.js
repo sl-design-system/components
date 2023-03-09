43c3e346c7d6439ff422d87ca989bad150b89c49
Diana Broeders <diana.broeders@sanoma.com>
2023-03-09 10:05:42 +0100

    add themes, remove themes, add mode toggle to storybook

--- tools/storybook-addon-themes/decorator.js
+++ tools/storybook-addon-themes/decorator.js
@@ -3,10 +3,10 @@ import { ADDON_ID } from './constants.js';
 import { updateTheme } from './theme.js';
 
 export const withTheme = (StoryFn, context) => {
-  let { themes, selectedTheme } = context.globals,
+  let { themes, selectedTheme, mode } = context.globals,
     [state] = useAddonState(ADDON_ID, { selected: selectedTheme });
 
-  updateTheme(themes.find(t => t.id === state.selected));
+  updateTheme(themes.find(t => t.id === state.selected), mode);
 
   return StoryFn();
 };
--- tools/storybook-addon-themes/preview.js
+++ tools/storybook-addon-themes/preview.js
@@ -3,29 +3,72 @@ import { withTheme } from './decorator.js';
 import { updateTheme } from './theme.js';
 
 export const decorators = [withTheme];
+export const globalTypes = {
+  mode: {
+    default: 'light',
+    name: '🌗 Mode:',
+    toolbar:{
+        // Array of plain string values or MenuItem shape (see below)
+        items: ['light', 'dark'],
+        // Change title based on selected value
+        dynamicTitle: true
+    }
+  }
+};
 export const globals = {
   themes: [
     {
-      id: 'bingel/light',
-      name: 'Bingel (light)',
+      id: 'bingel-dc',
+      name: 'Bingel DC',
       color: '#0074df',
       fonts: ['https://use.typekit.net/xrr8gdw.css']
     },
     {
-      id: 'magister/light',
-      name: 'Magister (light)',
-      color: '#1f97f9',
-      fonts: ['https://use.typekit.net/zkq0zzv.css']
+      id: 'bingel-int',
+      name: 'Bingel INT',
+      color: '#0074df',
+      fonts: ['https://use.typekit.net/xrr8gdw.css']
     },
     {
-      id: 'max-online/light',
-      name: 'MAX Online (light)',
+      id: 'clickedu',
+      name: 'Clickedu',
+      color: '#0074df',
+      fonts: ['https://use.typekit.net/xrr8gdw.css']
+    },
+    {
+      id: 'itslearning',
+      name: 'itslearning',
+      color: '#0074df',
+      fonts: ['https://use.typekit.net/xrr8gdw.css']
+    },
+    {
+      id: 'kampus',
+      name: 'Kampus',
+      color: '#0074df',
+      fonts: ['https://use.typekit.net/xrr8gdw.css']
+    },
+    {
+      id: 'magister',
+      name: 'Magister',
+      color: '#1f97f9',
+      fonts: ['https://use.typekit.net/zkq0zzv.css',
+      '/themes/sanoma-learning/fonts.css']
+    },
+    {
+      id: 'max',
+      name: 'MAX Online',
       color: '#253646',
       fonts: ['https://use.typekit.net/doq6twb.css']
     },
     {
-      id: 'sanoma-learning/light',
-      name: 'Sanoma Learning (light)',
+      id: 'neon',
+      name: 'NEON',
+      color: '#1E2922',
+      fonts: ['https://use.typekit.net/kes1hoh.css']
+    },
+    {
+      id: 'sanoma-learning',
+      name: 'Sanoma Learning',
       color: '#56CC8A',
       fonts: [
         'https://use.typekit.net/kes1hoh.css',
@@ -33,17 +76,9 @@ export const globals = {
       ],
       setup
     },
-    {
-      id: 'sanoma-learning/dark',
-      name: 'Sanoma Learning (dark)',
-      color: '#1E2922',
-      fonts: [
-        'https://use.typekit.net/kes1hoh.css',
-        '/themes/sanoma-learning/fonts.css'
-      ]
-    }
   ],
-  selectedTheme: 'sanoma-learning/light'
+  selectedTheme: 'sanoma-learning'
 };
 
 updateTheme(globals.themes.find(t => t.id === globals.selectedTheme));
+console.log(globals);

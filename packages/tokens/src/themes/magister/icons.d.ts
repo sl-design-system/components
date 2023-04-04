a40cb883d520e1940daafe8f9b24a608c6f86508
Diaan <Diaan@users.noreply.github.com>
2023-04-04 11:55:57 +0000

    ⚙️ Generate theme tokens

--- packages/tokens/src/themes/magister/icons.d.ts
+++ packages/tokens/src/themes/magister/icons.d.ts
@@ -1,41 +1,44 @@
 export declare const icons: {
-    'fa-face_smile': {
-        value: string;
-        type: string;
-        description: string;
-    };
-    'fa-menu': {
-        value: string;
-        type: string;
-        description: string;
-    };
-    'fa-fav': {
-        value: string;
-        type: string;
-        description: string;
-    };
-    'fa-chevron-down': {
+    "fa-face_smile": {
         value: string;
         type: string;
         description: string;
         svg: string;
     };
-    'fa-check': {
+    "fa-menu": {
+        value: string;
+        type: string;
+        description: string;
+        svg: string;
+    };
+    "fa-fav": {
+        value: string;
+        type: string;
+        description: string;
+        svg: string;
+    };
+    "fa-chevron-down": {
+        value: string;
+        type: string;
+        description: string;
+        svg: string;
+    };
+    "fa-check": {
         value: string;
         type: string;
         description: string;
         svg: string;
     };
     fav: {
-        value: string;
-        type: string;
-        description: string;
         svg: string;
     };
-    'open-eye': {
-        value: string;
-        type: string;
-        description: string;
+    "open-eye": {
+        svg: string;
+    };
+    "face-smile": {
+        svg: string;
+    };
+    hello: {
         svg: string;
     };
 };
--- packages/tokens/src/themes/sanoma-learning/icons.d.ts
+++ packages/tokens/src/themes/sanoma-learning/icons.d.ts
@@ -1,24 +1,39 @@
 export declare const icons: {
-    "face-smile": {
-        svg: string;
-    };
-    menu: {
+    "fa-face_smile": {
         value: string;
         type: string;
         description: string;
+        svg: string;
+    };
+    "fa-menu": {
+        value: string;
+        type: string;
+        description: string;
+        svg: string;
+    };
+    "fa-fav": {
+        value: string;
+        type: string;
+        description: string;
+        svg: string;
+    };
+    "fa-chevron-down": {
+        value: string;
+        type: string;
+        description: string;
+        svg: string;
+    };
+    "fa-check": {
+        value: string;
+        type: string;
+        description: string;
+        svg: string;
     };
     fav: {
         svg: string;
     };
-    "chevron-down": {
-        value: string;
-        type: string;
-        description: string;
-    };
-    check: {
-        value: string;
-        type: string;
-        description: string;
+    "face-smile": {
+        svg: string;
     };
     hello: {
         svg: string;

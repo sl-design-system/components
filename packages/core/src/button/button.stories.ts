e4324639c37ce3bd55c0f88b49a63e4bcb19e00a
Diana Broeders <diana.broeders@sanoma.com>
2023-04-12 13:22:58 +0200

    fix face-smile name, change icon in button story

--- packages/core/src/button/button.stories.ts
+++ packages/core/src/button/button.stories.ts
@@ -56,8 +56,8 @@ export const All: Story = {
     const fills = ['default', 'outline', 'link'];
     const sizes = ['sm', 'md', 'lg'];
     const disabledStates = [false, true];
-    const startIcon = icon === 'start' ? html`<sl-icon name="check"></sl-icon>` : '';
-    const endIcon = icon === 'end' ? html`<sl-icon name="star"></sl-icon>` : '';
+    const startIcon = icon === 'start' ? html`<sl-icon name="face-smile"></sl-icon>` : '';
+    const endIcon = icon === 'end' ? html`<sl-icon name="face-smile"></sl-icon>` : '';
     return html` <style>
         .grid {
           display: inline-grid;
@@ -72,10 +72,10 @@ export const All: Story = {
             disabledStates.map(
               (disabledState: boolean) => html`
                 <sl-button .fill=${fill} .size=${size} ?disabled=${disabledState}>
-                  <sl-icon name="check"></sl-icon>
+                  <sl-icon name="face-smile"></sl-icon>
                 </sl-button>
                 <sl-button .fill=${fill} .size=${size} ?disabled=${disabledState} variant="primary">
-                  <sl-icon name="check"></sl-icon>
+                  <sl-icon name="face-smile"></sl-icon>
                 </sl-button>
                 <sl-button .fill=${fill} .size=${size} ?disabled=${disabledState}>
                   ${startIcon}Default${endIcon}

740ecaff75dcd441da4796d6356d969d813994d5
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-04-14 10:27:10 +0200

    🚐

--- packages/tokens/src/themes/kampus/index.ts
+++ packages/tokens/src/themes/kampus/index.ts
@@ -8,6 +8,4 @@ import { icons } from './icons.js';
  */
 export const setup = (): void => {
   Icon.registerIcons(icons as IconLibrary);
-
-  console.log('SETUP Kampus in index.ts!');
 };
--- packages/tokens/src/themes/sanoma-learning/index.ts
+++ packages/tokens/src/themes/sanoma-learning/index.ts
@@ -15,6 +15,4 @@ import { icons } from './icons.js';
  */
 export const setup = (): void => {
   Icon.registerIcons(icons as IconLibrary);
-
-  console.log('SETUP Sanoma Learning in index.ts!');
 };

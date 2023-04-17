7b6c9201e0666289c32e623ff5ac0f9b815a1166
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-04-17 19:11:59 +0200

    🐒

--- packages/editor/register.ts
+++ packages/editor/register.ts
@@ -1,4 +1,4 @@
-import { Editor } from './editor.js';
+import { Editor } from './src/editor.js';
 
 customElements.define('sl-editor', Editor);
 
--- packages/editor/src/editor.stories.ts
+++ packages/editor/src/editor.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
-import './register.js';
+import '../register.js';
 
 interface Props {
   value: string;

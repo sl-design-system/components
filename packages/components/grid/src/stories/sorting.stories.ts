155195ef3d6531d991346d6aaff685ca64be210a
Jeroen Zwartepoorte <jeroen.zwartepoorte@sanoma.com>
2023-05-01 12:54:19 +0200

    Fix scoped elements with sortable column (#384)

--- packages/components/grid/src/stories/sorting.stories.ts
+++ packages/components/grid/src/stories/sorting.stories.ts
@@ -1,4 +1,7 @@
+import type { TemplateResult } from 'lit';
 import type { StoryObj } from '@storybook/web-components';
+import type { Person } from '@sl-design-system/example-data';
+import { Button } from '@sl-design-system/button';
 import { getPeople } from '@sl-design-system/example-data';
 import { html } from 'lit';
 import '../../register.js';
@@ -16,6 +19,27 @@ export const Single: Story = {
       <sl-grid .items=${people}>
         <sl-grid-sort-column path="firstName"></sl-grid-sort-column>
         <sl-grid-sort-column path="lastName"></sl-grid-sort-column>
+        <sl-grid-sort-column path="email"></sl-grid-sort-column>
+      </sl-grid>
+    `;
+  }
+};
+
+export const ScopedElements: Story = {
+  loaders: [async () => ({ people: (await getPeople()).people })],
+  render: (_, { loaded: { people } }) => {
+    const renderer = ({ firstName, lastName }: Person): TemplateResult => {
+      return html`<sl-button>${firstName} ${lastName}</sl-button>`;
+    };
+
+    return html`
+      <sl-grid .items=${people}>
+        <sl-grid-sort-column
+          header="User"
+          path="firstName"
+          .renderer=${renderer}
+          .scopedElements=${{ 'sl-button': Button }}
+        ></sl-grid-sort-column>
         <sl-grid-sort-column path="email"></sl-grid-sort-column>
       </sl-grid>
     `;

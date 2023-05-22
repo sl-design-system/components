fce8d0f192ac41ca62b81903ac4bdc50df70d802
arecuenco-dsgn <arecuenco-dsgn@users.noreply.github.com>
2023-05-22 07:36:12 +0000

    ⚙️ Generate theme tokens

--- packages/themes/bingel-dc/icons.ts
+++ packages/themes/bingel-dc/icons.ts
@@ -59,10 +59,16 @@ export const icons = {
   'sort-up': {
     svg: '<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><g><path d="M8.688 1.313a.964.964 0 0 0-1.407 0l-4 4a1.01 1.01 0 0 0-.218 1.093c.156.375.53.625.937.625h8c.375 0 .75-.25.906-.625a1.01 1.01 0 0 0-.219-1.093l-4-4ZM8 13.313 5.188 10.5h5.593L8 13.313Zm-.719 1.406a.964.964 0 0 0 1.407 0l4-4c.28-.281.374-.719.218-1.094A.981.981 0 0 0 12 9H4c-.406 0-.781.25-.937.625a1.01 1.01 0 0 0 .218 1.094l4 4Z"/></g></svg>'
   },
-  triangleExclamation: {
+  'triangle-exclamation': {
     value: 'triangle-exclamation',
     type: 'Icons',
-    description: 'icon.core.triangleExclamation',
+    description: 'icon.core.triangle-exclamation',
     svg: '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M248.4 84.3c1.6-2.7 4.5-4.3 7.6-4.3s6 1.6 7.6 4.3L461.9 410c1.4 2.3 2.1 4.9 2.1 7.5c0 8-6.5 14.5-14.5 14.5H62.5c-8 0-14.5-6.5-14.5-14.5c0-2.7 .7-5.3 2.1-7.5L248.4 84.3zm-41-25L9.1 385c-6 9.8-9.1 21-9.1 32.5C0 452 28 480 62.5 480h387c34.5 0 62.5-28 62.5-62.5c0-11.5-3.2-22.7-9.1-32.5L304.6 59.3C294.3 42.4 275.9 32 256 32s-38.3 10.4-48.6 27.3zM288 368a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm-8-184c0-13.3-10.7-24-24-24s-24 10.7-24 24v96c0 13.3 10.7 24 24 24s24-10.7 24-24V184z" fill="var(--fill-default)"></path></svg>'
+  },
+  'triangle-exclamation-solid': {
+    value: 'triangle-exclamation',
+    type: 'Icons',
+    description: 'icon.core.triangle-exclamation-solid',
+    svg: '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" fill="var(--fill-default)"></path></svg>'
   }
 };
--- packages/themes/clickedu/icons.ts
+++ packages/themes/clickedu/icons.ts
@@ -72,10 +72,16 @@ export const icons = {
   star: {
     svg: '<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.493 5.107 8.616.896H7.384l-1.792 4.21-4.567.488-.373 1.174 3.6 3.037-1.193 4.554 1.002.745 3.925-2.442 3.924 2.442 1.003-.745-1.217-4.44 3.652-3.15-.373-1.175-4.482-.487Zm-2.507-1.63L6.86 6.317l-.444.322-3.1.272 2.487 2.001.173.544-.856 3.039 2.594-1.662h.568l2.595 1.662-.901-2.94.172-.544 2.507-2.1-3-.272-.445-.321-1.224-2.841Z"/></svg>'
   },
-  triangleExclamation: {
+  'triangle-exclamation': {
     value: 'triangle-exclamation',
     type: 'Icons',
-    description: 'icon.core.triangleExclamation',
+    description: 'icon.core.triangle-exclamation',
     svg: '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M248.4 84.3c1.6-2.7 4.5-4.3 7.6-4.3s6 1.6 7.6 4.3L461.9 410c1.4 2.3 2.1 4.9 2.1 7.5c0 8-6.5 14.5-14.5 14.5H62.5c-8 0-14.5-6.5-14.5-14.5c0-2.7 .7-5.3 2.1-7.5L248.4 84.3zm-41-25L9.1 385c-6 9.8-9.1 21-9.1 32.5C0 452 28 480 62.5 480h387c34.5 0 62.5-28 62.5-62.5c0-11.5-3.2-22.7-9.1-32.5L304.6 59.3C294.3 42.4 275.9 32 256 32s-38.3 10.4-48.6 27.3zM288 368a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm-8-184c0-13.3-10.7-24-24-24s-24 10.7-24 24v96c0 13.3 10.7 24 24 24s24-10.7 24-24V184z" fill="var(--fill-default)"></path></svg>'
+  },
+  'triangle-exclamation-solid': {
+    value: 'triangle-exclamation',
+    type: 'Icons',
+    description: 'icon.core.triangle-exclamation-solid',
+    svg: '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" fill="var(--fill-default)"></path></svg>'
   }
 };

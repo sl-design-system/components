916ed44daa67bad8ed978fa93d5e46b7ff068ce4
Diana Broeders <diana.broeders@sanoma.com>
2023-05-25 16:14:02 +0200

    Got theming working??? 🤞✊

--- chromatic/.storybook/stories/magister.stories.ts
+++ chromatic/.storybook/stories/magister.stories.ts
@@ -7,13 +7,24 @@ import '@sl-design-system/label/register.js';
 import '@sl-design-system/radio-group/register.js';
 import '@sl-design-system/textarea/register.js';
 import { All as AllButton} from "../../../packages/components/button/src/button.stories";
+import { All as AllCheckbox} from "../../../packages/components/checkbox/src/checkbox.stories";
 import { All as AllRadioGroup} from "../../../packages/components/radio-group/src/radio-group.stories";
-import { updateTheme } from '../themes';
+import { html } from 'lit';
+import { themes } from '../themes';
 
-updateTheme('magister','light');
 
 export default {
   title: 'Magister',
+  decorators: [
+    (Story) => {
+      const fonts = themes.find(theme => theme.id==='magister')?.fonts || [];
+      return html`
+        <link href="/themes/magister/base.css" rel="stylesheet">
+        <link href="/themes/magister/light.css" rel="stylesheet">
+        ${fonts.map(font => html`<link href="${font}" rel="stylesheet">`)}
+        ${Story()}`
+    },
+  ],
   args: {
     text: 'Button',
     icon: 'none',
@@ -24,4 +35,5 @@ export default {
 };
 
 export const Buttons = AllButton;
+export const Checkbox = AllCheckbox;
 export const RadioGroup = AllRadioGroup;
--- chromatic/.storybook/stories/max.stories.ts
+++ chromatic/.storybook/stories/max.stories.ts
@@ -6,25 +6,34 @@ import '@sl-design-system/text-input/register.js';
 import '@sl-design-system/label/register.js';
 import '@sl-design-system/radio-group/register.js';
 import '@sl-design-system/textarea/register.js';
-import {MessageSize} from "@sl-design-system/shared";
 import { All as AllButton} from "../../../packages/components/button/src/button.stories";
+import { All as AllCheckbox} from "../../../packages/components/checkbox/src/checkbox.stories";
 import { All as AllRadioGroup} from "../../../packages/components/radio-group/src/radio-group.stories";
-import { updateTheme } from '../themes';
-
-
-const sizes: MessageSize[] = ['sm', 'md', 'lg'];
-updateTheme('max','light');
+import { html } from 'lit';
+import { themes } from '../themes';
 
 export default {
   title: 'Max',
+  decorators: [
+    (Story) => {
+      const fonts = themes.find(theme => theme.id==='max')?.fonts || [];
+      return html`
+        <link href="/themes/max/base.css" rel="stylesheet">
+        <link href="/themes/max/light.css" rel="stylesheet">
+        ${fonts.map(font => html`<link href="${font}" rel="stylesheet">`)}
+        ${Story()}`
+    },
+  ],
   args: {
     text: 'Button',
     icon: 'none',
     size: 'md',
     fill: 'default',
     variant: 'default'
-  },
+  }
 };
 
-export const Buttons = AllButton;
+
+export const Button = AllButton;
+export const Checkbox = AllCheckbox;
 export const RadioGroup = AllRadioGroup;

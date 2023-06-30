5d5925a79152d82ebfaa50f1ad5a60d207a5a084
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-06-30 11:48:52 +0200

    🎡

--- packages/components/shared/src/css.ts
+++ packages/components/shared/src/css.ts
@@ -1,8 +1,2 @@
-export const supportsTopLayer = CSS.supports('selector(:popover-open)');
-
-export const supportsHas = CSS.supports('selector(:has())');
-
-export const supportsAnchor = false; // CSS.supports('anchor-name: --test');
-
 export const firstFocusableSelector =
   'button:not([tabindex="-1"]), [href]:not([tabindex="-1"]), input:not([tabindex="-1"]), select:not([tabindex="-1"]), textarea:not([tabindex="-1"]), [tabindex]:not([tabindex="-1"]), [focusable]:not([tabindex="-1"])';

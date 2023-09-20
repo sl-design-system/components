708dbf93fd8d41ef4e4acfb6cac3da9a1be22208
Diana Broeders <diana.broeders@sanoma.com>
2023-09-20 09:06:57 +0200

    fix build?

--- packages/components/tooltip/src/tooltip.spec.ts
+++ packages/components/tooltip/src/tooltip.spec.ts
@@ -3,18 +3,18 @@ import '@sl-design-system/button/register.js';
 import {html} from 'lit';
 import '../register.js';
 import { Tooltip } from './tooltip.js';
-import { Button } from 'packages/components/button/src/button.js';
+import type { Button } from '@sl-design-system/button';
 
 
 describe('sl-tooltip', () => {
   let el: HTMLElement;
-  let button: Button | null;
-  let tooltip: Tooltip | null;
+  let button: Button;
+  let tooltip: Tooltip;
   
   const showTooltip = async () => {
     const focusinEvent = new Event('pointerover',{bubbles:true});
     button?.dispatchEvent(focusinEvent);
-    await el.updateComplete;
+    await tooltip.updateComplete;
     return new Promise(resolve => setTimeout(resolve));
   }
    
@@ -26,49 +26,49 @@ describe('sl-tooltip', () => {
           <sl-tooltip id="tooltip">Message</sl-tooltip>
         </div>
       `);
-      button = el.querySelector<Button>('sl-button');
-      tooltip = el.querySelector<Tooltip>('sl-tooltip');
+      button = el.querySelector('sl-button') as Button;
+      tooltip = el.querySelector('sl-tooltip') as Tooltip;
     });
     it('should not show the tooltip by default', () => {
-      expect(tooltip?.matches(':popover-open')).to.be.false;
+      expect(tooltip.matches(':popover-open')).to.be.false;
     });
 
     it('should toggle the tooltip on focusin and focusout', async () => {
       const focusinEvent = new Event('focusin',{bubbles:true});
       button?.dispatchEvent(focusinEvent);
-      expect(tooltip?.matches(':popover-open')).to.be.true;
+      expect(tooltip.matches(':popover-open')).to.be.true;
 
       
       const focusoutEvent = new Event('focusout',{bubbles:true});
       button?.dispatchEvent(focusoutEvent);
-      expect(tooltip?.matches(':popover-open')).to.be.false;
+      expect(tooltip.matches(':popover-open')).to.be.false;
     });
 
     it('should toggle the tooltip on pointerover and pointerout', async () => {
       const focusinEvent = new Event('pointerover',{bubbles:true});
       button?.dispatchEvent(focusinEvent);
-      expect(tooltip?.matches(':popover-open')).to.be.true;
+      expect(tooltip.matches(':popover-open')).to.be.true;
 
       
       const focusoutEvent = new Event('pointerout',{bubbles:true});
       button?.dispatchEvent(focusoutEvent);
-      expect(tooltip?.matches(':popover-open')).to.be.false;
+      expect(tooltip.matches(':popover-open')).to.be.false;
     });
 
     it('should be positioned at the top by default', async () => {
-      expect(tooltip?.position).to.equal('top');
+      expect(tooltip.position).to.equal('top');
     });
     
     it('should set the position to the position option chosen', async () => {
       tooltip.setAttribute('position', 'bottom');
       await tooltip.updateComplete;
 
-      expect(tooltip?.position).to.equal('bottom');
+      expect(tooltip.position).to.equal('bottom');
 
     });
 
     it('should not have a maxWidth by default', async () => {
-      tooltip.setAttribute('max-width', 150);
+      tooltip.setAttribute('max-width', '150');
       await tooltip.updateComplete;
 
       await showTooltip();

232b05ed6c5c731248bbd711869f957b25977b22
anna-lach <a.lach@nowaera.pl>
2023-10-03 16:57:06 +0200

    dialog styling changes, added border as well

--- packages/components/dialog/src/dialog.stories.ts
+++ packages/components/dialog/src/dialog.stories.ts
@@ -61,9 +61,7 @@ export const API: StoryObj = {
           Voluptate tempor amet cupidatat officia labore ipsum ad do. Dolore nulla ad magna nostrud cillum veniam sint
           et consectetur anim Lorem. Sint fugiat id deserunt magna et tempor veniam eu fugiat fugiat. Fugiat mollit sint
           labore adipisicing do mollit eu dolore nulla enim cillum. Pariatur amet occaecat dolor consectetur aliqua
-          mollit est aliquip irure cupidatat. Reprehenderit consectetur anim sunt voluptate dolor aute non enim aliqua
-          sit. Occaecat irure ullamco aliquip minim labore occaecat dolor magna duis. Voluptate tempor amet cupidatat
-          officia labore ipsum ad do.
+          mollit est aliquip irure cupidatat.
         </p>
         <sl-button slot="action" sl-dialog-close>Close</sl-button>
         <sl-button slot="action" fill="solid" variant="primary" sl-dialog-close autofocus>Action</sl-button>

d4e3d19cd62ed90516366836490b5cc2d90c4cfe
Diana Broeders <diana.broeders@sanoma.com>
2023-10-09 12:20:40 +0200

    added svg with css variables

--- website/src/site/ts/scripts/slds-components.ts
+++ website/src/site/ts/scripts/slds-components.ts
@@ -21,12 +21,12 @@ import '@sl-design-system/tooltip/register.js';
 import { Tab, TabGroup, TabPanel } from '@sl-design-system/tabs';
 import { setup } from '@sl-design-system/sanoma-learning';
 import { Icon } from '@sl-design-system/icon';
-import { faPenToSquare } from '@fortawesome/pro-regular-svg-icons';
+import { faBug, faMessagesQuestion, faPenToSquare } from '@fortawesome/pro-regular-svg-icons';
 import { faGithub, faSlack } from '@fortawesome/free-brands-svg-icons';
 
 setup();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-Icon.registerIcon(faPenToSquare, faGithub, faSlack);
+Icon.registerIcon(faPenToSquare, faBug, faGithub, faSlack, faMessagesQuestion);
 
 /** Workaround for the error:
  * Uncaught DOMException: "Failed to execute 'define' on 'CustomElementRegistry': the name "sl-tab-panel" has already been used with this registry"

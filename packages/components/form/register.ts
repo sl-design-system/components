582a9f02c99b0577bb7163215fef8d33a277e14e
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2023-10-30 11:03:32 +0100

    🎤

--- packages/components/form/register.ts
+++ packages/components/form/register.ts
@@ -1,11 +1,5 @@
-import { FormControlAdapter } from './src/adapter.js';
-import { TextInputAdapter } from './src/adapters/text-input-adapter.js';
 import { Error } from './src/error.js';
 import { Hint } from './src/hint.js';
-import { Label } from './src/label.js';
-
-FormControlAdapter.register(TextInputAdapter);
 
 customElements.define('sl-error', Error);
 customElements.define('sl-hint', Hint);
-customElements.define('sl-label', Label);

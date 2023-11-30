4c72026349f783c1ece8ff7d27f50e1a0dae7f53
Diana Broeders <diana.broeders@sanoma.com>
2023-11-30 08:42:24 +0100

    add options + fix token

--- packages/components/badge/src/badge.stories.ts
+++ packages/components/badge/src/badge.stories.ts
@@ -1,5 +1,7 @@
+import type { BadgeSize, BadgeVariant } from './badge.js';
 import type { Meta, StoryObj } from '@storybook/web-components';
-import '@sl-design-system/button/register.js';
+import { Icon } from '@sl-design-system/icon';
+import { faCheck, faGear } from '@fortawesome/pro-regular-svg-icons';
 import { html } from 'lit';
 import '../register.js';
 
@@ -7,11 +9,81 @@ interface Props {}
 
 type Story = StoryObj<Props>;
 
+const sizes: BadgeSize[] = ['sm', 'md', 'lg', 'xl', '2xl', '3xl'];
+const variants: BadgeVariant[] = ['neutral', 'primary', 'danger', 'success', 'warning', 'accent'];
+
+const sizeName = (size: string): string => {
+  switch (size) {
+    case 'sm':
+      return 'Small';
+    case 'md':
+      return 'Medium';
+    case 'lg':
+      return 'Large';
+    case 'xl':
+      return 'Extra Large';
+    case '2xl':
+      return '2 Extra Large';
+    case '3xl':
+      return '3 Extra Large';
+    case '4xl':
+      return '4 Extra Large';
+    default:
+      return 'Extra Small';
+  }
+};
+
 export default {
-  title: 'Button bar',
+  title: 'Badge',
   args: {},
   argTypes: {},
   render: () => html` <sl-badge> 99+ </sl-badge> `
 } satisfies Meta<Props>;
 
 export const Basic: Story = {};
+
+export const All: Story = {
+  render: () => {
+    // load a single icon:
+    Icon.registerIcon(faCheck, faGear);
+
+    return html`
+      <style>
+        table {
+          border-collapse: collapse;
+          margin-bottom: 24px;
+        }
+
+        th {
+          text-transform: capitalize;
+        }
+        th,
+        td {
+          padding: 4px 8px;
+        }
+      </style>
+      <table>
+        <thead>
+          <tr>
+            <th>Size</th>
+            ${variants.map(variant => html`<th>${variant}</th>`)}
+          </tr>
+        </thead>
+        <tbody>
+          ${sizes.map(
+            size => html` <tr>
+              <th>${sizeName(size)}</th>
+              ${variants.map(
+                variant => html`<td>
+                  <sl-badge .variant=${variant} .size=${size}><sl-icon name="far-check"></sl-icon></sl-badge>
+                  <sl-badge .variant=${variant} .size=${size}>99+</sl-badge>
+                  <sl-badge .variant=${variant} .size=${size}><sl-icon name="far-gear"></sl-icon> away</sl-badge>
+                </td>`
+              )}
+            </tr>`
+          )}
+        </tbody>
+      </table>
+    `;
+  }
+};
--- packages/components/badge/src/badge.ts
+++ packages/components/badge/src/badge.ts
@@ -1,9 +1,14 @@
 import type { CSSResultGroup, TemplateResult } from 'lit';
+import type { Icon } from '@sl-design-system/icon';
 import { LitElement, html } from 'lit';
+import { property, queryAssignedElements } from 'lit/decorators.js';
 import styles from './badge.scss.js';
 
+export type BadgeSize = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
+export type BadgeVariant = 'neutral' | 'primary' | 'danger' | 'success' | 'warning' | 'accent';
+
 /**
- * Show totals at a glance or group.
+ * Show totals at a glance or labels contents with a tag.
  *
  * ```html
  * <sl-badge> 99+ </sl-badge>
@@ -15,11 +20,17 @@ export class badge extends LitElement {
   /** @private */
   static override styles: CSSResultGroup = styles;
 
+  @property({ reflect: true }) size: BadgeSize = 'md';
+  @property({ reflect: true }) variant: BadgeVariant = 'neutral';
+
+  /** @private The slotted icons. */
+  @queryAssignedElements() icons?: Icon[];
+
   override render(): TemplateResult {
-    return html`<slot @slotchange=${this.#onSlotchange}></slot>`;
+    return this.size !== 'sm' ? html`<slot @slotchange=${this.#onSlotchange}></slot>` : html``;
   }
 
   async #onSlotchange(): Promise<void> {
-    console.log('change');
+    this.icons?.forEach(icon => (icon.size = 'xs'));
   }
 }

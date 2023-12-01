bf4c3ac08b64d158247a544915572d0e9615e9b3
anna-lach <a.lach@nowaera.pl>
2023-12-01 15:51:36 +0100

    inline message styling changes (with tokens), structure and story improvements

--- packages/components/inline-message/src/inline-message.stories.ts
+++ packages/components/inline-message/src/inline-message.stories.ts
@@ -20,15 +20,22 @@ export default {
 export const API: StoryObj = {
   args: {
     closingButton: true,
+    status: 'info',
     title: 'Dialog title',
     subtitle: 'Dialog subtitle',
     bodyContent: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac augue neque. Nunc sed ex ut neque lacinia rutrum nec vitae mi. Donec dictum urna elit, et feugiat nunc fringilla nec. Maecenas nisi lorem, facilisis nec libero ut, hendrerit ultricies orci. Vivamus massa ligula, ultricies quis odio a, scelerisque tincidunt lorem. Morbi quis pulvinar augue. Nunc eros magna, laoreet vitae ornare at, iaculis quis diam. Duis odio urna, viverra ut ex mattis, egestas tincidunt enim. Praesent ac ex tincidunt, hendrerit sem et, aliquam metus. Nunc quis nisi nulla. Sed nibh ante, posuere eu volutpat vitae, elementum ut leo. Ut aliquet tincidunt tellus, ut molestie urna ultrices in. Suspendisse potenti. Nunc non nunc eu nibh venenatis vestibulum. Maecenas rutrum nibh lacus.
     Fusce sodales purus ut arcu hendrerit, non interdum nulla suscipit. Duis vitae felis facilisis, eleifend ipsum ut, condimentum est. Nullam metus massa, venenatis vitae suscipit in, feugiat quis turpis. In pellentesque velit at sagittis mattis. Nam ut tellus elit. Proin luctus lectus velit, ut ultricies libero blandit blandit. Aenean molestie est ipsum, in dictum turpis dictum nec. Curabitur eu convallis quam. Proin efficitur velit nec quam ornare, id volutpat ex ornare. Vestibulum porttitor lobortis lacus, eu efficitur libero congue nec. Maecenas volutpat massa non nulla venenatis, aliquet gravida lectus aliquam. Pellentesque aliquam blandit condimentum. Phasellus non justo odio. Phasellus a dui posuere, dapibus risus tempus, laoreet augue. Sed tincidunt, lorem a placerat aliquet, nisi erat lobortis orci, in aliquet mi ante nec nisi. Pellentesque porttitor elit sem, nec scelerisque arcu suscipit eu.`
   },
-  render: ({ title, subtitle, bodyContent, closingButton }) => {
+  argTypes: {
+    status: {
+      control: 'inline-radio',
+      options: ['info', 'success', 'warning', 'danger']
+    }
+  },
+  render: ({ title, subtitle, bodyContent, closingButton, status }) => {
     return html`
       <sl-button fill="outline" size="md" @click=${onClick}>Show inline message</sl-button>
-      <sl-inline-message ?closing-button=${closingButton}>
+      <sl-inline-message ?closing-button=${closingButton} status=${status}>
         <span slot="title">${title}</span>
         <span slot="subtitle">${subtitle}</span>
         ${bodyContent}
--- packages/components/inline-message/src/inline-message.ts
+++ packages/components/inline-message/src/inline-message.ts
@@ -1,14 +1,15 @@
 import type { TemplateResult } from 'lit-html';
-import type { CSSResult, CSSResultGroup } from 'lit';
+import type { CSSResultGroup } from 'lit';
 import type { ScopedElementsMap } from '@open-wc/scoped-elements';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
-import { ButtonBar } from '@sl-design-system/button-bar';
 import { Icon } from '@sl-design-system/icon';
 import { Button } from '@sl-design-system/button';
 import { breakpoints } from '@sl-design-system/shared';
-import { LitElement, adoptStyles, html, nothing, unsafeCSS } from 'lit';
-import { property, query } from 'lit/decorators.js';
+import { LitElement, html, nothing } from 'lit';
+import { property } from 'lit/decorators.js';
 import styles from './inline-message.scss.js';
+
+export type InlineMessageStatus = 'info' | 'success' | 'warning' | 'danger';
 
 /**
  * A dialog component for displaying modal UI.
@@ -31,7 +32,6 @@ export class InlineMessage extends ScopedElementsMixin(LitElement) {
   static get scopedElements(): ScopedElementsMap {
     return {
       'sl-button': Button,
-      'sl-button-bar': ButtonBar,
       'sl-icon': Icon
     };
   }
@@ -39,17 +39,21 @@ export class InlineMessage extends ScopedElementsMixin(LitElement) {
   /** @private */
   static override styles: CSSResultGroup = [breakpoints, styles];
 
-  /** @private */
-  @query('dialog') dialog?: HTMLDialogElement;
+  // /** @private */
+  // @query('dialog') dialog?: HTMLDialogElement;
 
-  /** Disables the ability to close the dialog using the Escape key. */
-  @property({ type: Boolean, attribute: 'disable-close' }) disableClose = false;
+  // /** Disables the ability to close the dialog using the Escape key. */
+  // @property({ type: Boolean, attribute: 'disable-close' }) disableClose = false;
 
   /** Determines whether closing button (default one) should be shown in the top right corner. */
-  @property({ type: Boolean, attribute: 'closing-button' }) closingButton?: boolean;
+  @property({ type: Boolean, attribute: 'closing-button' }) closingButton = true;
 
   /** The ARIA role of the dialog. */
   @property() override role: 'dialog' | 'alertdialog' = 'dialog';
+
+  /** The status of the inline message.
+   * @type {'info' | 'success' | 'warning' | 'danger'} */
+  @property({ reflect: true }) status: InlineMessageStatus = 'info';
 
   override connectedCallback(): void {
     super.connectedCallback();
@@ -60,9 +64,12 @@ export class InlineMessage extends ScopedElementsMixin(LitElement) {
   override render(): TemplateResult {
     return html`
       <div class="inline-message-wrapper">
-        <slot>icon</slot>
+        <!--        <slot>icon</slot>-->
+        <slot name="icon" part="icon">
+          <sl-icon name="triangle-exclamation-solid" size="md"></sl-icon>
+        </slot>
         <slot>message</slot>
-        <slot name="icon" part="icon"></slot>
+        <!--        <slot name="icon" part="icon"></slot>-->
         ${this.closingButton
           ? html`
               <slot name="close-button" @click=${this.#onCloseClick}>
@@ -78,139 +85,103 @@ export class InlineMessage extends ScopedElementsMixin(LitElement) {
         </slot>
         <slot name="description" part="description"> </slot>
       </div>
-      <dialog
-        @cancel=${this.#onCancel}
-        @click=${this.#onClick}
-        @close=${this.#onClose}
-        @keydown=${this.#onKeydown}
-        .role=${this.role}
-        aria-labelledby="title"
-        part="dialog"
-      >
-        <slot name="header">
-          <div class="titles" part="titles">
-            <slot name="title" id="title"></slot>
-            <slot name="subtitle"></slot>
-          </div>
-          <slot name="header-actions">
-            <slot name="header-buttons"></slot>
-            ${this.closingButton
-              ? html`
-                  <slot name="close-button" @click=${this.#onCloseClick}>
-                    <sl-button fill="ghost" variant="default">
-                      <sl-icon name="xmark"></sl-icon>
-                    </sl-button>
-                  </slot>
-                `
-              : nothing}
-          </slot>
-        </slot>
-        <slot name="body">
-          <div class="body-content" part="body-content">
-            <slot></slot>
-          </div>
-        </slot>
-        <slot name="footer">
-          <sl-button-bar>
-            <slot name="actions"></slot>
-          </sl-button-bar>
-        </slot>
-      </dialog>
     `;
   }
 
-  showModal(): void {
-    this.inert = false;
-    this.dialog?.showModal();
-
-    // Disable scrolling while the dialog is open
-    document.documentElement.style.overflow = 'hidden';
-
-    /** Workaround for the backdrop background,
-     *  the backdrop doesn't inherit from the :root, so we cannot use tokens for the background-color,
-     *  needs to be removed in the future,
-     * the bug should be fixed: https://drafts.csswg.org/css-position-4/#backdrop */
-    const backdrop: CSSResult = unsafeCSS(
-      `::backdrop {
-        background-color: ${window.getComputedStyle(document.body).getPropertyValue('--sl-body-surface-overlay')};
-      }
-    `
-    );
-
-    if (this.shadowRoot) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      adoptStyles(this.shadowRoot, [breakpoints, styles, backdrop]);
-    }
-  }
-
-  close(): void {
-    if (this.dialog?.open) {
-      this.dialog?.close();
-    }
-  }
-
-  #onCancel(event: Event): void {
-    if (this.disableClose) {
-      event.preventDefault();
-    }
-  }
-
+  // showModal(): void {
+  //   this.inert = false;
+  //   this.dialog?.showModal();
+  //
+  //   // Disable scrolling while the dialog is open
+  //   document.documentElement.style.overflow = 'hidden';
+  //
+  //   /** Workaround for the backdrop background,
+  //    *  the backdrop doesn't inherit from the :root, so we cannot use tokens for the background-color,
+  //    *  needs to be removed in the future,
+  //    * the bug should be fixed: https://drafts.csswg.org/css-position-4/#backdrop */
+  //   const backdrop: CSSResult = unsafeCSS(
+  //     `::backdrop {
+  //       background-color: ${window.getComputedStyle(document.body).getPropertyValue('--sl-body-surface-overlay')};
+  //     }
+  //   `
+  //   );
+  //
+  //   if (this.shadowRoot) {
+  //     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  //     adoptStyles(this.shadowRoot, [breakpoints, styles, backdrop]);
+  //   }
+  // }
+  //
+  // close(): void {
+  //   if (this.dialog?.open) {
+  //     this.dialog?.close();
+  //   }
+  // }
+  //
+  // #onCancel(event: Event): void {
+  //   if (this.disableClose) {
+  //     event.preventDefault();
+  //   }
+  // }
+  //
   #onCloseClick(event: PointerEvent & { target: HTMLElement }): void {
     event.preventDefault();
     event.stopPropagation();
 
-    this.#closeDialogOnAnimationend(event.target as HTMLElement);
+    this.remove();
+
+    // this.#closeDialogOnAnimationend(event.target as HTMLElement);
 
     // TODO: emit sl-close
   }
-
-  #onClick(event: PointerEvent & { target: HTMLElement }): void {
-    if (event.target && this.dialog) {
-      const rect = this.dialog.getBoundingClientRect();
-      // Check if the user clicked on the sl-dialog-close button or on the backdrop
-      if (
-        event.target.matches('sl-button[sl-dialog-close]') ||
-        (!this.disableClose &&
-          (event.clientY < rect.top ||
-            event.clientY > rect.bottom ||
-            event.clientX < rect.left ||
-            event.clientX > rect.right))
-      ) {
-        this.#closeDialogOnAnimationend(event.target as HTMLElement);
-      }
-    }
-  }
-
-  #onKeydown(event: KeyboardEvent): void {
-    if (event.code === 'Escape' && !this.disableClose) {
-      this.#closeDialogOnAnimationend(event.target as HTMLElement);
-    }
-  }
-
-  #closeDialogOnAnimationend(target: HTMLElement): void {
-    this.dialog?.addEventListener(
-      'animationend',
-      () => {
-        this.dialog?.removeAttribute('closing');
-
-        if (target?.matches('sl-button[sl-dialog-close]')) {
-          this.dialog?.close(target?.getAttribute('sl-dialog-close') || '');
-        } else {
-          this.dialog?.close();
-        }
-      },
-      { once: true }
-    );
-
-    requestAnimationFrame(() => {
-      this.dialog?.setAttribute('closing', '');
-    });
-  }
-
-  #onClose(): void {
-    // Reenable scrolling after the dialog has closed
-    document.documentElement.style.overflow = '';
-
-    this.inert = true;
-  }
+  //
+  // #onClick(event: PointerEvent & { target: HTMLElement }): void {
+  //   if (event.target && this.dialog) {
+  //     const rect = this.dialog.getBoundingClientRect();
+  //     // Check if the user clicked on the sl-dialog-close button or on the backdrop
+  //     if (
+  //       event.target.matches('sl-button[sl-dialog-close]') ||
+  //       (!this.disableClose &&
+  //         (event.clientY < rect.top ||
+  //           event.clientY > rect.bottom ||
+  //           event.clientX < rect.left ||
+  //           event.clientX > rect.right))
+  //     ) {
+  //       this.#closeDialogOnAnimationend(event.target as HTMLElement);
+  //     }
+  //   }
+  // }
+  //
+  // #onKeydown(event: KeyboardEvent): void {
+  //   if (event.code === 'Escape' && !this.disableClose) {
+  //     this.#closeDialogOnAnimationend(event.target as HTMLElement);
+  //   }
+  // }
+  //
+  // #closeDialogOnAnimationend(target: HTMLElement): void {
+  //   this.dialog?.addEventListener(
+  //     'animationend',
+  //     () => {
+  //       this.dialog?.removeAttribute('closing');
+  //
+  //       if (target?.matches('sl-button[sl-dialog-close]')) {
+  //         this.dialog?.close(target?.getAttribute('sl-dialog-close') || '');
+  //       } else {
+  //         this.dialog?.close();
+  //       }
+  //     },
+  //     { once: true }
+  //   );
+  //
+  //   requestAnimationFrame(() => {
+  //     this.dialog?.setAttribute('closing', '');
+  //   });
+  // }
+  //
+  // #onClose(): void {
+  //   // Reenable scrolling after the dialog has closed
+  //   document.documentElement.style.overflow = '';
+  //
+  //   this.inert = true;
+  // }
 }

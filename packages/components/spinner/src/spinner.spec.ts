191123a84e6bd9ef9d06c6bc8e510840e04ae82a
Diana Broeders <diana.broeders@sanoma.com>
2024-01-02 15:07:08 +0100

    fixed test

--- packages/components/spinner/src/spinner.spec.ts
+++ packages/components/spinner/src/spinner.spec.ts
@@ -3,31 +3,19 @@ import { expect, fixture, should } from '@open-wc/testing';
 import { html } from 'lit';
 import '../register.js';
 
-describe('dna-spinner', () => {
+describe('sl-spinner', () => {
   let el: Spinner;
 
   beforeEach(async () => {
-    el = await fixture(html`<sl-spinner>99+</sl-spinner>`);
+    el = await fixture(html`<sl-spinner></sl-spinner>`);
   });
 
   it('should render correctly', () => {
     expect(el).shadowDom.to.equalSnapshot();
   });
 
-  it('should have size medium and variant neutral by default', () => {
+  it('should have size medium and variant default by default', () => {
     expect(el).to.have.attribute('size','md');
-    expect(el).to.have.attribute('variant','neutral');
-  });
-
-  it('should render the slotted content', () => {
-    expect(el).shadowDom.to.have.text('99+');
-    expect(el).shadowDom.to.equal(`<slot></slot>`);
-  });
-
-  it('should not render the slotted content for small size', async () => {
-    el.setAttribute('size','sm');
-    await el.updateComplete; 
-
-    expect(el).shadowDom.to.equal(``);
+    expect(el).to.have.attribute('variant','default');
   });
 });

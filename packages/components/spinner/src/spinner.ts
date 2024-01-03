04adb43d9f140989ec5142fbd9feba45d044de5a
Diana Broeders <diana.broeders@sanoma.com>
2024-01-03 17:16:09 +0100

    fixed safari wobble, changed easing function

--- packages/components/spinner/src/spinner.ts
+++ packages/components/spinner/src/spinner.ts
@@ -31,19 +31,21 @@ export class Spinner extends LitElement {
   }
 
   override render(): TemplateResult | typeof nothing {
-    return html`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48">
-      <path
-        fill-rule="evenodd"
-        d="M24 6C14.059 6 6 14.059 6 24s8.059 18 18 18 18-8.059 18-18S33.941 6 24 6ZM0 24C0 10.745 10.745 0 24 0s24 10.745 24 24-10.745 24-24 24S0 37.255 0 24Z"
-        clip-rule="evenodd"
-        style="fill:var(--_color);opacity:var(--_shadow-opacity);"
-      />
-      <path
-        fill-rule="evenodd"
-        d="M24 6C14.059 6 6 14.059 6 24a3 3 0 1 1-6 0C0 10.745 10.745 0 24 0s24 10.745 24 24a3 3 0 1 1-6 0c0-9.941-8.059-18-18-18Z"
-        clip-rule="evenodd"
-        style="fill:var(--_color);"
-      />
+    return html`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48" height=
+>     <g>
+        <path
+          fill-rule="evenodd"
+          d="M24 6C14.059 6 6 14.059 6 24s8.059 18 18 18 18-8.059 18-18S33.941 6 24 6ZM0 24C0 10.745 10.745 0 24 0s24 10.745 24 24-10.745 24-24 24S0 37.255 0 24Z"
+          clip-rule="evenodd"
+          style="fill:var(--_color);opacity:var(--_shadow-opacity);"
+        />
+        <path
+          fill-rule="evenodd"
+          d="M24 6C14.059 6 6 14.059 6 24a3 3 0 1 1-6 0C0 10.745 10.745 0 24 0s24 10.745 24 24a3 3 0 1 1-6 0c0-9.941-8.059-18-18-18Z"
+          clip-rule="evenodd"
+          style="fill:var(--_color);"
+        />
+      </g>
     </svg>`;
   }
 }

9602b6db2b5bf8ba9d4ddb242733924a5200779f
Diana Broeders <diana.broeders@sanoma.com>
2024-01-03 17:20:35 +0100

    cleanup

--- packages/components/spinner/src/spinner.ts
+++ packages/components/spinner/src/spinner.ts
@@ -31,8 +31,8 @@ export class Spinner extends LitElement {
   }
 
   override render(): TemplateResult | typeof nothing {
-    return html`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48" height=
->     <g>
+    return html`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48">
+      <g>
         <path
           fill-rule="evenodd"
           d="M24 6C14.059 6 6 14.059 6 24s8.059 18 18 18 18-8.059 18-18S33.941 6 24 6ZM0 24C0 10.745 10.745 0 24 0s24 10.745 24 24-10.745 24-24 24S0 37.255 0 24Z"

439f3d0ad9bca2df68017a338b4684375b514e2a
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-01-09 18:23:19 +0100

    🛩

--- packages/angular/projects/angular-core/src/forms/checkbox/checkbox.directive.ts
+++ packages/angular/projects/angular-core/src/forms/checkbox/checkbox.directive.ts
@@ -1,6 +1,7 @@
-import {Directive, ElementRef, forwardRef, HostListener, Inject, Injector, Renderer2} from '@angular/core';
-import {NG_VALIDATORS, NG_VALUE_ACCESSOR} from '@angular/forms';
-import {FormControlElementDirective} from '../form-control/form-control-element.directive';
+import { Directive, ElementRef, forwardRef, HostListener, Inject, Injector } from '@angular/core';
+import { NG_VALIDATORS, NG_VALUE_ACCESSOR} from '@angular/forms';
+import { type Checkbox } from '@sl-design-system/checkbox';
+import { FormControlElementDirective } from '../form-control/form-control-element.directive';
 
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
@@ -19,9 +20,7 @@ import {FormControlElementDirective} from '../form-control/form-control-element.
   ]
 })
 export class CheckboxDirective extends FormControlElementDirective {
-
   #initialValue?: string;
-
   #value?: string;
 
   get value(): string | undefined {
@@ -31,20 +30,21 @@ export class CheckboxDirective extends FormControlElementDirective {
   set value(val: string | undefined) {
     this.#value = val;
     this.onChange(this.#value);
-    this.elementRef.nativeElement.internals.value = this.#value;
+    this.elementRef.nativeElement.value = this.#value;
     this.validatorOnChange();
   }
 
   writeValue(value: string): void {
     this.#initialValue = value;
+
     if (value) {
       this.elementRef.nativeElement.value = this.#initialValue;
-      this.elementRef.nativeElement.setFormValue(value);
+      // this.elementRef.nativeElement.setFormValue(value);
       this.value = value;
     }
   }
 
-  constructor(public override elementRef: ElementRef, private renderer: Renderer2, @Inject(Injector) injector: Injector) {
+  constructor(public override elementRef: ElementRef<Checkbox>, @Inject(Injector) injector: Injector) {
     super(elementRef, injector);
   }
 

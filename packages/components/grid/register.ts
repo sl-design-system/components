06cfb6bff8f2c1a8d4a132099f21f2e8dc4f2461
Jeroen Zwartepoorte <jeroen.zwartepoorte@sanoma.com>
2024-01-26 09:04:15 +0100

    Add select & text-field columns to grid (#917)

--- packages/components/grid/register.ts
+++ packages/components/grid/register.ts
@@ -8,16 +8,20 @@ import type {
 import { Grid } from './src/grid.js';
 import { GridColumn } from './src/column.js';
 import { GridColumnGroup } from './src/column-group.js';
+import { GridFilterColumn } from './src/filter-column.js';
+import { GridSelectColumn } from './src/select-column.js';
 import { GridSelectionColumn } from './src/selection-column.js';
 import { GridSortColumn } from './src/sort-column.js';
-import { GridFilterColumn } from './src/filter-column.js';
+import { GridTextFieldColumn } from './src/text-field-column.js';
 
 customElements.define('sl-grid', Grid);
 customElements.define('sl-grid-column', GridColumn);
 customElements.define('sl-grid-column-group', GridColumnGroup);
 customElements.define('sl-grid-filter-column', GridFilterColumn);
+customElements.define('sl-grid-select-column', GridSelectColumn);
 customElements.define('sl-grid-selection-column', GridSelectionColumn);
 customElements.define('sl-grid-sort-column', GridSortColumn);
+customElements.define('sl-grid-text-field-column', GridTextFieldColumn);
 
 declare global {
   interface GlobalEventHandlersEventMap {
@@ -33,7 +37,9 @@ declare global {
     'sl-grid-column': GridColumn;
     'sl-grid-column-group': GridColumnGroup;
     'sl-grid-filter-column': GridFilterColumn;
+    'sl-grid-select-column': GridSelectColumn;
     'sl-grid-selection-column': GridSelectionColumn;
     'sl-grid-sort-column': GridSortColumn;
+    'sl-grid-text-field-column': GridTextFieldColumn;
   }
 }

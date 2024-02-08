a50a935585dbdf1a4e272c40c933096ebeeae1bc
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-02-08 14:09:22 +0100

    💙

--- packages/components/dialog/src/dialog.spec.ts
+++ packages/components/dialog/src/dialog.spec.ts
@@ -31,14 +31,14 @@ describe('sl-dialog', () => {
     });
 
     it('should not have a close button', () => {
-      expect(dialog.querySelector('slot[name="close-button"] sl-button')).not.to.exist;
+      expect(dialog.querySelector('sl-button[aria-label="Close"]')).not.to.exist;
     });
 
     it('should have a close button when set', async () => {
       el.closeButton = true;
       await el.updateComplete;
 
-      expect(dialog.querySelector('slot[name="close-button"] sl-button')).to.exist;
+      expect(dialog.querySelector('sl-button[aria-label="Close"]')).to.exist;
     });
 
     it('should have a role of dialog', () => {
@@ -198,7 +198,7 @@ describe('sl-dialog', () => {
       const onClose = spy();
 
       el.addEventListener('sl-close', onClose);
-      el.renderRoot.querySelector<Button>('slot[name="close-button"] sl-button')?.click();
+      el.renderRoot.querySelector<Button>('sl-button[aria-label="Close"]')?.click();
 
       // Simulate the animationend event that is used in #closeDialogOnAnimationend
       dialog.dispatchEvent(new Event('animationend'));

e916c1e0c78dcd558e78d00c5e4b0c9562a922f8
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-03-06 14:33:31 +0100

    🍬

--- packages/components/spinner/src/spinner.spec.ts
+++ packages/components/spinner/src/spinner.spec.ts
@@ -14,8 +14,11 @@ describe('sl-spinner', () => {
     expect(el).shadowDom.to.equalSnapshot();
   });
 
-  it('should have size medium and no variant by default', () => {
-    expect(el).to.have.attribute('size', 'md');
+  it('should not have a default size', () => {
+    expect(el).not.to.have.attribute('size');
+  });
+
+  it('should not have a default variant', () => {
     expect(el).not.to.have.attribute('variant');
   });
 });

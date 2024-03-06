3be7a02b4bc5218e5dd8f6e3405e6573cbd65c5f
Jeroen Zwartepoorte <jeroen.zwartepoorte@sanoma.com>
2024-03-06 15:03:46 +0100

    Cleaner workaround for grid scrolling bug (#1027)

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   '*.scss': 'stylelint --fix',
-  'packages/components/**/*.ts': 'eslint --config packages/components/eslint.config.js --fix'
+  'packages/components/**/*.ts': 'eslint --config packages/components/eslint.config.mjs --fix'
 };

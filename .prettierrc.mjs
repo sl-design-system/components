8c17afac77f626ba29e844ef72b76e294a7d9279
Jeroen Zwartepoorte <jeroen.zwartepoorte@sanoma.com>
2024-03-08 09:02:37 +0100

    Move prettier settings into stylelint config (#1031)

--- .prettierrc.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-  arrowParens: 'avoid',
-  printWidth: 120,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'none',
-  endOfLine: 'auto'
-};

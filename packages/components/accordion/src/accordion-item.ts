b39b2a3dc6764e16e9c611ac6c96a4eef61e9421
Anna Sobczak <111562742+anna-lach@users.noreply.github.com>
2024-03-13 11:09:45 +0100

    Feature/907 accordion component (#1036)
    * accordion initial setup
    
    * some accordion changes
    
    * accordion structure changes, summary added
    
    * accordion and items changes
    
    * removed unnecessary styling
    
    * accordion item styling
    
    * Styling changes, all opened elements
    
    * accordion single option, mechanism changes, events
    
    * Accordion changes, disabled item
    
    * Accordion disabled item
    
    * Only one item opened at once
    
    * Story arg for single item, improved showing only one item at once
    
    * Accordio changes, sticky, stories changes, styling with example animation (partially working)
    
    * stories changed, accordion item changes, problems with animations, open param
    
    * Accordion changes, partially working animations
    
    * Animations changes, partially working, problems in Chrome with opening twice and more
    
    * animations changes
    
    * Animations changes, partially working
    
    * animations changes
    
    * ⚙️ Generate theme tokens
    
    * added animation
    
    * Styling with tokens, improved animations, stories changes
    
    * some structure changes
    
    * Animations changes
    
    * Animations changes - working when collapse as well
    
    * Animations improvements, also for sticky
    
    * Some styling and stories changes
    
    * Improved animations, partially refactored
    
    * styling changes
    
    * Stories improvements, refactor, accessibility part
    
    * Animations changes, broken on closing
    
    * Animations fixed, sticky fixed, some initial unit tests
    
    * Accordion and accordion item unit tests, small accordion improvement
    
    * fixed animation on closing when single version, unit tests - unnecessary removed, changeset added
    
    * fixed test, toggle as private
    
    * changes after code review
    
    * tests fixes
    
    * cleaned up usage tab
    
    * typo
    
    * Update usage.md
    
    * Update usage.md
    
    * changes after code review
    
    * changes after code review
    
    ---------
    
    Co-authored-by: anna-lach <anna-lach@users.noreply.github.com>
    Co-authored-by: Diana Broeders <diana.broeders@sanoma.com>
    Co-authored-by: Daniëlle Rameau <111998351+DanielleRameau@users.noreply.github.com>

---
+++ packages/components/accordion/src/accordion-item.ts
@@ -0,0 +1,106 @@
+import { localized } from '@lit/localize';
+import { type EventEmitter, event } from '@sl-design-system/shared';
+import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
+import { property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+import styles from './accordion-item.scss.js';
+
+/**
+ * An accordion item component.
+ *
+ * @slot default - Body content for the accordion
+ * @part summary - Header element of the accordion-item
+ */
+@localized()
+export class AccordionItem extends LitElement {
+  /** @private */
+  static override styles: CSSResultGroup = styles;
+
+  /** A text shown in the header - as a title of the accordion item. */
+  @property() summary?: string;
+
+  /** Whether the element is disabled. */
+  @property({ type: Boolean, reflect: true }) disabled?: boolean;
+
+  /** Whether the details element is opened. */
+  @property({ reflect: true, type: Boolean }) open?: boolean;
+
+  /** Emits when the accordion item has been toggled. */
+  @event({ name: 'sl-toggle' }) toggleEvent!: EventEmitter<string>;
+
+  override render(): TemplateResult {
+    return html`
+      <details @toggle=${this.#onToggle} ?open=${this.open}>
+        <summary
+          id="summary-id"
+          aria-controls="content"
+          aria-disabled=${this.disabled ? 'true' : 'false'}
+          aria-expanded=${this.open ? 'true' : 'false'}
+          tabindex=${this.disabled ? -1 : 0}
+          part="summary"
+          @click=${this.#onClick}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="28"
+            viewBox="0 0 24 28"
+            fill="none"
+            class=${classMap({ opened: !!this.open })}
+          >
+            <rect class="horizontal-line" x="11.0103" y="6" width="1.97938" height="16" rx="0.824742" fill="#222222" />
+            <rect class="vertical-line" x="11.0103" y="6" width="1.97938" height="16" rx="0.824742" fill="#222222" />
+          </svg>
+          ${this.summary}
+        </summary>
+        <div class="wrapper">
+          <div id="content" class="panel" role="region" aria-labelledby="summary-id">
+            <slot></slot>
+          </div>
+        </div>
+      </details>
+    `;
+  }
+
+  #onToggle(event: ToggleEvent): void {
+    this.open = event.newState === 'open';
+
+    this.toggleEvent.emit(event.newState);
+  }
+
+  #onClick(event: Event & { target: HTMLElement }): void {
+    const wrapper = this.renderRoot.querySelector('.wrapper') as HTMLDivElement;
+
+    if (this.disabled || event.defaultPrevented) {
+      // No toggling when `disabled` or the user prevents it.
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+
+    const details = this.renderRoot.querySelector('details') as HTMLDetailsElement;
+
+    // In Chrome opening animation is not working by default when opening details on the second time
+    requestAnimationFrame(() => wrapper.classList.add('opening'));
+    wrapper.addEventListener(
+      'animationend',
+      () => {
+        wrapper.classList.remove('opening');
+      },
+      { once: true }
+    );
+
+    if (details.hasAttribute('open')) {
+      event.preventDefault();
+      wrapper.classList.add('closing');
+      wrapper.addEventListener(
+        'animationend',
+        () => {
+          details.removeAttribute('open');
+          wrapper.classList.remove('closing');
+        },
+        { once: true }
+      );
+    }
+  }
+}

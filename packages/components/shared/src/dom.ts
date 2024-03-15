a2d3df14f0b8aa23340aa8ed54f15a97e550e43f
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-03-15 19:58:22 +0100

    🔈

--- packages/components/shared/src/dom.ts
+++ packages/components/shared/src/dom.ts
@@ -1,8 +1,9 @@
-export const getScrollParent = (element: Element): Element | null => {
-  if (element == null) {
-    return null;
-  }
-
+/**
+ * Returns the first scrollable parent of the given element.
+ * @param element The element to find the scrollable parent of.
+ * @returns The first scrollable parent of the given element; if no explicit scroll parent, returns the html element.
+ */
+export const getScrollParent = (element: Element): Element => {
   if (element.scrollHeight > element.clientHeight) {
     return element;
   } else if (element.parentElement) {

5a5279dbaee4fdac9bd4f1e8f7c529cedb8a162a
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-03-19 14:14:32 +0100

    🐿

--- packages/checklist/src/checklist.ts
+++ packages/checklist/src/checklist.ts
@@ -1,4 +1,4 @@
-import { faCircleCheck, faCircleExclamation } from '@fortawesome/pro-regular-svg-icons';
+import { faCircleCheck, faCircleExclamation } from '@fortawesome/pro-solid-svg-icons';
 import { type ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
 import { Icon } from '@sl-design-system/icon';
 import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
@@ -12,9 +12,8 @@ declare global {
 }
 
 interface ChecklistItem {
-  correct: boolean;
   title: string;
-  description: string;
+  description: TemplateResult | void;
 }
 
 Icon.register(faCircleCheck, faCircleExclamation);
@@ -39,11 +38,12 @@ export class Checklist extends ScopedElementsMixin(LitElement) {
   override firstUpdated(): void {
     // Delay the check to the next frame after first render to
     // give the application time to initialize.
-    requestAnimationFrame(() => this.check());
+    requestAnimationFrame(() => setTimeout(() => void this.check(), 100));
   }
 
   override render(): TemplateResult {
     return html`
+      <link rel="stylesheet" href="https://use.typekit.net/kes1hoh.css" />
       <h1>SL Design System Checklist</h1>
       <p>
         Welcome to the Sanoma Learning Design System. You will find a checklist below of the steps described in our
@@ -52,9 +52,9 @@ export class Checklist extends ScopedElementsMixin(LitElement) {
 
       ${this.items.map(
         item => html`
-          <details .open=${!item.correct}>
+          <details .open=${!!item.description}>
             <summary @click=${(event: Event) => event.preventDefault()}>
-              <sl-icon .name=${item.correct ? 'far-circle-check' : 'far-circle-exclamation'} size="xl"></sl-icon>
+              <sl-icon .name=${item.description ? 'fas-circle-exclamation' : 'fas-circle-check'} size="xl"></sl-icon>
               ${item.title}
             </summary>
             <p>${item.description}</p>
@@ -70,45 +70,80 @@ export class Checklist extends ScopedElementsMixin(LitElement) {
     `;
   }
 
-  check(): void {
+  /** Perform the checks. */
+  async check(): Promise<void> {
     this.items = [
       {
-        correct: this.#checkTheme(),
         title: 'CSS Custom Properties present',
-        description:
-          'Nulla fugiat adipisicing anim veniam consequat irure sint consequat duis anim velit tempor aliquip. Culpa sint proident qui quis adipisicing quis. Deserunt aliqua fugiat amet anim ex anim. Excepteur eu irure cillum qui. Eiusmod excepteur aute occaecat ullamco cupidatat. Dolor qui labore magna non non adipisicing.'
+        description: this.#checkTheme()
       },
       {
-        correct: this.#checkFonts(),
         title: 'Webfonts are available',
-        description: 'Consectetur ex adipisicing elit irure nulla fugiat officia voluptate tempor sunt ut dolor.'
+        description: await this.#checkFonts()
       },
       {
-        correct: this.#checkIcons(),
         title: 'Theme is set up correctly',
-        description: 'Laborum anim proident qui Lorem.'
+        description: this.#checkIcons()
       },
       {
-        correct: this.#checkButton(),
         title: 'Button web component is loaded',
-        description: 'Eiusmod esse occaecat ad amet cupidatat elit exercitation sit.'
+        description: this.#checkButton()
       }
     ];
   }
 
-  #checkButton(): boolean {
-    return true;
+  #checkButton(): TemplateResult | void {
+    if (!customElements.get('sl-button')) {
+      return html`
+        The <code>sl-button</code> component is not loaded. Make sure you have installed the button package and added
+        the <code>import</code> statement to your application as described in the
+        <a href="https://sanomalearning.design/categories/getting-started/developers/#start-using-components"
+          >start using components</a
+        >
+        section.
+      `;
+    }
   }
 
-  #checkFonts(): boolean {
-    return true;
+  async #checkFonts(): Promise<TemplateResult | void> {
+    const styles = getComputedStyle(this),
+      required = ['--sl-text-typeset-font-family-body', '--sl-text-typeset-font-family-heading'].map(name =>
+        styles.getPropertyValue(name)
+      ),
+      available = Array.from((await document.fonts.ready).keys()).map(ff => ff.family);
+
+    if (!required.every(family => available.includes(family))) {
+      return html`Not all required fonts are available. Your theme uses the font families
+      ${required.map((family, index) => html`<code>${family}</code>${index < required.length - 1 ? ' and ' : ''}`)}. The
+      fonts used in the theme are not a part of the theme package. Please make sure the fonts are loaded.`;
+    }
   }
 
-  #checkIcons(): boolean {
-    return true;
+  #checkIcons(): TemplateResult | void {
+    const iconCount = Object.keys(window.SLDS?.icons ?? {}).length;
+
+    if (iconCount <= 2) {
+      return html`
+        The theme icons are not loaded. Please make sure you are calling the <code>setup()</code> method from the theme
+        package as described in the
+        <a href="https://sanomalearning.design/categories/getting-started/developers/#setup-a-theme">setup a theme</a>
+        section.
+      `;
+    }
   }
 
-  #checkTheme(): boolean {
-    return false;
+  #checkTheme(): TemplateResult | void {
+    const base = !!getComputedStyle(this).getPropertyValue('--sl-color-palette-white-base'),
+      lightOrDark = !!getComputedStyle(this).getPropertyValue('--sl-color-surface-solid-primary-foreground');
+
+    if (base && !lightOrDark) {
+      return html`The base theme is set up correctly, but the tokens for the light or dark mode are missing. You likely
+      need to include the stylesheet for the light or dark mode in your application.`;
+    } else if (!base && !lightOrDark) {
+      return html`We could not detect any theme tokens. Please make sure you have installed the theme package and added
+        the stylesheets to your application as described in the
+        <a href="https://sanomalearning.design/categories/getting-started/developers/#setup-a-theme">setup a theme</a>
+        section.`;
+    }
   }
 }

cea512bdcce715f6c01d54d8f906ad549b7257a5
Diana Broeders <diana.broeders@sanoma.com>
2024-04-10 14:17:22 +0200

    cleanup

--- website/src/_data/version.cjs
+++ website/src/_data/version.cjs
@@ -5,17 +5,17 @@ module.exports = async function() {
   // Pass in your unique custom cache key
   let asset = new AssetCache("component-version-numbers");
   // check if the cache is fresh within the last day
-  // if(asset.isCacheValid("1d")) {
-  //   // return cached data.
-  //   return asset.getCachedValue(); // a promise
-  // }
+  if(asset.isCacheValid("1d")) {
+    // return cached data.
+    return asset.getCachedValue(); // a promise
+  }
 
 
     const octokit = new Octokit({
       auth: process.env.GITHUB_API_TOKEN
     })
 
-    const pages = [1,2,3,4,5,6,7,8,9];
+    const pages = [1,2,3];
 
     const gitpackages = await pages.map(async page => octokit.request('GET /orgs/sl-design-system/packages', {
       org: 'sl-design-system',
@@ -28,8 +28,6 @@ module.exports = async function() {
 
     const gitpackagesData = await Promise.all(gitpackages);
     const packages = gitpackagesData.flatMap(page => page.data);
-    // await asset.save({pre:gitpackagesData,post:gitpackagesData.flatMap(page => page.data)}, "json");
-
 
     const versions = await packages.map(async p => {
       return octokit.request(`GET /orgs/sl-design-system/packages/npm/${p.name}/versions`, {

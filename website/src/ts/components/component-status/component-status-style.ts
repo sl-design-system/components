41ff8176863344dadefec2a795ee6231182eccd9
anna-lach <a.lach@nowaera.pl>
2024-05-07 14:27:14 +0200

    Initial component status added

---
+++ website/src/ts/components/component-status/component-status-style.ts
@@ -0,0 +1,114 @@
+import { css } from 'lit';
+
+export const componentStatusStyles = css`
+  :host {
+    display: none;
+  }
+
+  @media screen and (min-width: 1200px) {
+    :host {
+      display: block;
+      font: var(--typography-body);
+    }
+  }
+
+  h2 {
+    font: var(--typography-h2);
+    margin-block: var(--typography-h2-margin-block);
+  }
+
+  a.header-anchor {
+    color: inherit;
+    font-size: inherit;
+    text-decoration: none;
+  }
+
+  .ds-tabs {
+    position: sticky;
+    inset-block-start: 15%;
+  }
+
+  .ds-tabs-wrapper {
+    column-gap: 0.4rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    overflow-x: scroll;
+    padding-inline-start: 0;
+    scrollbar-width: none;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .ds-tabs-wrapper::before {
+    content: 'CONTENTS';
+    display: block;
+    font-size: 1.2rem;
+    font-weight: 600;
+    line-height: 1.5rem;
+    margin-block-end: 0.8rem;
+    margin-inline-start: 0.8rem;
+  }
+
+  .ds-tabs__container {
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: start;
+    margin: auto;
+    overflow: hidden;
+    padding-block-start: 0;
+    background-color: var(--background-color);
+    inset-block-start: -0.1rem;
+    overflow-x: hidden;
+    transition: padding-top 300ms;
+  }
+
+  .ds-tabs__vertical-indicator {
+    background-color: var(--tab-indicator-background);
+    block-size: 100%;
+    border-radius: 0.1rem;
+    inline-size: 0.2rem;
+    inset-block-start: 2.3rem;
+    position: relative;
+    height: 32px;
+    transition: all 400ms cubic-bezier(0.38, 0.8, 0.32, 1.07);
+  }
+
+  .ds-tab--vertical.active:not(:focus-visible) {
+    color: var(--highlight-color);
+  }
+
+  .ds-tabs__vertical-slider {
+    background-color: var(--vertical-slider-background);
+    position: relative;
+  }
+
+  .ds-tab--vertical {
+    background: none;
+    border: 0;
+    color: var(--tab-color);
+    cursor: pointer;
+    font-size: 1.4rem;
+    font-weight: 400;
+    line-height: 2rem;
+    padding: 0.6rem 0 0.6rem 0.8rem;
+    text-decoration: none;
+    transition: color $transition-timing linear;
+
+    &.active:not(:focus-visible) {
+      color: var(--highlight-color);
+    }
+
+    &:hover {
+      color: var(--highlight-color);
+    }
+
+    &:focus-visible {
+      border-radius: 0.6rem;
+      box-shadow: inset var(--focus-box-shadow);
+      outline: none;
+    }
+  }
+`;
---
+++ website/src/ts/components/component-status/component-status.ts
@@ -0,0 +1,193 @@
+import { LitElement, CSSResultGroup, type TemplateResult, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+// import { verticalTabsStyles } from './vertical-tabs-style';
+import { componentStatusStyles } from './component-status-style';
+
+/**
+ * Vertical tabs are getting parentElement as a source of tabs - `sections` with `h2` elements or `link-in-navigation` with `h2` elements.
+ *
+ * Example:
+ *
+ * <div>
+ *   <section>
+ *     <h2>First</h2>
+ *     My first content
+ *   </section>
+ *   <section>
+ *     <h2>Second</h2>
+ *     My second content
+ *   </section>
+ *   <ds-vertical-tabs></ds-vertical-tabs>
+ * </div>
+ * */
+
+const isMobileOrTablet = (): boolean => matchMedia('(width < 1200px)').matches;
+
+@customElement('ds-vertical-tabs')
+export class ComponentStatus extends LitElement {
+  /** @private */
+  static override styles: CSSResultGroup = componentStatusStyles;
+
+  /** Used to render vertical links content - tagElement is a source of links text, H2 is the default */
+  @property() tagElement = 'H2';
+
+  nextUniqueId = 0;
+
+  observer = new IntersectionObserver(
+    entries => {
+      let updated = false;
+      const sections = Array.from(this.parentElement?.querySelectorAll('section[id], [link-in-navigation][id]') || []),
+        links = Array.from(this.renderRoot.querySelectorAll('.ds-tab--vertical')) as HTMLElement[];
+
+      if (links.length === entries.filter(entry => entry.intersectionRatio === 1).length || (!window.location.hash && entries.length === links.length)) {
+        return;
+      }
+
+      entries.forEach(entry => {
+        if (updated) {
+          return;
+        }
+        let section: Element | null | undefined;
+        if (entry.isIntersecting && entry.intersectionRatio > 0) {
+          section = entry.target;
+          let index = sections.findIndex(b => {
+            return (b as HTMLElement).id.toLowerCase() == (section as HTMLElement).getAttribute('id')?.toLowerCase();
+          });
+
+          if (links[index]) {
+            this.#setActiveTab(links[index]);
+            this.#alignVerticalTabIndicator(links[index]);
+          }
+        }
+
+         updated = true;
+      });
+    },
+    { root: null, rootMargin: '-86px 0px 0px 0px' }
+  );
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    if (isMobileOrTablet()) {
+      return;
+    }
+
+
+    requestAnimationFrame(() => {
+    /** Source to observe - `sections` with `id` elements and `link-in-navigation` with `id` elements */
+    const sections = Array.from(this.parentElement?.querySelectorAll('section[id], [link-in-navigation][id]') || []),
+       verticalTabs = this.renderRoot.querySelectorAll('.ds-tab--vertical');
+
+      if (verticalTabs.length && !window.location.hash) {
+        this.#setActiveTab(verticalTabs[0] as HTMLElement);
+       window.scrollTo(0, 0);
+      } else if (window.location.hash) {
+        setTimeout(() => {
+          const targetElement = document.querySelector(window.location.hash),
+                links = Array.from(this.renderRoot.querySelectorAll('.ds-tab--vertical')) as HTMLElement[];
+          let index = links.findIndex(link => {
+            return (link as HTMLAnchorElement).hash == window.location.hash;
+          });
+          if (targetElement) {
+            links[index].click();
+            targetElement.scrollIntoView();
+          }
+        }, 100);
+      }
+
+    if (sections.length) {
+     sections.forEach(section => this.observer.observe(section));
+    }
+    });
+
+    window.addEventListener('hashchange',  () => {
+      const links = Array.from(this.renderRoot.querySelectorAll('.ds-tab--vertical')) as HTMLElement[];
+      let index = links.findIndex(link => {
+        return (link as HTMLAnchorElement).hash == window.location.hash;
+      });
+        links[index].click();
+    }, false);
+  }
+
+  override disconnectedCallback(): void {
+    this.observer?.disconnect();
+
+    super.disconnectedCallback();
+  }
+
+  override render(): TemplateResult {
+    /** generates nav links from parentElement content */
+    const links = Array.from(this.parentElement?.querySelectorAll('.header-anchor, [link-in-navigation]') || [])
+      .map(element => {
+        if (element.parentElement?.tagName === this.tagElement) {
+          if (element.parentElement.parentNode) {
+            (element.parentElement.parentNode as Element).id = element.parentElement.id;
+          }
+          return element.parentElement as Element;
+        } else if (element.hasAttribute('link-in-navigation')) {
+          return element as Element;
+        }
+        return null;
+      })
+      .filter(element => element !== null);
+
+    return html`
+            <div><slot></slot></div>
+    `;
+  }
+
+  // #onClick(event: Event) {
+  //   this.observer?.disconnect();
+  //
+  //   const link = event.target as HTMLElement;
+  //
+  //   this.#setActiveTab(link);
+  //   this.#alignVerticalTabIndicator(link);
+  //
+  //   const indicator = this.renderRoot.querySelector('.ds-tabs__vertical-indicator');
+  //   indicator?.addEventListener(
+  //     'transitionend',
+  //     () => {
+  //       setTimeout(() => {
+  //         /** Source to observe - `sections` with `id` elements and `link-in-navigation` with `id` elements */
+  //       const sections = Array.from(this.parentElement?.querySelectorAll('section[id], [link-in-navigation][id]') || []);
+  //       if (sections.length) {
+  //        sections.forEach(section => this.observer.observe(section));
+  //       }
+  //       }, 500);
+  //     })
+  // }
+
+  #setActiveTab(verticalTab: HTMLElement): void {
+    const currentVerticalTabLink = this.renderRoot.querySelector('[aria-selected="true"]'),
+      verticalTabs = this.renderRoot.querySelectorAll('.ds-tab--vertical');
+
+    currentVerticalTabLink?.setAttribute('aria-selected', 'false');
+    verticalTab.setAttribute('aria-selected', 'true');
+
+    verticalTabs.forEach(v => v.classList.remove('active'));
+    verticalTab.classList.add('active');
+  }
+
+  #alignVerticalTabIndicator(tab: Element): void {
+    const currentVerticalIndicatorElement = this.renderRoot.querySelector(
+        '.ds-tabs__vertical-indicator'
+      ) as HTMLElement,
+      tabsWrapper = this.renderRoot.querySelector('.ds-tabs-wrapper');
+
+    if (!currentVerticalIndicatorElement || !tabsWrapper) {
+      return;
+    }
+    currentVerticalIndicatorElement.style.top = `${
+      tab.getBoundingClientRect().top - tabsWrapper.getBoundingClientRect().top
+    }px`;
+    currentVerticalIndicatorElement.style.height = `${tab.getBoundingClientRect().height}px`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ds-component-status': ComponentStatus;
+  }
+}

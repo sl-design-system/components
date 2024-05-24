c3d2a0503ac3756017eb11ffa0450ab5b5d405c5
anna-lach <a.lach@nowaera.pl>
2024-05-24 14:54:01 +0200

    nunjucks filter

--- website/.eleventy.cjs
+++ website/.eleventy.cjs
@@ -34,7 +34,7 @@ module.exports = function(eleventyConfig) {
     return JSON.stringify(value, null, 2);
   });
 
-  eleventyConfig.addFilter("sortByTitle", function (arr, exclude) {
+  eleventyConfig.addNunjucksFilter("sortByTitle", function (arr, exclude) {
     const excluded = arr.filter(elem => elem.title == exclude);
     JSON.parse(JSON.stringify(excluded));
     arr.filter(elem => elem.title != exclude).slice().sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()))

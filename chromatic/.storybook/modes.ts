87920de627f13a45f99387536088995668722bb4
Diana Broeders <diana.broeders@sanoma.com>
2024-05-30 11:16:37 +0200

    build fix

--- chromatic/.storybook/modes.ts
+++ chromatic/.storybook/modes.ts
@@ -5,47 +5,62 @@ export const allModes = {
   },
   'bingel-dc': {
     theme: 'bingel-dc',
+    viewport: 1280,
   },
   'bingel-int': {
     theme: 'bingel-int',
+    viewport: 1280,
   },
   'clickedu': {
     theme: 'clickedu',
+    viewport: 1280,
   },
   'editorial-suite': {
     theme: 'editorial-suite',
+    viewport: 1280,
   },
   'itslearning': {
     theme: 'itslearning',
+    viewport: 1280,
   },
   'kampus': {
     theme: 'kampus',
+    viewport: 1280,
   },
   'magister': {
     theme: 'magister',
+    viewport: 1280,
   },
   'max': {
     theme: 'max',
+    viewport: 1280,
   },
   'my-digital-book': {
     theme: 'my-digital-book',
+    viewport: 1280,
   },
   'myvanin': {
     theme: 'myvanin',
+    viewport: 1280,
   },
   'neon': {
     theme: 'neon',
+    viewport: 1280,
   },
   'nowa-era': {
     theme: 'nowa-era',
+    viewport: 1280,
   },
   'sanoma-learning': {
     theme: 'sanoma-learning',
+    viewport: 1280,
   },
   'sanoma-utbildning': {
     theme: 'sanoma-utbildning',
+    viewport: 1280,
   },
   'teas': {
     theme: 'teas',
+    viewport: 1280,
   },
 };

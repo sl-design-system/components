e3da01e52858e4625092e9ddaa322a5ff5934a1c
anna-lach <a.lach@nowaera.pl>
2024-06-19 16:15:28 +0200

    format-date changes, stories changes, removed unnecessary styles, checking whether it's a date or not

--- packages/components/format-date/src/format-date.stories.ts
+++ packages/components/format-date/src/format-date.stories.ts
@@ -3,136 +3,95 @@ import '@sl-design-system/button-bar/register.js';
 import { type Meta, type StoryObj } from '@storybook/web-components';
 import { type TemplateResult, html, nothing } from 'lit';
 import '../register.js';
-// import { type InlineMessageVariant } from './inline-message';
-import { FormatDate, type InlineMessageVariant } from './format-date';
+import { FormatDate } from './format-date';
 
-interface Props extends Pick<FormatDate, 'indismissible' | 'variant'> {
-  title: string;
-  button: string;
-  body: string | TemplateResult;
+interface Props extends FormatDate {
+  fallback: string | TemplateResult;
 }
 type Story = StoryObj<Props>;
 
-const variants: InlineMessageVariant[] = ['info', 'success', 'warning', 'danger'];
-
 export default {
-  title: 'Components/Format date',
+  title: 'Utilities/Format date',
   tags: ['draft'],
   args: {
-    button: 'Action',
-    variant: 'info'
+    fallback: 'Action',
+    date: new Date,
   },
   argTypes: {
-    body: {
+    fallback: {
       table: {
         disable: true
       }
     },
-    button: {
-      table: {
-        disable: true
-      }
-    },
-    variant: {
-      control: 'inline-radio',
-      options: variants
-    }
   },
-  render: ({ body, button, indismissible, title, variant }) => html`
-    <sl-format-date ?indismissible=${indismissible} .variant=${variant}>
-      ${title ? html`<span slot="title">${title}</span>` : nothing}
-      ${button ? html`<sl-button fill="outline" slot="action" variant="info">${button}</sl-button>` : nothing} ${body}
+  render: ({ fallback, date }) => html`
+    date? ${date}
+    <sl-format-date .date=${date}>
+      ${fallback}
     </sl-format-date>
   `
 } satisfies Meta<Props>;
 
+// date=${new Date()}
+// date='hj02/02/2022
+
 export const Basic: Story = {
   args: {
-    body: 'The main content of the message'
+    fallback: 'This is not a valid date',
   }
 };
 
 export const Details: Story = {
   args: {
-    title: 'Inline message title',
-    body: html`
-      <style>
-        p {
-          margin-block: 0 0.5rem;
-        }
-        ul {
-          list-style-position: inside;
-          margin: 0;
-          padding: 0;
-        }
-      </style>
-      <p>The main content of the message</p>
-      <ul>
-        <li>Error 1</li>
-        <li>Error 2</li>
-        <li>Error 3</li>
-        <li>Error 4</li>
-      </ul>
-    `
+    fallback: 'Details fallback'
   }
 };
 
-export const All: StoryObj = {
-  render: () => html`
-    <style>
-      #root-inner {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        margin: 0 auto;
-        max-inline-size: min(600px, 80vw);
-      }
-      sl-inline-message::part(title):first-letter {
-        text-transform: capitalize;
-      }
-    </style>
-    <sl-inline-message indismissible variant="info">The main content of the message</sl-inline-message>
-    <sl-inline-message variant="info">The main content of the message</sl-inline-message>
-    <sl-inline-message variant="info">
-      <sl-button fill="outline" slot="action" variant="info">Action</sl-button>
-      The main content of the message
-    </sl-inline-message>
-    <sl-inline-message indismissible variant="info">
-      <sl-button fill="outline" slot="action" variant="info">Action</sl-button>
-      The main content of the message
-    </sl-inline-message>
-    <sl-inline-message variant="info">
-      Duis deserunt ad quis Lorem. Consectetur non deserunt fugiat consequat pariatur amet commodo velit ut est sunt.
-      Exercitation culpa ea officia fugiat culpa laborum sit fugiat esse proident.
-    </sl-inline-message>
-    <sl-inline-message variant="info">
-      <sl-button fill="outline" slot="action" variant="info">Action</sl-button>
-      Duis deserunt ad quis Lorem. Consectetur non deserunt fugiat consequat pariatur amet commodo velit ut est sunt.
-      Exercitation culpa ea officia fugiat culpa laborum sit fugiat esse proident.
-    </sl-inline-message>
-    <sl-inline-message variant="info">
-      <span slot="title">
-        "info" inline message title esse laboris nisi ut quis ullamco dolor elit do commodo ea mollit eu irure.
-      </span>
-      <sl-button fill="outline" slot="action" variant="info">Action</sl-button>
-      Duis ut magna commodo minim cillum voluptate incididunt ea labore adipisicing do ad anim. Incididunt non consequat
-      eiusmod aliqua consequat Lorem eu culpa labore aute laboris eiusmod.
-    </sl-inline-message>
-    ${variants.map(
-      variant => html`
-        <sl-inline-message variant=${variant}>
-          <sl-button fill="outline" slot="action" .variant=${variant}>Action</sl-button>
-          The main content of the message
-        </sl-inline-message>
-        <sl-inline-message variant=${variant}>
-          <span slot="title">
-            "info" inline message title esse laboris nisi ut quis ullamco dolor elit do commodo ea mollit eu irure.
-          </span>
-          <sl-button fill="outline" slot="action" .variant=${variant}>Action</sl-button>
-          Duis ut magna commodo minim cillum voluptate incididunt ea labore adipisicing do ad anim. Incididunt non
-          consequat eiusmod aliqua consequat Lorem eu culpa labore aute laboris eiusmod.
-        </sl-inline-message>
-      `
-    )}
-  `
-};
+// TODO in All - examples of all in one place
+
+// export const All: StoryObj = {
+//   render: () => html`
+//     <style>
+//       #root-inner {
+//         display: flex;
+//         flex-direction: column;
+//         gap: 1rem;
+//         margin: 0 auto;
+//         max-inline-size: min(600px, 80vw);
+//       }
+//       sl-inline-message::part(title):first-letter {
+//         text-transform: capitalize;
+//       }
+//     </style>
+//     <sl-inline-message indismissible>The main content of the message</sl-inline-message>
+//     <sl-inline-message>The main content of the message</sl-inline-message>
+//     <sl-inline-message>
+//       <sl-button fill="outline" slot="action">Action</sl-button>
+//       The main content of the message
+//     </sl-inline-message>
+//     <sl-inline-message indismissible>
+//       <sl-button fill="outline" slot="action">Action</sl-button>
+//       The main content of the message
+//     </sl-inline-message>
+//     <sl-inline-message>
+//       Duis deserunt ad quis Lorem. Consectetur non deserunt fugiat consequat pariatur amet commodo velit ut est sunt.
+//       Exercitation culpa ea officia fugiat culpa laborum sit fugiat esse proident.
+//     </sl-inline-message>
+//     <sl-inline-message>
+//       <sl-button fill="outline" slot="action">Action</sl-button>
+//       Duis deserunt ad quis Lorem. Consectetur non deserunt fugiat consequat pariatur amet commodo velit ut est sunt.
+//       Exercitation culpa ea officia fugiat culpa laborum sit fugiat esse proident.
+//     </sl-inline-message>
+//     <sl-inline-message>
+//       <span slot="title">
+//         "info" inline message title esse laboris nisi ut quis ullamco dolor elit do commodo ea mollit eu irure.
+//       </span>
+//       <sl-button fill="outline" slot="action">Action</sl-button>
+//       Duis ut magna commodo minim cillum voluptate incididunt ea labore adipisicing do ad anim. Incididunt non consequat
+//       eiusmod aliqua consequat Lorem eu culpa labore aute laboris eiusmod.
+//     </sl-inline-message>
+//   `
+// };
+
+
+// TODO: make separated file with formatting date function
--- packages/components/format-date/src/format-date.ts
+++ packages/components/format-date/src/format-date.ts
@@ -1,7 +1,7 @@
 import { LocaleMixin } from '@sl-design-system/shared';
 import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
 import { property } from 'lit/decorators.js';
-import styles from './inline-message.scss.js';
+// import styles from './inline-message.scss.js';
 // import {Locale} from "@sl-design-system/shared/src/mixins";
 // import { locale } from "@sl-design-system/shared/src/mixins/locale-mixin.js";
 
@@ -21,8 +21,6 @@ export function formatDate(date: Date, options: FormatDateOptions): string {
   return new Intl.DateTimeFormat(locale, rest).format(date);
 }
 
-export type InlineMessageVariant = 'info' | 'success' | 'warning' | 'danger';
-
 /**
  * A format date component for formatting date and time.
  *
@@ -40,7 +38,7 @@ export class FormatDate extends LocaleMixin(LitElement) {
   // }
 
   /** @internal */
-  static override styles: CSSResultGroup = styles;
+ // static override styles: CSSResultGroup = styles;
 
   #date?: Date = new Date();
 
@@ -51,73 +49,144 @@ export class FormatDate extends LocaleMixin(LitElement) {
 
   @property() year?: '2-digit' | 'numeric';
 
+  /** The format for displaying the month. */
+  @property() month?: 'numeric' | '2-digit' | 'narrow' | 'short' | 'long';
+
+  /** The format for displaying the day. */
+  @property() day?: 'numeric' | '2-digit';
+
+  /** The format for displaying the hour. */
+  @property() hour?: 'numeric' | '2-digit';
+
+  /** The format for displaying the minute. */
+  @property() minute?: 'numeric' | '2-digit';
+
+  /** The format for displaying the second. */
+  @property() second?: 'numeric' | '2-digit';
+
+  /** The format for displaying the time. */
+  @property({ attribute: 'time-zone-name' }) timeZoneName?: 'short' | 'long';
+
+  /** The time zone to express the time in. */
+  @property({ attribute: 'time-zone' }) timeZone?: string;
+
+  // /** When set, 24 hour time will always be used. */
+  // @property({ attribute: 'hour-format' }) hourFormat: 'auto' | '12' | '24' = 'auto';
+
+  /** When set, 24 hour time will always be used. */
+  @property({ type: Boolean, attribute: 'hour-12' }) hour12?: boolean;
+
+  // TODO: month, day, dayPeriod, hour, minute, second, fractionalseconddigits??, timeZoneName, formatMatcher??, datestyle, timestyle
+  // TODO; calendar, numberingSystem, hour12, hourCycle, timeZone
+
   /** Observe the size and determine where to place the action button if present. */
   // #observer = new ResizeObserver(() => this.#onResize());
 
   /** Will hide the close button if set. */
   @property({ type: Boolean, reflect: true }) indismissible?: boolean;
 
-  /** @internal If the action is missing, we need to hide the action part. */
-  @property({ type: Boolean, attribute: 'no-action', reflect: true }) noAction = true;
+  // /** @internal If the action is missing, we need to hide the action part. */
+  // @property({ type: Boolean, attribute: 'no-action', reflect: true }) noAction = true;
 
   /** @internal If the title is missing, the content needs to be placed where the title should be. */
   @property({ type: Boolean, attribute: 'no-title', reflect: true }) noTitle = true;
 
-  /** The variant of the inline message. */
-  @property({ reflect: true }) variant: InlineMessageVariant = 'info';
-
   /** @internal Calculates the height of the title and wraps the button if longer than 1 line. */
   @property({ type: Boolean, attribute: 'wrap-action', reflect: true }) wrapAction?: boolean;
 
-  /** @internal The name of the icon, depending on the variant. */
-  get iconName(): string {
-    switch (this.variant) {
-      case 'success':
-        return 'circle-check-solid';
-      case 'warning':
-        return 'octagon-exclamation-solid';
-      case 'danger':
-        return 'triangle-exclamation-solid';
-      default:
-        return 'info';
+  set date(value: number | string | Date | undefined | null) {
+    const oldVal = this.#date;
+    console.log('oldVal', oldVal, 'value????',  value);
+
+    if (value instanceof Date) {
+      this.#date = value;
+    } else if (typeof value === 'number') {
+      this.#date = new Date(value);
+    } else if (typeof value === 'string' && this.#isDateValid(value)) {
+      this.#date = new Date(value); //value;
+      // this.#date = dateConverter.fromAttribute(value);
+    } else {
+      this.#date = undefined;
     }
+
+    console.log('date in set date?', this.#date);
+
+    this.requestUpdate('date', oldVal);
   }
 
-  // override connectedCallback(): void {
-  //   // super.connectedCallback();
-  //
-  //   this.#observer.observe(this);
-  // }
-  //
-  // override disconnectedCallback(): void {
-  //   this.#observer.disconnect();
-  //
-  //   // super.disconnectedCallback();
-  // }
+  /** The date/time to format. If not set, the current date and time will be used. */
+  @property()
+  get date(): Date | undefined {
+    return this.#date;
+  }
 
-  // override updated(changes: PropertyValues<this>): void {
-  //   super.updated(changes);
-  //
-  //   if (changes.has('variant')) {
-  //     this.setAttribute('role', ['danger', 'warning'].includes(this.variant) ? 'alert' : 'status');
+  // /** @internal The name of the icon, depending on the variant. */
+  // get iconName(): string {
+  //   switch (this.variant) {
+  //     case 'success':
+  //       return 'circle-check-solid';
+  //     case 'warning':
+  //       return 'octagon-exclamation-solid';
+  //     case 'danger':
+  //       return 'triangle-exclamation-solid';
+  //     default:
+  //       return 'info';
   //   }
   // }
 
   override render(): TemplateResult {
     // Intl.DateTimeFormatOptions()
     // new Intl.DateTimeFormat(Locale)
-    console.log('date', this.#date, 'with locales', new Intl.DateTimeFormat());
+    console.log('thiiiis.date', this.date);
+    console.log('date', this.#date, 'with locales', new Intl.DateTimeFormat(), new Intl.DateTimeFormat('pl').format(this.#date));
     return html`
-      <div class="wrapper">
+      ${(!this.date || !this.#isDateValid(this.date)) ? html`<slot></slot>` : this.date}
+      <div class="wrapper" style="background: lightskyblue;">
         ${this.#date}
         <slot></slot>
         ${formatDate(new Date(), { locale: 'pl', weekday: 'long', timeZoneName: 'short' })}
+        dateStyle: ${formatDate(new Date(), { locale: 'en', dateStyle: 'long' })}
+        dateValid??? ${this.#isDateValid(this.#date)}
+        22dateValid??? ${this.#isDateValid('unknown')}
       </div>
+      <div style="background: lightyellow;">
+      ${(!this.date || !this.#isDateValid(this.date))
+        ? html`slooooot <slot></slot>`
+        : this.date}
+        ${this.#date}
+      </div>
+      <div style="background: lightgreen;">
+        ${(!this.date)
+          ? html`test<slot></slot>`
+          : this.date}
+        ${this.#date}
+      </div>
+      this.dejt? ${this.date} ${this.#formatDateTime(new Date)}
+      <slot></slot>
     `;
   } // ${formatDate(this.#date, {locale: 'pl'})}
   // TODO: test timezone eg. UTC
+  // TODO: render check if this is a date or sth else, if not retun fallback
 
-  // #onActionSlotChange(event: Event & { target: HTMLSlotElement }): void {
+  // TODO hour12 undefined when not set?
+
+  #formatDateTime(date: Date): string { // TODO: in separated file?
+    const hour12 = this.hour12 ? this.hour12 : undefined;
+    console.log('hour12', hour12);
+    return date.toString();
+  }
+
+  // #isDateValid(date: string): boolean {
+  //   return !isNaN(new Date(date));
+  // }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument
+  #isDateValid(date: any): boolean { // TODO: change any to sth else
+    const parsedDate = new Date(date);
+    return !isNaN(parsedDate.getTime());
+  }
+
+  // #onSlotChange(event: Event & { target: HTMLSlotElement }): void {
   //   this.noAction = !event.target.assignedElements({ flatten: true }).length;
   // }
 }

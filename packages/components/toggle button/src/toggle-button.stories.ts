f4b434db356ef3b02779168847a1aa19211dbbcb
Diana Broeders <diana.broeders@sanoma.com>
2024-06-27 16:54:08 +0200

    get it working

--- packages/components/toggle button/src/toggle-button.stories.ts
+++ packages/components/toggle button/src/toggle-button.stories.ts
@@ -1,20 +1,53 @@
+import { faGear as farGear } from '@fortawesome/pro-regular-svg-icons';
+import { faEyeSlash, faGear as fasGear } from '@fortawesome/pro-solid-svg-icons';
+
+import { Icon } from '@sl-design-system/icon';
 import '@sl-design-system/icon/register.js';
 import { type Meta, type StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import '../register.js';
-import { type ToggleButton } from './toggle-button.js';
+import { type ToggleButton, ToggleButtonFill, ToggleButtonSize } from './toggle-button.js';
 
-type Props = Pick<ToggleButton, 'size' | 'variant'> & { icon?: boolean; text?: string };
+type Props = Pick<ToggleButton, 'size' | 'fill' | 'pressed'>;
 type Story = StoryObj<Props>;
+
+const fills: ToggleButtonFill[] = ['outline', 'ghost'];
+const sizes: ToggleButtonSize[] = ['sm', 'md', 'lg'];
+
+Icon.register(fasGear, farGear, faEyeSlash);
 
 export default {
   title: 'Components/ToggleButton',
   tags: ['draft'],
-  render: () => html`
-    <sl-toggle-button>
-      <sl-icon name="check"></sl-icon>
-    </sl-toggle-button>
-  `
+  args: {
+    pressed: false,
+    size: 'md',
+    fill: 'ghost'
+  },
+  argTypes: {
+    size: {
+      control: 'inline-radio',
+      options: sizes
+    },
+    fill: {
+      control: 'inline-radio',
+      options: fills
+    }
+  },
+  render: ({ fill, size, pressed }) => {
+    const onToggle = (event: Event & { target: ToggleButton }): void => {
+      console.log(event.target.pressed);
+    };
+
+    return html`
+      <sl-toggle-button .fill=${fill} .size=${size} ?pressed=${pressed} @sl-toggle=${onToggle}>
+        <sl-icon name="far-gear"></sl-icon>
+        <sl-icon name="fas-gear" slot="pressed"></sl-icon>
+      </sl-toggle-button>
+    `;
+  }
 } satisfies Meta<Props>;
 
 export const Basic: Story = {};
+
+export const All: Story = {};
--- packages/components/toggle button/src/toggle-button.ts
+++ packages/components/toggle button/src/toggle-button.ts
@@ -1,3 +1,5 @@
+import { type EventEmitter, EventsController, event } from '@sl-design-system/shared';
+import { type SlFocusEvent, type SlToggleEvent } from '@sl-design-system/shared/events.js';
 import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import styles from './toggle-button.scss.js';
@@ -10,7 +12,7 @@ declare global {
 
 export type ToggleButtonEmphasis = 'subtle' | 'bold';
 export type ToggleButtonSize = 'sm' | 'md' | 'lg';
-export type ToggleButtonVariant = 'ghost' | 'outline';
+export type ToggleButtonFill = 'ghost' | 'outline';
 
 /**
  * Show totals at a glance or labels contents with a tag.
@@ -22,32 +24,91 @@ export type ToggleButtonVariant = 'ghost' | 'outline';
  * @slot default - Contents of the toggle-button
  */
 export class ToggleButton extends LitElement {
-  /** @internal */
+  /** @private */
   static override styles: CSSResultGroup = styles;
+
+  // eslint-disable-next-line no-unused-private-class-members
+  #events = new EventsController(this, {
+    click: this.#onClick,
+    keydown: this.#onKeydown
+  });
+
+  /** @internal Emits when the button item has been toggled. */
+  @event({ name: 'sl-toggle' }) toggleEvent!: EventEmitter<SlToggleEvent<boolean>>;
+
+  /** @internal Emits when the component receives focus. */
+  @event({ name: 'sl-focus' }) focusEvent!: EventEmitter<SlFocusEvent>;
 
   /** The size of the toggle-button component. */
   @property({ reflect: true }) size: ToggleButtonSize = 'md';
 
   /** The variant of the toggle-button. */
-  @property({ reflect: true }) variant: ToggleButtonVariant = 'ghost';
+  @property({ reflect: true }) fill: ToggleButtonFill = 'ghost';
 
   /** The variant of the toggle-button. */
-  @property({ reflect: true }) pressed = false;
+  @property({ type: Boolean, reflect: true }) pressed = false;
+
+  /** Whether the switch is disabled; when set no interaction is possible. */
+  @property({ type: Boolean, reflect: true }) disabled?: boolean;
 
   override render(): TemplateResult {
-    return html`<sl-button><slot @slotchange=${this.#onSlotChange}></slot></sl-button>`;
+    return html`
+      <slot @slotchange=${this.#onSlotChange} class="default"></slot>
+      <slot @slotchange=${this.#onSlotChange} name="pressed"></slot>
+    `;
   }
 
   #onSlotChange(event: Event & { target: HTMLSlotElement }): void {
-    const elements = event.target.assignedElements({ flatten: true }),
-      icon = elements.length === 1 && elements[0].tagName === 'SL-ICON',
-      text = event.target
-        .assignedNodes({ flatten: true })
-        .filter(node => node.nodeType === Node.TEXT_NODE)
-        .map(node => node.textContent?.trim())
-        .join('');
+    const assignedNodes = event.target.assignedNodes({ flatten: true });
+    this.#setIconProperties(assignedNodes);
+  }
 
-    // Toggle the round attribute if the toggle-button contains only an icon, or a single character
-    this.toggleAttribute('round', (icon && text === '') || (!icon && text?.length === 1));
+  #onClick(event: Event): void {
+    if (this.disabled) {
+      return;
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.pressed = !this.pressed;
+    this.toggleEvent.emit(this.pressed);
+  }
+
+  #onKeydown(event: KeyboardEvent): void {
+    if (['Enter', ' '].includes(event.key)) {
+      this.#onClick(event);
+    }
+  }
+
+  #hasOnlyIconAsChild(el: HTMLElement): boolean {
+    return (
+      (el.textContent || '').trim().length === 0 && el.children.length === 1 && el.children[0].nodeName === 'SL-ICON'
+    );
+  }
+
+  #setIconProperties(assignedNodes: Node[]): void {
+    const filteredNodes = assignedNodes.filter(node => {
+      return node.nodeType === Node.ELEMENT_NODE || (node.textContent && node.textContent.trim().length > 0);
+    });
+
+    let hasIcon = false;
+
+    filteredNodes.forEach(node => {
+      const el = node as HTMLElement;
+      if (el.nodeName === 'SL-ICON') {
+        el.setAttribute('size', this.size);
+      } else if (this.#hasOnlyIconAsChild(el)) {
+        (el.children[0] as HTMLElement).setAttribute('size', this.size);
+      }
+    });
+
+    if (filteredNodes.length === 1) {
+      const el = filteredNodes[0] as HTMLElement;
+      // This button is icon-only if it only contains an icon.
+      hasIcon = el.nodeName === 'SL-ICON' || this.#hasOnlyIconAsChild(el);
+    }
+
+    this.toggleAttribute('icon-only', hasIcon);
   }
 }

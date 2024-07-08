d2ea76df832df44610f87c902034e153b6695b9e
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-07-08 13:52:21 +0200

    🐄

--- packages/components/calendar/src/month-view.stories.ts
+++ packages/components/calendar/src/month-view.stories.ts
@@ -4,22 +4,35 @@ import { ifDefined } from 'lit/directives/if-defined.js';
 import '../register.js';
 import { type MonthView } from './month-view.js';
 
-type Props = Pick<MonthView, 'firstDayOfWeek' | 'month' | 'showWeekNumbers'>;
+type Props = Pick<MonthView, 'firstDayOfWeek' | 'locale' | 'month' | 'showWeekNumbers'>;
 type Story = StoryObj<Props>;
 
 export default {
   title: 'Components/Month view',
   tags: ['draft'],
   args: {
+    month: new Date(),
     showWeekNumbers: false
   },
   argTypes: {
     firstDayOfWeek: {
       control: 'number'
+    },
+    locale: {
+      control: 'inline-radio',
+      options: ['de', 'en-GB', 'es', 'fi', 'it', 'nl', 'no', 'pl', 'sv']
+    },
+    month: {
+      control: 'date'
     }
   },
-  render: ({ firstDayOfWeek, showWeekNumbers }) => html`
-    <sl-month-view ?show-week-numbers=${showWeekNumbers} first-day-of-week=${ifDefined(firstDayOfWeek)}></sl-month-view>
+  render: ({ firstDayOfWeek, month, locale, showWeekNumbers }) => html`
+    <sl-month-view
+      ?show-week-numbers=${showWeekNumbers}
+      first-day-of-week=${ifDefined(firstDayOfWeek)}
+      locale=${ifDefined(locale)}
+      month=${ifDefined(month ? month.toISOString() : undefined)}
+    ></sl-month-view>
   `
 } satisfies Meta<Props>;
 

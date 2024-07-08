df1a06dee8a4dbc03eb6aa3f123394f2d9637063
anna-lach <a.lach@nowaera.pl>
2024-07-08 15:39:56 +0200

    tarnslation fix

--- packages/locales/src/nl.ts
+++ packages/locales/src/nl.ts
@@ -5,9 +5,7 @@ import { str } from '@lit/localize';
 
 export const templates = {
   s091d3d07b5b3076f: 'Ok',
-  s129fac0ae1df7c8f: 'waarschuwing',
   s13fdff160ffad275: 'Wis tekst',
-  s19fe06e3408e53d0: 'succes',
   s2ceb11be2290bb1b: 'Annuleren',
   s39bba60b440fcf00: 'verplicht',
   s4215e0aa2fa8f5b4: 'Type hier om te filteren',
@@ -20,10 +18,8 @@ export const templates = {
   s62aa2ba3fe47ece4: 'Vink tenminste één optie aan.',
   s6580790b036f0c6f: 'actief',
   s6abb1cd87fe0114e: 'Home',
-  s7ce4fd9430e80cea: 'waarde',
   s8079b71872346425: 'Voer een waarde in.',
   s864c948d4629240f: 'Toon alles',
-  s9f7452dd75d54d31: 'fout',
   sa447e2f801a7eb2d: 'Kies een optie.',
   sa996297f6a208e98: 'Kruimelpad',
   sb85774dc5d18ff0f: 'Bevestig',

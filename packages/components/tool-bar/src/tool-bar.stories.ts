4187745871ec99eaa9f9e185ee7fb44636f295a6
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-07-10 20:22:19 +0200

    🌏

--- packages/components/tool-bar/src/tool-bar.stories.ts
+++ packages/components/tool-bar/src/tool-bar.stories.ts
@@ -1,17 +1,35 @@
+import '@sl-design-system/button/register.js';
+import '@sl-design-system/select/register.js';
 import { type Meta, type StoryObj } from '@storybook/web-components';
-import { html } from 'lit';
+import { type TemplateResult, html } from 'lit';
 import '../register.js';
 import { type ToolBar } from './tool-bar.js';
 
-type Props = ToolBar;
+type Props = Pick<ToolBar, 'disabled'> & { items?(): TemplateResult };
 type Story = StoryObj<Props>;
 
 export default {
   title: 'Components/Tool bar',
   tags: ['draft'],
-  render: () => {
-    return html`<sl-tool-bar></sl-tool-bar>`;
+  args: {
+    disabled: false
+  },
+  argTypes: {
+    items: {
+      table: { disable: true }
+    }
+  },
+  render: ({ disabled, items }) => {
+    return html`<sl-tool-bar ?disabled=${disabled}>${items?.()}</sl-tool-bar>`;
   }
 } satisfies Meta<Props>;
 
-export const Basic: Story = {};
+export const Basic: Story = {
+  args: {
+    items: () => html`
+      <sl-button>Button 1</sl-button>
+      <sl-button>Button 2</sl-button>
+      <sl-button>Button 3</sl-button>
+    `
+  }
+};
--- packages/components/tool-bar/src/tool-bar.ts
+++ packages/components/tool-bar/src/tool-bar.ts
@@ -1,4 +1,5 @@
 import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
+import { property } from 'lit/decorators.js';
 import styles from './tool-bar.scss.js';
 
 declare global {
@@ -11,7 +12,22 @@ export class ToolBar extends LitElement {
   /** @internal */
   static override styles: CSSResultGroup = styles;
 
+  /** If true, the tool-bar is disabled and cannot be interacted with. */
+  @property({ type: Boolean, reflect: true }) disabled?: boolean;
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    this.setAttribute('role', 'toolbar');
+  }
+
   override render(): TemplateResult {
-    return html`<slot>HOHOHO</slot>`;
+    return html`<slot @slotchange=${this.#onSlotChange}></slot>`;
+  }
+
+  #onSlotChange(event: Event & { target: HTMLSlotElement }) {
+    const elements = event.target.assignedElements({ flatten: true });
+
+    console.log(elements);
   }
 }

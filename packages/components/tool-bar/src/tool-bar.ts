4e92acf72396c989516e44298fa0d023c1482432
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-07-11 09:38:31 +0200

    🚍

--- packages/components/tool-bar/src/tool-bar.ts
+++ packages/components/tool-bar/src/tool-bar.ts
@@ -1,3 +1,6 @@
+import { type ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
+import { Icon } from '@sl-design-system/icon';
+import { MenuButton } from '@sl-design-system/menu';
 import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import styles from './tool-bar.scss.js';
@@ -8,9 +11,20 @@ declare global {
   }
 }
 
-export class ToolBar extends LitElement {
+export class ToolBar extends ScopedElementsMixin(LitElement) {
+  /** @internal */
+  static get scopedElements(): ScopedElementsMap {
+    return {
+      'sl-icon': Icon,
+      'sl-menu-button': MenuButton
+    };
+  }
+
   /** @internal */
   static override styles: CSSResultGroup = styles;
+
+  // Observer changes to the size of the element.
+  #observer = new ResizeObserver(() => this.#onResize());
 
   /** If true, the tool-bar is disabled and cannot be interacted with. */
   @property({ type: Boolean, reflect: true }) disabled?: boolean;
@@ -19,10 +33,22 @@ export class ToolBar extends LitElement {
     super.connectedCallback();
 
     this.setAttribute('role', 'toolbar');
+
+    this.#observer.observe(this);
+  }
+
+  override disconnectedCallback(): void {
+    this.#observer.disconnect();
+
+    super.disconnectedCallback();
   }
 
   override render(): TemplateResult {
     return html`<slot @slotchange=${this.#onSlotChange}></slot>`;
+  }
+
+  #onResize(): void {
+    console.log('resize');
   }
 
   #onSlotChange(event: Event & { target: HTMLSlotElement }) {

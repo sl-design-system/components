ce57945f87099c17fe7f45b917d34ad209a24a63
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-09-01 11:07:31 +0200

    🖍

--- packages/components/listbox/src/option-group.spec.ts
+++ packages/components/listbox/src/option-group.spec.ts
@@ -13,4 +13,19 @@ describe('sl-option-group', () => {
   it('should have a group role', () => {
     expect(el).to.have.attribute('role', 'group');
   });
+
+  it('should not have a label by default', () => {
+    expect(el.label).to.be.undefined;
+    expect(el.renderRoot.querySelector('.label')).to.not.exist;
+  });
+
+  it('should have a label when set', async () => {
+    el.label = 'Group label';
+    await el.updateComplete;
+
+    const label = el.renderRoot.querySelector('.label');
+
+    expect(label).to.exist;
+    expect(label).to.have.text('Group label');
+  });
 });
--- packages/components/listbox/src/option.spec.ts
+++ packages/components/listbox/src/option.spec.ts
@@ -13,4 +13,45 @@ describe('sl-option', () => {
   it('should have an option role', () => {
     expect(el).to.have.attribute('role', 'option');
   });
+
+  it('should not be disabled by default', () => {
+    expect(el).not.to.have.attribute('disabled');
+    expect(el.disabled).not.to.be.true;
+  });
+
+  it('should be disabled when set', async () => {
+    el.disabled = true;
+    await el.updateComplete;
+
+    expect(el).to.have.attribute('disabled');
+  });
+
+  it('should not be selected by default', () => {
+    expect(el).not.to.have.attribute('selected');
+    expect(el.selected).not.to.be.true;
+  });
+
+  it('should be selected when set', async () => {
+    el.selected = true;
+    await el.updateComplete;
+
+    expect(el).to.have.attribute('selected');
+  });
+
+  it('should have a hidden check icon by default', () => {
+    const icon = el.renderRoot.querySelector('sl-icon[name="check"]');
+
+    expect(icon).to.exist;
+    expect(icon).to.have.style('visibility', 'hidden');
+  });
+
+  it('should show the check icon when selected', async () => {
+    el.selected = true;
+    await el.updateComplete;
+
+    const icon = el.renderRoot.querySelector('sl-icon[name="check"]');
+
+    expect(icon).to.exist;
+    expect(icon).to.have.style('visibility', 'visible');
+  });
 });

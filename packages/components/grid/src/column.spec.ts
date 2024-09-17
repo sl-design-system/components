fc4791c1b18d7c1ff3679fd722ea24535e800a01
Diana Broeders <diana.broeders@sanoma.com>
2024-09-17 09:34:48 +0200

    Feature/1364 data grid review (#1423)
    * removed default value, added tests
    
    * added tests for column-group
    
    * fixed column width and made sl-column next to sl-column-group possible
    
    * changed around the odd and even row colours to better match header
    
    * added comments, attempt to improve sorter
    
    * attempt to write tests for sorter + clean up code
    
    * changeset added
    
    * filter column tests (in progress)
    
    * filter column tests changes, initial test for filter
    
    * filter and filter column unit tests changes
    
    * filter tests changes
    
    * filter tests updates (not working yet)
    
    * trying to fix filter tests
    
    * filter column tests update, still cannot check options
    
    * filter and filter column tests changes
    
    * 🚂
    
    * 🛥
    
    * filter column tests changes
    
    * filter column tests, filter test change
    
    * Trying to change filter test example
    
    * trying to improve the example in filter tests
    
    * filter test updated
    
    * new tests, removed unnecessary elements
    
    * cleanup
    
    * missing export
    
    * filter and filter column changes after review
    
    * review comments
    
    * fixed columns widths in non-grouped columns next to grouped columns
    
    * fix build
    
    ---------
    
    Co-authored-by: anna-lach <a.lach@nowaera.pl>
    Co-authored-by: Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>

---
+++ packages/components/grid/src/column.spec.ts
@@ -0,0 +1,104 @@
+import { setupIgnoreWindowResizeObserverLoopErrors } from '@lit-labs/virtualizer/support/resize-observer-errors.js';
+import { expect, fixture } from '@open-wc/testing';
+import { Avatar } from '@sl-design-system/avatar';
+import '@sl-design-system/avatar/register.js';
+import { html } from 'lit';
+import { Person } from 'tools/example-data/index.js';
+import '../register.js';
+import { GridColumnDataRenderer } from './column.js';
+import { type Grid } from './grid.js';
+
+setupIgnoreWindowResizeObserverLoopErrors(beforeEach, afterEach);
+
+describe('sl-column', () => {
+  let el: Grid;
+  let cells: HTMLElement[];
+
+  describe('defaults', () => {
+    beforeEach(async () => {
+      el = await fixture(html`
+        <sl-grid>
+          <sl-grid-column path="firstName"></sl-grid-column>
+          <sl-grid-column path="lastName"></sl-grid-column>
+          <sl-grid-column path="age" align="end" grow="3" header="Current age"></sl-grid-column>
+        </sl-grid>
+      `);
+      el.items = [
+        { firstName: 'John', lastName: 'Doe', age: 20 },
+        { firstName: 'Jane', lastName: 'Smith', age: 40 }
+      ];
+      await el.updateComplete;
+
+      // Give grid time to render the table structure
+      await new Promise(resolve => setTimeout(resolve, 100));
+      await el.updateComplete;
+
+      cells = Array.from(el.renderRoot.querySelectorAll('tbody tr:first-of-type td'));
+    });
+
+    it('should render column headers', () => {
+      const columns = Array.from(el.renderRoot.querySelectorAll('th')).map(col => col.textContent);
+
+      expect(columns).to.deep.equal(['First name', 'Last name', 'Current age']);
+    });
+
+    it('should have the right justify-content value', () => {
+      expect(cells.map(cell => getComputedStyle(cell).justifyContent)).to.deep.equal(['start', 'start', 'end']);
+    });
+
+    it('should have the right grow value', () => {
+      expect(cells.map(cell => getComputedStyle(cell).flexGrow)).to.deep.equal(['1', '1', '3']);
+    });
+
+    it('should have the right parts', () => {
+      expect(cells.map(cell => cell.getAttribute('part'))).to.deep.equal([
+        'data first-name',
+        'data last-name',
+        'data age'
+      ]);
+    });
+  });
+
+  describe('custom renderer', () => {
+    beforeEach(async () => {
+      const avatarRenderer: GridColumnDataRenderer<Person> = ({ firstName, lastName }) => {
+        return html`<sl-avatar .displayName=${[firstName, lastName].join(' ')} size="sm"></sl-avatar>`;
+      };
+
+      el = await fixture(html`
+        <sl-grid>
+          <sl-grid-column
+            header="Person"
+            .renderer=${avatarRenderer}
+            .scopedElements=${{
+              'sl-avatar': Avatar
+            }}
+          ></sl-grid-column>
+          <sl-grid-column path="age" parts="number"></sl-grid-column>
+        </sl-grid>
+      `);
+      el.items = [
+        { firstName: 'John', lastName: 'Doe', age: 20 },
+        { firstName: 'Jane', lastName: 'Smith', age: 40 }
+      ];
+      await el.updateComplete;
+
+      // Give grid time to render the table structure
+      await new Promise(resolve => setTimeout(resolve, 100));
+      await el.updateComplete;
+
+      cells = Array.from(el.renderRoot.querySelectorAll('tbody tr:first-of-type td'));
+    });
+
+    it('should render the elements set with the custom renderer', () => {
+      const avatar = cells[0].querySelector('sl-avatar') as Avatar;
+
+      expect(avatar).to.exist;
+      expect(avatar?.shadowRoot?.querySelector('[part="name"]')?.textContent).to.equal('John Doe');
+    });
+
+    it('should have the right parts, including one set on the column', () => {
+      expect(cells.map(cell => cell.getAttribute('part'))).to.deep.equal(['data', 'data number age']);
+    });
+  });
+});

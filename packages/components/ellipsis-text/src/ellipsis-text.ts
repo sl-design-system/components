abaf2ccc5a54bcf2ac757086aa561e6bbcce4f55
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-10-01 18:05:29 +0200

    🎁

--- packages/components/ellipsis-text/src/ellipsis-text.ts
+++ packages/components/ellipsis-text/src/ellipsis-text.ts
@@ -18,7 +18,55 @@ export class EllipsisText extends ScopedElementsMixin(LitElement) {
   /** @internal */
   static override styles: CSSResultGroup = styles;
 
+  /** Observe size changes. */
+  #observer = new ResizeObserver(() => this.#onResize());
+
+  /** The lazy tooltip. */
+  #tooltip?: Tooltip | (() => void);
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    this.#observer.observe(this);
+  }
+
+  override disconnectedCallback(): void {
+    this.#observer.disconnect();
+
+    if (this.#tooltip instanceof Tooltip) {
+      this.#tooltip.remove();
+    } else if (this.#tooltip) {
+      this.#tooltip();
+    }
+
+    this.#tooltip = undefined;
+
+    super.disconnectedCallback();
+  }
+
   override render(): TemplateResult {
     return html`<slot></slot>`;
+  }
+
+  #onResize(): void {
+    if (this.offsetWidth < this.scrollWidth) {
+      this.#tooltip ||= Tooltip.lazy(
+        this,
+        tooltip => {
+          this.#tooltip = tooltip;
+          tooltip.position = 'bottom';
+          tooltip.textContent = this.textContent?.trim() || '';
+        },
+        { context: this.shadowRoot! }
+      );
+    } else if (this.#tooltip instanceof Tooltip) {
+      this.removeAttribute('aria-describedby');
+
+      this.#tooltip.remove();
+      this.#tooltip = undefined;
+    } else if (this.#tooltip) {
+      this.#tooltip();
+      this.#tooltip = undefined;
+    }
   }
 }

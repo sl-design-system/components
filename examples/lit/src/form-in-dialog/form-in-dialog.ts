b21b9551317f12d5018684643a3ee4e785ddaf01
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-10-05 11:06:03 +0200

    🌻

--- examples/lit/src/form-in-dialog/form-in-dialog.ts
+++ examples/lit/src/form-in-dialog/form-in-dialog.ts
@@ -1,18 +1,15 @@
-import { type ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
-import { Button } from '@sl-design-system/button';
+import { type ScopedElementsMap } from '@open-wc/scoped-elements/lit-element.js';
 import { Dialog } from '@sl-design-system/dialog';
 import { Form, FormController, FormField, FormValidationErrors } from '@sl-design-system/form';
 import { TextField } from '@sl-design-system/text-field';
-import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
-import { query } from 'lit/decorators.js';
+import { type CSSResultGroup, type TemplateResult, html } from 'lit';
 import styles from './form-in-dialog.scss.js';
 
-export class FormInDialog extends ScopedElementsMixin(LitElement) {
+export class FormInDialog extends Dialog {
   /** @internal */
-  static get scopedElements(): ScopedElementsMap {
+  static override get scopedElements(): ScopedElementsMap {
     return {
-      'sl-button': Button,
-      'sl-dialog': Dialog,
+      ...super.scopedElements,
       'sl-form': Form,
       'sl-form-field': FormField,
       'sl-form-validation-errors': FormValidationErrors,
@@ -26,36 +23,35 @@ export class FormInDialog extends ScopedElementsMixin(LitElement) {
   /** Controller for managing form state. */
   #form = new FormController(this);
 
-  /** The dialog component. */
-  @query('sl-dialog') dialog!: Dialog;
+  override renderHeader(): TemplateResult {
+    return super.renderHeader('Form in dialog');
+  }
 
-  override render(): TemplateResult {
+  override renderBody(): TemplateResult {
     return html`
-      <sl-dialog>
-        <span slot="title">Title</span>
-        <sl-form>
-          <sl-form-field label="First name">
-            <sl-text-field autofocus name="firstName" required></sl-text-field>
-          </sl-form-field>
-          <sl-form-field label="Last name">
-            <sl-text-field name="lastName" required></sl-text-field>
-          </sl-form-field>
+      <sl-form>
+        <sl-form-field label="First name">
+          <sl-text-field autofocus name="firstName" required></sl-text-field>
+        </sl-form-field>
+        <sl-form-field label="Last name">
+          <sl-text-field name="lastName" required></sl-text-field>
+        </sl-form-field>
 
-          <sl-form-validation-errors .controller=${this.#form}></sl-form-validation-errors>
-        </sl-form>
-        <sl-button sl-dialog-close fill="ghost" slot="actions">Cancel</sl-button>
-        <sl-button @click=${this.#onSave} slot="actions" variant="primary">Save</sl-button>
-      </sl-dialog>
+        <sl-form-validation-errors .controller=${this.#form}></sl-form-validation-errors>
+      </sl-form>
     `;
   }
 
-  showModal(): void {
-    this.dialog.showModal();
+  override renderActions(): TemplateResult {
+    return html`
+      <sl-button sl-dialog-close fill="ghost" slot="actions">Cancel</sl-button>
+      <sl-button @click=${this.#onSave} slot="actions" variant="primary">Save</sl-button>
+    `;
   }
 
   #onSave(): void {
     if (this.#form.reportValidity()) {
-      this.dialog.close();
+      this.close();
     }
   }
 }

c4502b3f71eb162f5933f4aa5f0821742b0ab4ea
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-10-10 13:19:54 +0200

    🚃

--- packages/components/calendar/src/calendar.stories.ts
+++ packages/components/calendar/src/calendar.stories.ts
@@ -1,16 +1,17 @@
 import { type Meta, type StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import '../register.js';
 import { type Calendar } from './calendar.js';
 
-type Props = Pick<Calendar, 'locale'>;
+type Props = Pick<Calendar, 'locale' | 'month'>;
 type Story = StoryObj<Props>;
 
 export default {
   title: 'Calendar/Calendar',
   tags: ['draft'],
-  render: () => {
-    return html`<sl-calendar></sl-calendar>`;
+  render: ({ locale, month }) => {
+    return html`<sl-calendar locale=${ifDefined(locale)} month=${ifDefined(month?.toISOString())}></sl-calendar>`;
   }
 } satisfies Meta<Props>;
 
--- packages/components/calendar/src/calendar.ts
+++ packages/components/calendar/src/calendar.ts
@@ -2,9 +2,12 @@ import { faChevronLeft } from '@fortawesome/pro-regular-svg-icons';
 import { localized, msg } from '@lit/localize';
 import { type ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
 import { Button } from '@sl-design-system/button';
+import { FormatDate } from '@sl-design-system/format-date';
 import { Icon } from '@sl-design-system/icon';
 import { LocaleMixin } from '@sl-design-system/shared';
+import { dateConverter } from '@sl-design-system/shared/converters.js';
 import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
+import { property } from 'lit/decorators.js';
 import styles from './calendar.scss.js';
 import { MonthView } from './month-view.js';
 
@@ -15,6 +18,7 @@ export class Calendar extends LocaleMixin(ScopedElementsMixin(LitElement)) {
   static get scopedElements(): ScopedElementsMap {
     return {
       'sl-button': Button,
+      'sl-format-date': FormatDate,
       'sl-icon': Icon,
       'sl-month-view': MonthView
     };
@@ -23,10 +27,15 @@ export class Calendar extends LocaleMixin(ScopedElementsMixin(LitElement)) {
   /** @internal */
   static override styles: CSSResultGroup = styles;
 
+  /** The month that the calendar opens on. */
+  @property({ converter: dateConverter }) month = new Date();
+
   override render(): TemplateResult {
     return html`
       <div part="header">
-        <sl-button @click=${this.#onToggleMonthYear} part="current-month-year" fill="link">October 2024</sl-button>
+        <sl-button @click=${this.#onToggleMonthYear} part="current-month-year" fill="link">
+          <sl-format-date .date=${this.month} month="long" year="numeric"></sl-format-date>
+        </sl-button>
         <sl-button @click=${this.#onPrevious} aria-label=${msg('Previous month')} fill="ghost" variant="primary">
           <sl-icon name="far-chevron-left"></sl-icon>
         </sl-button>
@@ -34,16 +43,16 @@ export class Calendar extends LocaleMixin(ScopedElementsMixin(LitElement)) {
           <sl-icon name="chevron-right"></sl-icon>
         </sl-button>
       </div>
-      <sl-month-view .locale=${this.locale}></sl-month-view>
+      <sl-month-view .locale=${this.locale} .month=${this.month}></sl-month-view>
     `;
   }
 
   #onPrevious(): void {
-    console.log('Previous month');
+    this.month = new Date(this.month.getFullYear(), this.month.getMonth() - 1);
   }
 
   #onNext(): void {
-    console.log('Next month');
+    this.month = new Date(this.month.getFullYear(), this.month.getMonth() + 1);
   }
 
   #onToggleMonthYear(): void {

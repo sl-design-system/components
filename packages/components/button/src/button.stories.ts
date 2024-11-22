795fb530fca8e89b7e1bb01608317ded08ff748c
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-11-22 10:14:58 +0100

    🍧

--- packages/components/button/src/button.stories.ts
+++ packages/components/button/src/button.stories.ts
@@ -1,58 +1,62 @@
-import { faPinata, faUniversalAccess } from '@fortawesome/pro-regular-svg-icons';
-import '@sl-design-system/button-bar/register.js';
+import { faUniversalAccess } from '@fortawesome/pro-regular-svg-icons';
 import { Icon } from '@sl-design-system/icon';
 import '@sl-design-system/icon/register.js';
 import { type Meta, type StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import '../register.js';
-import { type Button, type ButtonFill, type ButtonSize, type ButtonVariant } from './button.js';
+import { type Button } from './button.js';
 
-interface Props extends Pick<Button, 'disabled' | 'fill' | 'size' | 'variant'> {
+interface Props extends Pick<Button, 'disabled' | 'fill' | 'shape' | 'size' | 'variant'> {
   icon: string;
   text: string;
 }
-
 type Story = StoryObj<Props>;
 
-const fills: ButtonFill[] = ['solid', 'outline', 'link', 'ghost'];
-const variants: ButtonVariant[] = ['primary', 'secondary', 'success', 'info', 'warning', 'danger'];
-const disabledStates = [false, true];
-const sizes: ButtonSize[] = ['sm', 'md', 'lg'];
-
-Icon.register(faPinata, faUniversalAccess);
+Icon.register(faUniversalAccess);
 
 export default {
   title: 'Actions/Button',
   tags: ['stable'],
   args: {
-    text: 'Button',
+    disabled: false,
     icon: 'none',
-    disabled: false
+    text: 'Button'
   },
   argTypes: {
-    size: {
+    fill: {
       control: 'inline-radio',
-      options: sizes
+      options: ['solid', 'outline', 'link', 'ghost']
     },
     icon: {
       control: 'inline-radio',
       options: ['start', 'end', 'none']
     },
-    fill: {
+    shape: {
       control: 'inline-radio',
-      options: fills
+      options: ['square', 'pill']
+    },
+    size: {
+      control: 'inline-radio',
+      options: ['sm', 'md', 'lg']
     },
     variant: {
       control: 'radio',
-      options: variants
+      options: ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'inverted']
     }
   },
-  render: ({ fill, size, text, variant, icon, disabled }) => {
+  render: ({ disabled, fill, icon, shape, size, text, variant }) => {
     const startIcon = icon === 'start' ? html`<sl-icon name="face-smile"></sl-icon>` : '';
     const endIcon = icon === 'end' ? html`<sl-icon name="face-smile"></sl-icon>` : '';
 
     return html`
-      <sl-button .fill=${fill} .size=${size} .variant=${variant} ?disabled=${disabled}>
+      <sl-button
+        ?disabled=${disabled}
+        fill=${ifDefined(fill)}
+        shape=${ifDefined(shape)}
+        size=${ifDefined(size)}
+        variant=${ifDefined(variant)}
+      >
         ${startIcon}${text}${endIcon}
       </sl-button>
     `;
@@ -61,55 +65,15 @@ export default {
 
 export const Basic: Story = {};
 
-export const Sizes: Story = {
-  argTypes: {
-    size: {
-      table: {
-        disable: true
-      }
-    }
-  },
-  render: ({ fill, variant }) => html`
-    <style>
-      .grid {
-        display: inline-grid;
-        gap: 1rem;
-        grid-template-columns: repeat(4, max-content);
-        justify-items: start;
-      }
-    </style>
-    <div class="grid">
-      ${sizes.map(
-        size => html`
-          <sl-button .fill=${fill} .size=${size} .variant=${variant}><sl-icon name="face-smile"></sl-icon></sl-button>
-          <sl-button .fill=${fill} .size=${size} .variant=${variant}
-            ><sl-icon name="face-smile"></sl-icon> Icon ${size}</sl-button
-          >
-          <sl-button .fill=${fill} .size=${size} .variant=${variant}
-            >Icon ${size}<sl-icon name="face-smile"></sl-icon
-          ></sl-button>
-          <sl-button .fill=${fill} .size=${size} .variant=${variant}>${size}</sl-button>
-        `
-      )}
-    </div>
-  `
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  }
 };
 
-export const AlignmentIssues: Story = {
-  argTypes: {
-    size: {
-      table: {
-        disable: true
-      }
-    }
-  },
-  render: ({ fill, variant }) => {
-    return html`
-      <sl-button .fill=${fill} size="md" .variant=${variant}><sl-icon name="far-pinata"></sl-icon></sl-button>
-      <sl-button .fill=${fill} size="md" .variant=${variant}><sl-icon name="far-pinata"></sl-icon></sl-button><br />
-      <span>Some random text</span>
-      <sl-button .fill=${fill} size="md" .variant=${variant}><sl-icon name="far-pinata"></sl-icon></sl-button>
-    `;
+export const Pill: Story = {
+  args: {
+    shape: 'pill'
   }
 };
 
@@ -373,125 +337,6 @@ export const All: Story = {
 
         <div class="inverted-background"></div>
       </section>
-    `;
-  }
-};
-
-export const All2: Story = {
-  render: () => {
-    return html`
-      <style>
-        sl-button-bar {
-          margin-block-end: 8px;
-        }
-        table {
-          border-collapse: collapse;
-        }
-
-        th {
-          text-transform: capitalize;
-        }
-        th,
-        td {
-          padding: 4px 8px;
-        }
-        thead td {
-          text-align: center;
-        }
-
-        tbody td:nth-of-type(6n) {
-          border-right: 2px solid #dedede;
-          padding-right: 24px;
-        }
-        tbody td:nth-of-type(6n + 1):not(:first-of-type) {
-          padding-left: 24px;
-        }
-        tbody td:last-of-type {
-          border: none;
-        }
-      </style>
-      <h2>Sizes:</h2>
-      ${sizes.map(
-        size => html`
-          <sl-button-bar>
-            <sl-button fill="solid" .size=${size} variant="primary"><sl-icon name="face-smile"></sl-icon></sl-button>
-            <sl-button fill="solid" .size=${size} variant="primary"
-              ><sl-icon name="face-smile"></sl-icon> Icon ${size}</sl-button
-            >
-            <sl-button fill="solid" .size=${size} variant="primary"
-              >Icon ${size}<sl-icon name="face-smile"></sl-icon
-            ></sl-button>
-            <sl-button fill="solid" .size=${size} variant="primary">${size}</sl-button>
-          </sl-button-bar>
-        `
-      )}
-      <h2>Variants:</h2>
-      <table>
-        <thead>
-          <tr>
-            <td></td>
-            ${fills.map(fill => html`<th colspan="6">${fill}</th>`)}
-          </tr>
-          <tr>
-            <td></td>
-            ${fills.map(() =>
-              disabledStates.map(
-                disabledState => html`
-                  <td colspan="3" class=${disabledState ? 'sb-disabled' : ''}>
-                    ${disabledState ? 'Disabled' : 'Enabled'}
-                  </td>
-                `
-              )
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          ${variants.map(
-            variant => html`
-              <tr>
-                <th>${variant}</th>
-                ${fills.map(fill =>
-                  disabledStates.map(
-                    disabledState =>
-                      html` <td class=${disabledState ? 'sb-disabled' : ''}>
-                          <sl-button
-                            .fill=${fill}
-                            size="md"
-                            ?disabled=${disabledState}
-                            .variant=${variant}
-                            data-mock-state
-                            >Button
-                          </sl-button>
-                        </td>
-                        <td class=${disabledState ? 'sb-disabled' : ''}>
-                          <sl-button
-                            .fill=${fill}
-                            size="md"
-                            ?disabled=${disabledState}
-                            .variant=${variant}
-                            data-mock-state
-                          >
-                            <sl-icon name="face-smile"></sl-icon> Button
-                          </sl-button>
-                        </td>
-                        <td class=${disabledState ? 'sb-disabled' : ''}>
-                          <sl-button
-                            .fill=${fill}
-                            size="md"
-                            ?disabled=${disabledState}
-                            .variant=${variant}
-                            data-mock-state
-                          >
-                            <sl-icon name="face-smile"></sl-icon>
-                          </sl-button>
-                        </td>`
-                  )
-                )}
-              </tr>
-            `
-          )}
-        </tbody>
-      </table>
     `;
   }
 };

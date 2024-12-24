fca3a5c4bf0583ef6abebf8200820f26c010abb8
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2024-12-24 12:03:06 +0100

    ✈️

--- packages/components/drawer/src/drawer.stories.ts
+++ packages/components/drawer/src/drawer.stories.ts
@@ -1,80 +1,64 @@
 import '@sl-design-system/button/register.js';
-import { type StoryObj } from '@storybook/web-components';
-import { html } from 'lit';
+import { type Meta, type StoryObj } from '@storybook/web-components';
+import { html, nothing } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import '../register.js';
 import { type Drawer } from './drawer.js';
+
+type Props = Pick<Drawer, 'attachment'> & { styles?: string };
+type Story = StoryObj<Props>;
 
 export default {
   title: 'Overlay/Drawer',
   tags: ['draft'],
-  args: {
-    attachment: 'right',
-    buttonSize: 'sm'
-  },
   argTypes: {
     attachment: {
-      control: 'radio',
+      control: 'inline-radio',
       options: ['right', 'left', 'top', 'bottom']
     },
-    buttonSize: {
-      control: 'radio',
-      options: ['sm', 'md', 'lg']
+    styles: {
+      table: { disable: true }
     }
   },
-  parameters: {
-    // Disables Chromatic's snapshotting on a story level
-    chromatic: { disableSnapshot: true }
+  render: ({ attachment, styles }) => {
+    const onClick = (event: Event & { target: HTMLElement }) => {
+      (event.target.nextElementSibling as Drawer).toggle();
+    };
+
+    return html`
+      ${styles
+        ? html`
+            <style>
+              ${styles}
+            </style>
+          `
+        : nothing}
+      <sl-button @click=${onClick}>Show Drawer</sl-button>
+      <sl-drawer attachment=${ifDefined(attachment)}>
+        <h2 slot="title">In this side panel you can find a lot of info</h2>
+        <p>
+          Apple pie chocolate jelly-o carrot cake gummi bears halvah cake cheesecake. Sesame snaps macaroon shortbread
+          cheesecake muffin soufflé. Powder croissant sugar plum candy canes cupcake chupa chups cake marzipan.
+          Chocolate bar pie jujubes chocolate powder jelly. Marshmallow biscuit bear claw cookie topping. Tart sugar
+          plum toffee gingerbread macaroon danish brownie. Candy canes dragée sesame snaps lollipop ice cream.
+        </p>
+      </sl-drawer>
+    `;
+  }
+} satisfies Meta<Props>;
+
+export const Basic: Story = {
+  args: {
+    styles: `
+      sl-drawer::part(content) {
+        @media (width > 600px) {
+          inline-size: 300px;
+        }
+      }
+    `
   }
 };
 
-const onClick = (event: Event & { target: HTMLElement }): void => {
-  (event.target.nextElementSibling as Drawer).showModal();
-};
+export const Mobile: Story = {};
 
-export const API: StoryObj = {
-  render: ({ attachment, buttonSize }) => html`
-    <sl-button @click=${onClick}>Show Drawer</sl-button>
-    <sl-drawer .attachment=${attachment} .closeButtonSize=${buttonSize}>
-      <h2 slot="title">In this sidepanel you can find a lot of info</h2>
-      <p>
-        Apple pie chocolate jelly-o carrot cake gummi bears halvah cake cheesecake. Sesame snaps macaroon shortbread
-        cheesecake muffin soufflé. Powder croissant sugar plum candy canes cupcake chupa chups cake marzipan. Chocolate
-        bar pie jujubes chocolate powder jelly. Marshmallow biscuit bear claw cookie topping. Tart sugar plum toffee
-        gingerbread macaroon danish brownie. Candy canes dragée sesame snaps lollipop ice cream.
-      </p>
-    </sl-drawer>
-  `
-};
-
-export const DisableClose: StoryObj = {
-  render: () => html`
-    <sl-button @click=${onClick}>Show Drawer</sl-button>
-    <sl-drawer disable-close>
-      <span slot="title" id="title">Drawer title</span>
-      <p>
-        Jelly beans macaroon bonbon chocolate cake jelly beans chocolate lollipop cake. Wafer bonbon powder toffee pie.
-        Shortbread sweet dessert tiramisu danish jelly-o wafer. Brownie lemon drops cake lollipop tart candy cookie
-        gummies chocolate. Cupcake gummies sesame snaps topping gummi bears. Croissant danish marshmallow macaroon
-        fruitcake.
-      </p>
-    </sl-drawer>
-  `
-};
-
-export const CompleteHeader: StoryObj = {
-  render: ({ attachment, buttonSize }) => html`
-    <sl-button @click=${onClick}>Show Drawer</sl-button>
-    <sl-drawer .attachment=${attachment} .closeButtonSize=${buttonSize}>
-      <h1 slot="title">Test title</h1>
-      <sl-button slot="actions">download</sl-button>
-      <p>
-        Macaroon caramels tootsie roll cookie liquorice cake gingerbread cookie. Toffee fruitcake macaroon cheesecake
-        muffin gingerbread apple pie. Donut powder lollipop macaroon jelly-o. Powder powder tiramisu brownie jelly
-        macaroon jelly ice cream. Cake macaroon pudding cookie cookie powder macaroon. Sesame snaps cheesecake jujubes
-        tootsie roll macaroon oat cake jujubes cotton candy. Chocolate chocolate cake tart fruitcake sugar plum. Lemon
-        drops dessert pastry jujubes bonbon fruitcake muffin. Candy canes wafer brownie chocolate cake macaroon
-        cheesecake.
-      </p>
-    </sl-drawer>
-  `
-};
+export const Tablet: Story = {};
--- packages/components/drawer/src/drawer.ts
+++ packages/components/drawer/src/drawer.ts
@@ -1,6 +1,8 @@
+import { localized, msg } from '@lit/localize';
 import { type ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
-import { Button, type ButtonSize } from '@sl-design-system/button';
+import { Button } from '@sl-design-system/button';
 import { ButtonBar } from '@sl-design-system/button-bar';
+import { Icon } from '@sl-design-system/icon';
 import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
 import { property, query } from 'lit/decorators.js';
 import styles from './drawer.scss.js';
@@ -21,28 +23,25 @@ export type DrawerAttachment = 'right' | 'left' | 'top' | 'bottom';
  * @slot header - Header content for the drawer
  * @slot title - The title of the drawer
  */
+@localized()
 export class Drawer extends ScopedElementsMixin(LitElement) {
   /** @internal */
   static get scopedElements(): ScopedElementsMap {
     return {
       'sl-button': Button,
-      'sl-button-bar': ButtonBar
+      'sl-button-bar': ButtonBar,
+      'sl-icon': Icon
     };
   }
 
   /** @internal */
   static override styles: CSSResultGroup = styles;
 
-  @query('dialog') dialog?: HTMLDialogElement;
-
-  /** Disables the ability to close the dialog using the Escape key. */
-  @property({ type: Boolean, attribute: 'disable-close' }) disableClose = false;
+  /** @internal The popover element */
+  @query('[popover]') popoverElement!: HTMLElement;
 
   /** The side of the screen where the drawer is attached */
-  @property({ reflect: true }) attachment: DrawerAttachment = 'right';
-
-  /** The size of the button */
-  @property({ attribute: 'close-button-size' }) closeButtonSize: ButtonSize = 'sm';
+  @property({ reflect: true }) attachment?: DrawerAttachment;
 
   override connectedCallback(): void {
     super.connectedCallback();
@@ -52,75 +51,56 @@ export class Drawer extends ScopedElementsMixin(LitElement) {
 
   override render(): TemplateResult {
     return html`
-      <dialog
-        @cancel=${this.#onCancel}
-        @click=${this.#onClick}
-        @close=${this.#onClose}
+      <div
+        @beforetoggle=${this.#onBeforeToggle}
+        @toggle=${this.#onToggle}
         aria-labelledby="title"
-        part="dialog"
+        part="popover"
+        popover
       >
-        <div>
-          <sl-button-bar>
-            <sl-button
-              sl-dialog-close
-              .size=${this.closeButtonSize}
-              tab-index="0"
-              aria-label="back to page"
-              title="close"
-              >x</sl-button
-            >
-            <slot name="actions"></slot>
-          </sl-button-bar>
-          <slot name="title" id="title"></slot>
+        <div part="content">
+          <div part="header">
+            <sl-button-bar reverse>
+              <sl-button @click=${this.#onClick} aria-label=${msg('Close the drawer')} fill="ghost">
+                <sl-icon name="xmark"></sl-icon>
+              </sl-button>
+              <slot name="actions"></slot>
+            </sl-button-bar>
+            <slot name="title" id="title"></slot>
+          </div>
+          <slot></slot>
         </div>
-        <slot></slot>
-      </dialog>
+      </div>
     `;
   }
 
-  showModal(): void {
-    this.inert = false;
-    this.dialog?.showModal();
-
-    // Disable scrolling while the dialog is open
-    document.documentElement.style.overflow = 'hidden';
+  hide(): void {
+    this.popoverElement?.hidePopover();
   }
 
-  close(): void {
-    if (this.dialog?.open) {
-      this.dialog?.close();
+  show(): void {
+    this.popoverElement?.showPopover();
+  }
+
+  toggle(): void {
+    this.popoverElement?.togglePopover();
+  }
+
+  #onBeforeToggle(event: ToggleEvent): void {
+    if (event.newState === 'closed') {
+      document.documentElement.style.overflow = '';
+      this.inert = true;
+    } else {
+      document.documentElement.style.overflow = 'hidden';
+      this.inert = false;
     }
   }
 
-  #onCancel(event: Event): void {
-    if (this.disableClose) {
-      event.preventDefault();
-    }
+  #onClick(): void {
+    this.hide();
   }
 
-  #onClick(event: PointerEvent & { target: HTMLElement }): void {
-    if (event.target.matches('sl-button[sl-dialog-close]')) {
-      this.dialog?.close(event.target.getAttribute('sl-dialog-close') || '');
-    } else if (!this.disableClose && this.dialog) {
-      const rect = this.dialog.getBoundingClientRect();
-
-      // Check if the user clicked on the backdrop
-      if (
-        event.clientY < rect.top ||
-        event.clientY > rect.bottom ||
-        event.clientX < rect.left ||
-        event.clientX > rect.right
-      ) {
-        // If so, close the dialog
-        this.dialog.close();
-      }
-    }
-  }
-
-  #onClose(): void {
-    // Reenable scrolling after the dialog has closed
-    document.documentElement.style.overflow = '';
-
-    this.inert = true;
+  #onToggle(event: ToggleEvent): void {
+    console.log('toggle', event);
   }
 }

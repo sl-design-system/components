44b12bf83808f5e23f24d3402975932427b63a29
Diana Broeders <diana.broeders@sanoma.com>
2025-01-20 16:51:30 +0100

    new export script + name spacing

--- scripts/output-pages-to-folders-svg.js
+++ scripts/output-pages-to-folders-svg.js
@@ -1,3 +1,5 @@
+import { kebabCase } from 'change-case';
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,13 +11,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import fs from 'fs';
 import path from 'path';
-export default ({ output, getDirname = (options) => `${options.pageName}${path.sep}${options.dirname}`, getBasename = (options) => `${options.basename}.svg`, }) => {
+export default ({ 
+  output, 
+  getDirname = (options) => `${options.pageName}${path.sep}${options.dirname}`, 
+  getBasename = (options) => `${options.pathToComponent.length?`${kebabCase(options.pathToComponent[0].name)}-`:''}${options.basename}.svg`, 
+}) => {
   return (pages) => __awaiter(void 0, void 0, void 0, function* () {
         pages.forEach(({ name: pageName, components }) => {
           const outputFolder = `${output}/${pageName}/icons`;
-            components.forEach(({ name: componentName, svg, figmaExport }) => {
+          // console.log(components);
+          components.forEach(({ name: componentName, svg, figmaExport }) => {
                 const options = Object.assign({ pageName,
                     componentName }, figmaExport);
+                console.log(options, figmaExport.pathToComponent);
                 const filePath = path.resolve(outputFolder, getDirname(options));
                 fs.mkdirSync(filePath, { recursive: true });
                 fs.writeFileSync(path.resolve(filePath, getBasename(options)), svg);

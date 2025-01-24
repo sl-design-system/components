a49bf5ff65d127a2ffe69548da4c88c7c360d646
Diana Broeders <diana.broeders@sanoma.com>
2025-01-24 15:08:23 +0100

    fixed inclusion of custom icons

--- scripts/import-icons.js
+++ scripts/import-icons.js
@@ -103,21 +103,6 @@ const buildIcons = async theme => {
     await fs.mkdir(iconsFolderPath);
   }
 
-  for (const file of await fs.readdir(iconsFolderPath)) {
-    await fs.unlink(join(iconsFolderPath, file));
-  }
-
-  // // load all custom icons from figma and store svgs
-  // await new Promise((resolve, reject) => {
-  //   console.log(`Extracting icons from Figma for ${theme}...`);
-  //   exec(`yarn run figma-export use-config .figmaexportrc.js ${figmaIconPages[theme]} ${theme}`, { cwd }, error => {
-  //     if (error) {
-  //       reject(error);
-  //     }
-
-  //     resolve();
-  //   });
-  // });
 
   // 3. Convert downloaded icons to appropriate format?
   // We only need the `<path>` data for `<sl-icon>`
@@ -140,7 +125,7 @@ const buildIcons = async theme => {
   // 4. Write the output to `icons.json`???? Or just `icons.ts` which exports
   console.log(`Writing icons to ${theme}...`);
   const filePath = join(cwd, `../packages/themes/${theme}/icons.ts`),
-    sortedIcons = Object.fromEntries(Object.entries({ ...icons, ...coreCustomIcons }).sort()),
+    sortedIcons = Object.fromEntries(Object.entries({ ...icons, ...coreCustomIcons, ...iconsCustom }).sort()),
     source = `export const icons = ${JSON.stringify(sortedIcons)};`,
     results = await eslint.lintText(source, { filePath });
   
@@ -199,4 +184,3 @@ const exportCoreIcons = async () => {
 
 const coreCustomIcons  = await exportCoreIcons();
 buildAllIcons();
-// buildIcons('clickedu');

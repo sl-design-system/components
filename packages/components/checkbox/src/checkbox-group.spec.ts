84f10b8b056443dd770856d46784b7b42fa14fc2
anna-lach <a.lach@nowaera.pl>
2025-03-24 11:05:48 +0100

    tests

--- packages/components/checkbox/src/checkbox-group.spec.ts
+++ packages/components/checkbox/src/checkbox-group.spec.ts
@@ -320,35 +320,25 @@ describe('sl-checkbox-group', () => {
       `);
     });
 
+    it('should have proper values', () => {
+      expect(el.querySelectorAll('sl-checkbox')[0].value).to.equal(null);
+      expect(el.querySelectorAll('sl-checkbox')[1].value).to.equal(undefined);
+    });
+
     it('should have a value of true when the checkbox with null value is checked', async () => {
       el.querySelector('sl-checkbox')?.click();
-      await new Promise(resolve => setTimeout(resolve, 100));
+      await new Promise(resolve => setTimeout(resolve));
 
       expect(el.value).to.deep.equal([true, null]);
     });
 
     it('should have a value of true when the checkbox with undefined value is checked', async () => {
       el.querySelectorAll('sl-checkbox')[1]?.click();
-      await new Promise(resolve => setTimeout(resolve, 100));
+      await new Promise(resolve => setTimeout(resolve));
 
       expect(el.value).to.deep.equal([null, true]);
     });
-
-    it('should update checkbox value when necessary', () => {
-      el.querySelectorAll('sl-checkbox')[1].formValue = 'oldValue';
-      el.value = ['newValue', 'value2'];
-      console.log('1: el.value', el.value);
-      el.value = ['value', true];
-      // el.willUpdate(new Map([['value', true]]));
-
-      console.log('2: el.value', el.value);
-
-      // TODO: check update event maybe?
-
-      expect(el.querySelectorAll('sl-checkbox')[0].formValue).to.equal('newValue');
-      expect(el.querySelectorAll('sl-checkbox')[1].formValue).to.equal('value2');
-    });
-  }); // TODO: make test with .value="null" only
+  });
 
   describe('form integration', () => {
     let el: FormIntegrationTestComponent;

0964baccc5cb4e82340a2ca73c1720bbf0c85208
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2025-04-11 13:52:49 +0200

    🐣

--- packages/components/grid/src/stories/scrolling.stories.ts
+++ packages/components/grid/src/stories/scrolling.stories.ts
@@ -48,13 +48,36 @@ export const Horizontal: Story = {
       return html`${firstName} ${lastName}`;
     };
 
+    const columns = [
+      'email',
+      'profession',
+      'address.phone',
+      'address.street',
+      'address.city',
+      'address.zip',
+      'address.state'
+    ];
+
+    const onAdd = () => {
+      const count = document.querySelectorAll('sl-grid-column').length,
+        column = document.createElement('sl-grid-column');
+
+      column.path = columns[count];
+      document.querySelector('sl-grid')?.appendChild(column);
+    };
+
+    const onRemove = () => document.querySelector('sl-grid-column:last-of-type')?.remove();
+
     return html`
       <style>
         body {
           padding-block-start: 0 !important;
         }
+        sl-button-bar {
+          padding-block-start: 1rem;
+        }
         .cover {
-          background: var(--sl-elevation-surface-base-default-idle);
+          background: var(--sl-elevation-surface-base-default);
           block-size: 1rem;
           position: sticky;
           inset-block-start: 0;
@@ -64,16 +87,14 @@ export const Horizontal: Story = {
           inset-block-start: 1rem;
         }
       </style>
+      <sl-button-bar>
+        <sl-button @click=${onAdd}>Add column</sl-button>
+        <sl-button @click=${onRemove}>Remove column</sl-button>
+      </sl-button-bar>
       <div class="cover"></div>
       <sl-grid .items=${people}>
         <sl-grid-column header="Name" .renderer=${nameRenderer}></sl-grid-column>
-        <sl-grid-column path="email"></sl-grid-column>
-        <sl-grid-column path="profession"></sl-grid-column>
-        <sl-grid-column path="address.phone"></sl-grid-column>
-        <sl-grid-column path="address.street"></sl-grid-column>
-        <sl-grid-column path="address.city"></sl-grid-column>
-        <sl-grid-column path="address.zip"></sl-grid-column>
-        <sl-grid-column path="address.state"></sl-grid-column>
+        ${columns.map(c => html`<sl-grid-column path=${c}></sl-grid-column>`)}
       </sl-grid>
     `;
   }
@@ -97,7 +118,7 @@ export const HorizontalSticky: Story = {
           padding-block-start: 0 !important;
         }
         .cover {
-          background: var(--sl-elevation-surface-base-default-idle);
+          background: var(--sl-elevation-surface-base-default);
           block-size: 1rem;
           position: sticky;
           inset-block-start: 0;

1189ca11f354ccea57af50b1b487c693450b739c
Diana Broeders <diana.broeders@sanoma.com>
2025-04-14 12:04:46 +0200

    get modes

---
+++ scripts/figma-plugins/theme-selector/code.d.ts
@@ -0,0 +1,1 @@
+declare const localCollections: any;
--- scripts/figma-plugins/theme-selector/code.ts
+++ scripts/figma-plugins/theme-selector/code.ts
@@ -8,6 +8,8 @@
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+const localCollections = await figma.variables.getLocalVariableCollectionsAsync();
+console.log('localCollections', localCollections);
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the

e8c68897cd1a1072972fc91bc7e656a64e545a2e
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2025-05-29 12:56:44 +0200

    🌙

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,13 +8,18 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   test: {
+    onConsoleLog(log: string, type: 'stdout' | 'stderr'): boolean | void {
+      return !(log.startsWith('Lit is in dev mode') || log === 'null');
+    },
     workspace: [
       {
         extends: true,
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') })
+          storybookTest({
+            configDir: path.join(dirname, '.storybook')
+          })
         ],
         test: {
           name: 'storybook',

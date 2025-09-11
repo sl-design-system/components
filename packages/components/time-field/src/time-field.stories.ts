4452465ea29f2e1df2080036520b444290e934f4
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2025-09-11 14:24:59 +0200

    🏄

--- packages/components/time-field/src/time-field.stories.ts
+++ packages/components/time-field/src/time-field.stories.ts
@@ -1,14 +1,26 @@
 import { type Meta, type StoryObj } from '@storybook/web-components-vite';
+import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import '../register.js';
 import { type TimeField } from './time-field.js';
 
-type Props = Pick<TimeField, 'value'>;
+type Props = Pick<TimeField, 'disabled' | 'placeholder' | 'value'>;
 type Story = StoryObj<Props>;
 
 export default {
-  title: 'Form/Time Field',
+  title: 'Form/Time field',
   tags: ['draft'],
-  render: () => '<sl-time-field></sl-time-field>'
+  args: {
+    disabled: false,
+    placeholder: 'Select time'
+  },
+  render: ({ disabled, placeholder, value }) => html`
+    <sl-time-field
+      ?disabled=${disabled}
+      placeholder=${ifDefined(placeholder)}
+      value=${ifDefined(value)}
+    ></sl-time-field>
+  `
 } satisfies Meta<Props>;
 
 export const Basic: Story = {};
--- packages/components/time-field/src/time-field.ts
+++ packages/components/time-field/src/time-field.ts
@@ -1,8 +1,14 @@
+import { faClock } from '@fortawesome/pro-regular-svg-icons';
+import { localized, msg } from '@lit/localize';
 import { type ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
-import { FormControlMixin } from '@sl-design-system/form';
+import { FormControlMixin, type SlFormControlEvent, type SlUpdateStateEvent } from '@sl-design-system/form';
 import { Icon } from '@sl-design-system/icon';
+import { type EventEmitter, anchor, event } from '@sl-design-system/shared';
+import { type SlBlurEvent, type SlChangeEvent, type SlFocusEvent } from '@sl-design-system/shared/events.js';
 import { FieldButton, TextField } from '@sl-design-system/text-field';
-import { type CSSResultGroup, LitElement, type TemplateResult, html } from 'lit';
+import { type CSSResultGroup, LitElement, type PropertyValues, type TemplateResult, html, nothing } from 'lit';
+import { property, query } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import styles from './time-field.scss.js';
 
 declare global {
@@ -11,7 +17,13 @@ declare global {
   }
 }
 
+Icon.register(faClock);
+
+@localized()
 export class TimeField extends FormControlMixin(ScopedElementsMixin(LitElement)) {
+  /** @internal The default offset of the popover to the text-field. */
+  static offset = 6;
+
   /** @internal */
   static get scopedElements(): ScopedElementsMap {
     return {
@@ -24,7 +36,206 @@ export class TimeField extends FormControlMixin(ScopedElementsMixin(LitElement))
   /** @internal */
   static override styles: CSSResultGroup = styles;
 
+  /** @internal The default margin between the popover and the viewport. */
+  static viewportMargin = 8;
+
+  /**
+   * Flag indicating whether the popover was just closed. We need to know this so we can
+   * properly handle button clicks that close the popover. If the popover was just closed,
+   * we don't want to show it again when the button click event fires.
+   */
+  #popoverJustClosed = false;
+
+  /** @internal Emits when the focus leaves the component. */
+  @event({ name: 'sl-blur' }) blurEvent!: EventEmitter<SlBlurEvent>;
+
+  /** @internal Emits when the value changes. */
+  @event({ name: 'sl-change' }) changeEvent!: EventEmitter<SlChangeEvent<Date>>;
+
+  /** Whether the time field is disabled; when set no interaction is possible. */
+  @property({ type: Boolean }) override disabled?: boolean;
+
+  /** @internal Emits when the component gains focus. */
+  @event({ name: 'sl-focus' }) focusEvent!: EventEmitter<SlFocusEvent>;
+
+  /** @internal The input element in the light DOM. */
+  input!: HTMLInputElement;
+
+  /**
+   * The placeholder for the time field.
+   * @default undefined
+   */
+  @property() placeholder?: string;
+
+  /**
+   * Whether the time field is readonly.
+   * @default false
+   */
+  @property({ type: Boolean }) readonly?: boolean;
+
+  /**
+   * Whether the time field is a required field.
+   * @default false
+   */
+  @property({ type: Boolean, reflect: true }) override required?: boolean;
+
+  /** @internal The text field. */
+  @query('sl-text-field') textField!: TextField;
+
+  /** @internal The wrapper element that is also the popover. */
+  @query('[part="wrapper"]') wrapper?: HTMLSlotElement;
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    if (!this.input) {
+      this.input = this.querySelector<HTMLInputElement>('input[slot="input"]') || document.createElement('input');
+      this.input.autocomplete = 'off';
+      this.input.slot = 'input';
+
+      if (!this.input.parentElement) {
+        this.append(this.input);
+      }
+    }
+
+    this.setFormControlElement(this.input);
+
+    // This is a workaround, because :has is not working in Safari and Firefox with :host element as it works in Chrome
+    const style = document.createElement('style');
+    style.innerHTML = `
+      sl-time-field:has(input:hover):not(:focus-within)::part(text-field) {
+        --_bg-opacity: var(--sl-opacity-interactive-plain-hover);
+      }
+    `;
+    this.prepend(style);
+  }
+
+  override updated(changes: PropertyValues<this>): void {
+    super.updated(changes);
+
+    if (changes.has('required')) {
+      if (this.textField) {
+        this.textField.required = !!this.required;
+      }
+    }
+  }
+
   override render(): TemplateResult {
-    return html`time-field`;
+    return html`
+      <sl-text-field
+        @sl-blur=${this.#onTextFieldBlur}
+        @sl-change=${this.#onTextFieldChange}
+        @sl-focus=${this.#onTextFieldFocus}
+        @sl-form-control=${this.#onTextFieldFormControl}
+        @sl-update-state=${this.#onTextFieldUpdateState}
+        ?disabled=${this.disabled}
+        ?readonly=${this.readonly}
+        ?required=${this.required}
+        .showValidity=${this.showValidity}
+        part="text-field"
+        placeholder=${ifDefined(this.placeholder)}
+      >
+        <slot name="input" slot="input"></slot>
+        <sl-field-button
+          @click=${this.#onButtonClick}
+          ?disabled=${this.disabled || this.readonly}
+          aria-label=${msg('Toggle calendar', { id: 'sl.dateField.toggleCalendar' })}
+          slot="suffix"
+          tabindex=${this.disabled ? '-1' : '0'}
+        >
+          <sl-icon name="far-clock"></sl-icon>
+        </sl-field-button>
+      </sl-text-field>
+
+      <slot
+        ${anchor({
+          element: this,
+          offset: TimeField.offset,
+          position: 'bottom-start',
+          viewportMargin: TimeField.viewportMargin
+        })}
+        @beforetoggle=${this.#onBeforeToggle}
+        @toggle=${this.#onToggle}
+        @keydown=${this.#onKeydown}
+        name="calendar"
+        part="wrapper"
+        popover
+        tabindex="-1"
+      >
+        ${this.wrapper?.matches(':popover-open') ? html`<div>Time dropdown</div>` : nothing}
+      </slot>
+    `;
+  }
+
+  #onBeforeToggle(event: ToggleEvent): void {
+    if (event.newState === 'open') {
+      this.input.setAttribute('aria-expanded', 'true');
+    } else {
+      this.input.setAttribute('aria-expanded', 'false');
+      this.#popoverJustClosed = true;
+    }
+  }
+
+  #onButtonClick(): void {
+    // Prevents the popover from reopening immediately after it was just closed
+    if (!this.#popoverJustClosed) {
+      this.wrapper?.togglePopover();
+    }
+  }
+
+  #onTextFieldBlur(event: SlBlurEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.blurEvent.emit();
+    this.updateState({ touched: true });
+  }
+
+  #onTextFieldChange(event: SlChangeEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.updateValidity();
+  }
+
+  #onTextFieldFocus(event: SlFocusEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.focusEvent.emit();
+  }
+
+  #onTextFieldFormControl(event: SlFormControlEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  #onTextFieldUpdateState(event: SlUpdateStateEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.updateValidity();
+  }
+
+  #onToggle(event: ToggleEvent): void {
+    if (event.newState === 'closed') {
+      this.#popoverJustClosed = false;
+    } else {
+      // Wait for the calendar to render in the popover
+      requestAnimationFrame(() => {
+        // this.renderRoot.querySelector('sl-calendar')?.focus();
+      });
+    }
+
+    // Trigger a rerender so the calendar will be rendered
+    this.requestUpdate();
+  }
+
+  #onKeydown(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      // Prevents the Escape key event from bubbling up, so that pressing 'Escape' inside the date field
+      // does not close parent containers (such as dialogs).
+      event.stopPropagation();
+    }
   }
 }

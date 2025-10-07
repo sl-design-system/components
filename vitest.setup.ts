22b4e2d7411bbc43237e78431c4617bea9cd7414
Diana Broeders <diana.broeders@sanoma.com>
2025-10-07 15:21:58 +0200

    fix font in tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -9,8 +9,13 @@ chai.use(sinonChai);
 
 // Set the body font
 let style = document.createElement('style');
-style.innerText = `body { font: var(--sl-text-body-md-normal); }`;
+style.innerText = `body { font: var(--sl-text-new-body-md); }`;
 document.head.appendChild(style);
+
+// Load the SL light theme
+// style = document.createElement('style');
+// style.innerText = await commands.readFile('./packages/themes/sanoma-learning/light-old.css');
+// document.head.appendChild(style);
 
 // Load the SL light theme
 style = document.createElement('style');

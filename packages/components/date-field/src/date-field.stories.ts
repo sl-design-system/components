68e8eabe8f3d4c2775cabd6888cc5e12a6eb4f53
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2026-03-04 11:53:54 +0100

    🚖

--- packages/components/date-field/src/date-field.stories.ts
+++ packages/components/date-field/src/date-field.stories.ts
@@ -164,7 +164,8 @@ export const MinMax: Story = {
     hint: 'This story has a minimum date of June 10, 2025 and a maximum date of June 20, 2025. The calendar will open to June 2025 by default.',
     month: new Date(2025, 0, 1),
     max: new Date(2025, 0, 20),
-    min: new Date(2025, 0, 10)
+    min: new Date(2025, 0, 10),
+    reportValidity: true
   }
 };
 

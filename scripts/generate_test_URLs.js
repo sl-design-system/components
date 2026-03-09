560ab2e8b755fa2b9691dccbafb1249e6b9a2c7e
a11ymiko <kacper.mikocki@sanoma.com>
2026-03-09 15:36:49 +0100

    a11y tests for website added

---
+++ scripts/generate_test_URLs.js
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+const files = process.env.CHANGED_MD.split(' ').filter(Boolean);
+
+const urls = files.map(file => file.replace(/^website\/src\//, '').replace(/\.md$/, '/'));
+
+fs.writeFileSync('changed-urls.json', JSON.stringify(urls, null, 2));
---
+++ tests/website/website_a11y.spec.ts
@@ -0,0 +1,42 @@
+import { test } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+import { createHtmlReport } from 'axe-html-reporter';
+import fs from 'fs';
+import urls from '../../changed-urls.json';
+
+urls.forEach(url => {
+  test(`Website accessibility test on ${url}`, async ({ page }) => {
+    const projectName = test.info().project.name;
+    const resultsAll = [];
+
+    await page.goto(url);
+
+    const safeName = url === '/' ? 'home' : url.replace(/\//g, '_').replace(/^\_/, '');
+
+    let accessibilityScanResults;
+    if (url === '/') {
+      accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    } else {
+      accessibilityScanResults = await new AxeBuilder({ page }).include('.ds-container').analyze();
+    }
+
+    const report = createHtmlReport({
+      results: {
+        violations: accessibilityScanResults.violations
+      },
+      options: {
+        projectKey: `${projectName}-${safeName}`
+      }
+    });
+
+    fs.mkdirSync(`reports/${projectName}`, { recursive: true });
+    fs.writeFileSync(`reports/${projectName}/${safeName}_a11y_report.html`, report);
+
+    const summary = {
+      url,
+      violations: accessibilityScanResults.violations.length
+    };
+
+    fs.appendFileSync('reports/summary.json', `${JSON.stringify(summary)} + \n`);
+  });
+});

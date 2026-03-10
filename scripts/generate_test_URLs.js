41a52d13f16d4bed0eae2b3f3a17e08c016b46df
a11ymiko <kacper.mikocki@sanoma.com>
2026-03-10 14:10:26 +0100

    changes to action workflow, fixed yarn.lock, deleted package-lock

--- scripts/generate_test_URLs.js
+++ scripts/generate_test_URLs.js
@@ -2,6 +2,10 @@ import fs from 'fs';
 
 const files = process.env.CHANGED_MD.split(' ').filter(Boolean);
 
+// If there are no changed files, we want to test the homepage
+if (files.length === 0) {
+  files = '/';
+}
 const urls = files.map(file => file.replace(/^website\/src\//, '').replace(/\.md$/, '/'));
 
 fs.writeFileSync('changed-urls.json', JSON.stringify(urls, null, 2));

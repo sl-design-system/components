0e2d8e51d31ecfcb76f1d4d2d24ec779c1dc74c6
a11ymiko <kacper.mikocki@sanoma.com>
2026-03-11 10:09:53 +0100

    change to array in generate_test_URLs

--- scripts/generate_test_URLs.js
+++ scripts/generate_test_URLs.js
@@ -1,11 +1,7 @@
 import fs from 'fs';
 
-let files = process.env.CHANGED_MD.split(' ').filter(Boolean);
+const files = process.env.CHANGED_MD.length !== 0 ? process.env.CHANGED_MD.split(' ').filter(Boolean) : ['/'];
 
-// If there are no changed files, we want to test the homepage
-if (files.length === 0) {
-  files = '/';
-}
 const urls = files.map(file => file.replace(/^website\/src\//, '').replace(/\.md$/, '/'));
 
 fs.writeFileSync('changed-urls.json', JSON.stringify(urls, null, 2));

78d1179a8b0fdbe75ed7ae8972b0d51010c34318
a11ymiko <kacper.mikocki@sanoma.com>
2026-03-27 14:42:42 +0100

    added support for manually passed URLs in tests. fixed reports generated for pages with no violations

--- website/tests/website_a11y.spec.ts
+++ website/tests/website_a11y.spec.ts
@@ -2,12 +2,26 @@ import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 import { AxeResults } from 'axe-core';
 import { createHtmlReport } from 'axe-html-reporter';
-import urls from '../../changed-urls.json';
+import { readFileSync } from 'node:fs';
 
 let axe: AxeBuilder;
 let results: AxeResults;
 const homePageUrl = '/';
 const domainName = 'http://localhost:8000/';
+
+function getArgumentValue(name: string): string | undefined {
+  const arg = process.argv.find(arg => arg.startsWith(`--${name}=`));
+  return arg ? arg.split('=')[1] : undefined;
+}
+
+const cliUrl = getArgumentValue('url');
+let urls: string[];
+
+if (cliUrl) {
+  urls = cliUrl.split(',');
+} else {
+  urls = JSON.parse(readFileSync(new URL('../../changed-urls.json', import.meta.url), 'utf-8')) as string[];
+}
 
 function createNumberedList<T>(items: T[]): string {
   return items.map((item, index) => `${index + 1}. ${item}`).join('\n');
@@ -30,17 +44,17 @@ test.afterEach(async ({ page }) => {
       })
       .join('\n\n');
     console.error(`Accessibility violations found for ${page.url()}:\n\n${violationDetails}`);
-  }
 
-  createHtmlReport({
-    results: {
-      violations: results.violations
-    },
-    options: {
-      outputDir: 'reports/website',
-      reportFileName: `${page.url().replace(domainName, '').replaceAll('/', '_')}a11y_report.html`
-    }
-  });
+    createHtmlReport({
+      results: {
+        violations: results.violations
+      },
+      options: {
+        outputDir: 'reports/website',
+        reportFileName: `${page.url().replace(domainName, '').replaceAll('/', '_')}a11y_report.html`
+      }
+    });
+  }
 });
 
 // Test only the homepage scanning the full page including <header> and <nav>

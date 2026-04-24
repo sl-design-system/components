10444e46bd150ce20cf3a7153cd1bd4b3469e4eb
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2026-04-24 11:50:08 +0200

    😋

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,12 +1,15 @@
 export default {
   'packages/components/**/*.scss': [
     'stylelint --quiet-deprecation-warnings --config packages/components/stylelint.config.mjs --fix',
-    'oxfmt'
+    'oxfmt --no-error-on-unmatched-pattern'
   ],
   'packages/components/**/*.ts': [
     'eslint --config packages/components/eslint.config.mjs --fix',
-    'oxfmt'
+    'oxfmt --no-error-on-unmatched-pattern'
   ],
-  '*.scss': ['stylelint --quiet-deprecation-warnings --fix', 'oxfmt'],
-  '!(*.scss)': 'oxfmt'
+  '*.scss': [
+    'stylelint --quiet-deprecation-warnings --fix',
+    'oxfmt --no-error-on-unmatched-pattern'
+  ],
+  '!(*.scss)': 'oxfmt --no-error-on-unmatched-pattern'
 };

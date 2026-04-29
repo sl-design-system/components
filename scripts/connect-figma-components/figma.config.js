3f49d2a0af76a379209fc0b534ed730c1b1b459d
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2026-04-29 12:55:25 +0200

    ⛰

--- scripts/connect-figma-components/figma.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@figma/code-connect').CodeConnectConfig} */
-module.exports = {
-  codeConnect: {
-    include: ['**/*.figma.ts'],
-    exclude: ['**/node_modules/**'],
-    parser: 'custom'
-  }
-};
---
+++ scripts/connect-figma-components/src/button.figma.ts
@@ -0,0 +1,64 @@
+// url=https://www.figma.com/design/CHpKrPIdXdbV2u7X8vizKI/Components-2.0?node-id=301-54273
+/// <reference types="@figma/code-connect/figma-types" />
+import figma from 'figma';
+
+const instance = figma.selectedInstance;
+
+function getExample() {
+  const buttonVariants = instance.findInstance('Button-Variants');
+  if (buttonVariants.type === 'ERROR') return null;
+
+  const disabled = buttonVariants.getString('State') === 'Disabled';
+
+  // The default fill is "solid".
+  const fill =
+    buttonVariants.getEnum('Type', {
+      Outline: 'outline',
+      Ghost: 'ghost',
+      Link: 'link'
+    }) || 'solid';
+
+  // The default variant is "secondary".
+  const variant =
+    buttonVariants.getEnum('Variant', {
+      Primary: 'primary',
+      Positive: 'success',
+      Info: 'info',
+      Caution: 'warning',
+      Negative: 'danger',
+      Inverted: 'inverted'
+    }) || 'secondary';
+
+  const buttonBase = buttonVariants.findInstance('Button-Base');
+  if (buttonBase.type === 'ERROR') return null;
+
+  const iconStart = buttonBase.getBoolean('Icon Start'),
+    iconEnd = buttonBase.getBoolean('Icon End'),
+    label = buttonBase.getString('𝐓 - Label');
+
+  // The default size is "md".
+  const size =
+    buttonBase.getEnum('↕️ - Size', {
+      Small: 'sm',
+      Large: 'lg'
+    }) || 'md';
+
+  let icon;
+  if (iconStart || iconEnd) {
+    icon = buttonBase.findInstance('Base/Icon', { traverseInstances: true });
+  }
+
+  return figma.code`
+    <sl-button${disabled ? ' disabled' : ''}${fill !== 'solid' ? ` fill="${fill}"` : ''}${size !== 'md' ? ` size="${size}"` : ''}${
+      variant !== 'secondary' ? ` variant="${variant}"` : ''
+    }>
+  ${iconStart ? icon?.executeTemplate().example : ''}${iconStart && icon ? '\n  ' : ''}${label}${iconEnd && icon ? '\n  ' : ''}${iconEnd ? icon?.executeTemplate().example : ''}
+</sl-button>
+  `;
+}
+
+export default {
+  example: getExample(),
+  id: 'button',
+  imports: []
+};

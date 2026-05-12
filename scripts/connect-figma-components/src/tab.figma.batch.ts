99df7798735be3f144b89526181742531a603653
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2026-05-12 15:22:45 +0200

    🦂

--- scripts/connect-figma-components/src/tab.figma.batch.ts
+++ scripts/connect-figma-components/src/tab.figma.batch.ts
@@ -11,8 +11,35 @@ function getExample() {
 
   const title = tabBase.getString('𝑻 - Title');
 
+  const tabConfig = instance.findInstance('tab-config', { traverseInstances: true });
+  if (tabConfig.type === 'ERROR') return null;
+
+  const hasBadge = tabConfig.getBoolean('badge'),
+    hasIcon = tabConfig.getBoolean('icon'),
+    iconOnly = tabConfig.getBoolean('icon only');
+
+  let badge;
+  if (hasBadge) {
+    badge = tabConfig.findInstance('sl-badge', { traverseInstances: true });
+    if (badge.type === 'ERROR') return null;
+
+    badge.properties.slot = { value: 'badge' };
+  }
+
+  let icon;
+  if (hasIcon) {
+    icon = tabConfig.findInstance('Base/Icon', { traverseInstances: true });
+    if (icon.type === 'ERROR') return null;
+
+    icon.properties.slot = { value: 'icon' };
+  }
+
   return figma.code`
-    <sl-tab ${selected ? 'selected' : ''}>${title}</sl-tab>
+    <sl-tab ${selected ? 'selected' : ''}>
+      ${icon?.executeTemplate().example}
+      ${iconOnly ? '' : title}
+      ${badge?.executeTemplate().example}
+    </sl-tab>
   `;
 }
 

d83cb84971512955a5646118cd2e3cb9c9f5200a
Jeroen Zwartepoorte <jeroen.zwartepoorte@iddinkgroup.com>
2026-08-11 10:20:38 +0200

    🍦

--- packages/components/shared/src/css.ts
+++ packages/components/shared/src/css.ts
@@ -1,6 +1,2 @@
-import breakpoints from './breakpoints.css' with { type: 'css' };
-
-export { breakpoints };
-
 export const firstFocusableSelector =
   'button:not([tabindex="-1"]), [href]:not([tabindex="-1"]), input:not([tabindex="-1"]), select:not([tabindex="-1"]), textarea:not([tabindex="-1"]), [tabindex]:not([tabindex="-1"]), [focusable]:not([tabindex="-1"])';
